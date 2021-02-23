e87eaec1ef15988e331ce78d4c458907597c30a1
Mat Jones <mat.jones@agilebits.com>
2021-02-23 16:30:41 -0500

    fix brew SwiftBar plugin

--- SwiftBarPlugins/homebrewcask.4h.sh
+++ SwiftBarPlugins/homebrewcask.4h.sh
@@ -8,24 +8,20 @@ exit_with_error() {
   exit 1;
 }
 
-/usr/local/bin/brew update &> /dev/null || exit_with_error; 
+/usr/local/bin/brew update &> /dev/null || exit_with_error;
 
-brewcasklist=$(/usr/local/bin/brew cask ls -1 | sed 's_(!)__g' | xargs /usr/local/bin/brew cask info | grep -A 1 'Not installed' | sed -e 's_Not installed__g' -e 's_From: https://github\.com/caskroom/homebrew-cask/blob/master/Casks/__g' -e 's_\.rb__g');
+HAS_UPDATES=$(brew outdated);
 
-brewcasknum=$(for line in $brewcasklist; do echo "$line" | grep "[a-z]" ; done | wc -w | xargs);
-
-echo "Brew Updates: ${brewcasknum}"
-echo "---"
-
-if [[ "${brewcasknum}" != "0" ]]; then
-    for line in $brewcasklist; do 
-        echo "$line" | grep "[a-z]" | sed 's_\(.*\)_& | bash=brew param1=cask param2=install param3=& terminal=true refresh=_g';
-    done
+if [[ "$HAS_UPDATES" ]]; then
+  echo "Brew: Ugrades Available";
+  echo "---";
 else
-    echo "No updates available."
-fi
+  echo "Brew";
+  echo "---";
+  echo "All packages up-to-date."
+fi;
 
-echo "Brew Update | bash=brew param1=update terminal=true refresh="
-echo "Brew Upgrade | bash=brew param1=upgrade terminal=true refresh="
-echo "Brew Cleanup | bash=brew param1=cleanup terminal=true refresh="
-echo "Brew Cask Cleanup | bash=brew param1=cask param2=cleanup terminal=true refresh="
+echo "Brew Update | bash=brew param1=update terminal=true refresh=true"
+echo "Brew Upgrade | bash=brew param1=upgrade terminal=true refresh=true"
+echo "Brew Cleanup | bash=brew param1=cleanup terminal=true refresh=true"
+echo "Brew Cask Cleanup | bash=brew param1=cask param2=cleanup terminal=true refresh=true"
