298785b9abd1e9c04765eb8b198bfcf55b42e552
Mat Jones <mat.jones@agilebits.com>
2021-03-02 12:47:00 -0500

    fix nuke-docker alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -12,7 +12,7 @@ alias vi="nvim"
 function nuke-docker
     set -l docker_container_names (docker ps --format "{{.Names}}")
     if test -n "$docker_container_names"
-        docker kill "$docker_container_names"
+        docker kill $docker_container_names
     else
         echo "No docker containers running."
     end
