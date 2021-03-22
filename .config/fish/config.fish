9ca489d57c427cc167cee148a4e87897fb9fe5f2
Mat Jones <mat.jones@agilebits.com>
2021-03-22 17:26:55 -0400

    update fzf to ignore .DS_Store files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,8 @@ export GPG_TTY=(tty)
 
 source $HOME/.config/fish/check-globals.fish
 
-set FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=fg:#d0d0d0,bg:#000000,hl:#5f87af --color=fg+:#d0d0d0,bg+:#2b2a2a,hl+:#5f87af --color=info:#4cc853,prompt:#4cc853,pointer:#ff77ff --color=marker:#4cc853,spinner:#ff77ff,header:#87afaf"
+set FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git --ignore .DS_Store -g ""'
+set FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --color=fg:#d0d0d0,bg:#000000,hl:#5f87af --color=fg+:#d0d0d0,bg+:#2b2a2a,hl+:#5f87af --color=info:#4cc853,prompt:#4cc853,pointer:#ff77ff --color=marker:#4cc853,spinner:#ff77ff,header:#87afaf"
 
 set PATH $PATH "$HOME/scripts"
 
