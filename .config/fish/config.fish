c763517e832461f1f51b948c25d9ad0487439503
Mat Jones <mjones44@ycp.edu>
2021-04-16 09:49:06 -0400

    add sources to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ export GPG_TTY=(tty)
 
 source $HOME/.config/fish/check-globals.fish
 
+eval (/opt/homebrew/bin/brew shellenv)
+thefuck --alias | source
+
 set FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git --ignore .DS_Store -g ""'
 set FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --color=fg:#d0d0d0,bg:#000000,hl:#5f87af --color=fg+:#d0d0d0,bg+:#2b2a2a,hl+:#5f87af --color=info:#4cc853,prompt:#4cc853,pointer:#ff77ff --color=marker:#4cc853,spinner:#ff77ff,header:#87afaf"
 
