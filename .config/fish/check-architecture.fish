5e9c716b064ecfd9b1523e1fcd30a8b2f7c4b9d1
Mat Jones <mjones44@ycp.edu>
2021-04-25 12:24:51 -0400

    add check-architecture.fish

---
+++ .config/fish/check-architecture.fish
@@ -0,0 +1,12 @@
+if [ (uname -m) = "x86_64" ]
+    if [ (sysctl -in sysctl.proc_translated) = "1" ]
+        echo "Running on Rosetta 2"
+        set -gx ARCHPREFERENCES arm64
+    else
+        echo "Running on native Intel"
+  end
+else if [ (uname -m) = "arm64" ]
+    echo "Running on ARM"
+else
+    echo "Unknown architecture: $arch_name"
+end
