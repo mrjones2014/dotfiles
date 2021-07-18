3ff6945fcfec1e33850045029c2f02672f425847
Mat Jones <mjones44@ycp.edu>
2021-07-18 15:01:34 -0400

    run prettier via lsp

--- .config/nvim/lua/custom-commands.lua
+++ .config/nvim/lua/custom-commands.lua
@@ -1,5 +1,5 @@
 -- run Prettier
-vim.cmd('command! -nargs=0 Prettier :CocCommand prettier.formatFile')
+vim.cmd('command! -nargs=0 Prettier :lua vim.lsp.buf.formatting()<CR>')
 
 -- alias git to Git for vim-fugitive
 vim.cmd('cnoreabbrev git Git')
