882471448d4c4feadc6658db2febad1099b33627
Mat Jones <mjones44@ycp.edu>
2021-07-18 14:21:03 -0400

    add comments to init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,17 @@
+-- Configuring Neovim
+require('plugins')
+require('settings')
+require('lsp-init')
+require('custom-commands')
+require('keybinds')
+require('custom-filetypes')
+
+-- Configuring Plugins
+require('configure-plugins-generic')
+require('configure-nvimtree')
+require('configure-dashboard')
+require('configure-autopairs')
+require('configure-lualine')
+require('configure-tabline')
+require('configure-compe')
+require('configure-theme')
