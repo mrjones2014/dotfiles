7ae06113f5a658a9ec2083cd892bd1cb2fe5e9e2
Mat Jones <mjones44@ycp.edu>
2021-07-18 14:18:59 -0400

    fix gohtmltmpl detection config

--- .config/nvim/lua/custom-filetypes.lua
+++ .config/nvim/lua/custom-filetypes.lua
@@ -1,7 +1,33 @@
+-- TODO couldn't quite get this working with a lua equivalent
 vim.cmd([[
+  " Found on: https://discourse.gohugo.io/t/vim-syntax-highlighting-for-hugo-html-templates/19398/8
+  function! DetectCustomFiletype()
+      if expand('%:e') == "html" && search("{{") != 0
+          set filetype=gohtmltmpl
+          return
+      endif
+
+      " if the first line is not a shebang, do nothing
+      if getline(1) !~ "#!/"
+          return
+      endif
+
+      if getline(1) =~ "zsh"
+          set filetype=zsh
+      endif
+
+      if getline(1) =~ "bash"
+          set filetype=bash
+      endif
+
+      if getline(0) =~ "fish"
+          set filetype=fish
+      endif
+  endfunction
+
   augroup CustomFileTypeDetection
-    autocmd! BufRead, BufReadPost,BufNewFile,BufWritePost * lua require('filetype-utils').detect_filetypes()
-    autocmd! FileType Fish compiler fish
-    autocmd! FileType json set conceallevel=0
+    au! BufRead,BufNewFile,BufWritePost * call DetectCustomFiletype()
+    au! FileType fish compiler fish
+    au! FileType json set conceallevel=0
   augroup END
 ]])
