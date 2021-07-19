d75e21dee4f8ce351f03a028de1880f69a4f0b5e
Mat Jones <mat.jones@agilebits.com>
2021-07-19 09:57:25 -0400

    format and organize imports on save

--- .config/nvim/lua/lsp/typescript.lua
+++ .config/nvim/lua/lsp/typescript.lua
@@ -1,3 +1,14 @@
+local utils = require('lsp/utils')
+local tsUtils = require('nvim-lsp-ts-utils')
+local on_attach = function(client, buffnr)
+  utils.on_attach()
+  tsUtils.setup {
+    enable_import_on_completion = true,
+    eslint_enable_diagnostics = true,
+  }
+  tsUtils.setup_client(client)
+end
+
 require('lspconfig').tsserver.setup({
-  on_attach = require('lsp/utils').on_attach,
+  on_attach = on_attach,
 })
