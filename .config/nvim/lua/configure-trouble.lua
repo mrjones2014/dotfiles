88f3f3c6c5bffab9d67d8a5140c5989b454db47e
Mat Jones <mat.jones@agilebits.com>
2021-07-19 10:35:03 -0400

    add trouble.nvim

---
+++ .config/nvim/lua/configure-trouble.lua
@@ -0,0 +1,6 @@
+require('trouble').setup({})
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
