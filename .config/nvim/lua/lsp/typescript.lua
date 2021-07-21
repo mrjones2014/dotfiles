f4ec6c3d878173abba21b5dc0d9bb60a643cec94
Mat Jones <mat.jones@agilebits.com>
2021-07-21 11:44:30 -0400

    fix typescript formatting

--- .config/nvim/lua/lsp/typescript.lua
+++ .config/nvim/lua/lsp/typescript.lua
@@ -1,28 +1,20 @@
 local utils = require('lsp/utils')
-local tsUtils = require('nvim-lsp-ts-utils')
-local on_attach = function(client, buffnr)
-  utils.on_attach()
-  tsUtils.setup {
-    enable_import_on_completion = true,
-    eslint_enable_diagnostics = true,
-  }
-  tsUtils.setup_client(client)
-end
 
-local function organize_imports()
+local function organize_imports_and_format()
   local params = {
     command = "_typescript.organizeImports",
     arguments = { vim.api.nvim_buf_get_name(0) },
     title = ""
   }
-  vim.lsp.buf.execute_command(params)
+  vim.lsp.buf_request_sync(vim.api.nvim_get_current_buf(), "workspace/executeCommand", params, 1500)
+  vim.cmd('Neoformat')
 end
 
 require('lspconfig').tsserver.setup({
-  on_attach = on_attach,
+  on_attach = utils.on_attach,
   commands = {
-    OrganizeImports = {
-      organize_imports,
+    OrganizeAndFormat = {
+      organize_imports_and_format,
       description = "Organize Imports"
     }
   }
