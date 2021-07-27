737360855d98b3d225e030e02c664bede6e043f7
Mat Jones <mat.jones@agilebits.com>
2021-07-27 16:36:20 -0400

    update commentstring for gohtmltmpl

--- .config/nvim/lua/custom-filetypes.lua
+++ .config/nvim/lua/custom-filetypes.lua
@@ -1,8 +1,12 @@
 -- TODO couldn't quite get this working with a lua equivalent
 vim.cmd([[
+  function SetGoHtmlOpts()
+    setlocal syntax=gohtmltmpl
+    setlocal commentstring={{/*\ %s\ */}}
+  endfunction
   augroup CustomFileTypeDetection
-    au! BufEnter *.html set syntax=gohtmltmpl
-    au! FileType html set syntax=gohtmltmpl
+    au! BufEnter *.html :call SetGoHtmlOpts()
+    au! FileType html :call SetGoHtmlOpts()
     au! FileType fish compiler fish
     au! FileType json set conceallevel=0
   augroup END
