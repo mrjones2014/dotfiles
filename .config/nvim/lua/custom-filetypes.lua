685b9697c2792ae0185eb434ebb8a0e78c7fb4a6
Mat Jones <mat.jones@agilebits.com>
2021-07-27 13:51:55 -0400

    uncomment custom filetype detection code

--- .config/nvim/lua/custom-filetypes.lua
+++ .config/nvim/lua/custom-filetypes.lua
@@ -1,9 +1,9 @@
 -- TODO couldn't quite get this working with a lua equivalent
--- vim.cmd([[
---   augroup CustomFileTypeDetection
---     au! BufEnter *.html set syntax=gohtmltmpl
---     au! FileType html set syntax=gohtmltmpl
---     au! FileType fish compiler fish
---     au! FileType json set conceallevel=0
---   augroup END
--- ]])
+vim.cmd([[
+  augroup CustomFileTypeDetection
+    au! BufEnter *.html set syntax=gohtmltmpl
+    au! FileType html set syntax=gohtmltmpl
+    au! FileType fish compiler fish
+    au! FileType json set conceallevel=0
+  augroup END
+]])
