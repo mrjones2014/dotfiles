5f426222a9a135292c96ba99302045cf875a0a87
Mat Jones <mat.jones@agilebits.com>
2021-08-02 15:55:13 -0400

    fix typo

--- .config/fish/check-architecture.fish
+++ .config/fish/check-architecture.fish
@@ -2,7 +2,7 @@ function check_architecture
   if [ (uname -m) = "x86_64" ]
     if [ "(sysctl -in sysctl.proc_translated)" = "1" ]
         echo "Running on Rosetta 2"
-        set -gx ARCHPREFERENCES arm64
+        set -gx ARCHPREFERENCE arm64
     else
         echo "Running on native Intel"
     end
