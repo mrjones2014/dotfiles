3b1d192a25d8eed06c82efde5df1c49bf437f232
Mat Jones <mat.jones@agilebits.com>
2021-08-02 16:32:36 -0400

    make nvim-tree respect .gitignore

--- .config/nvim/lua/configure-nvimtree.lua
+++ .config/nvim/lua/configure-nvimtree.lua
@@ -1,18 +1,21 @@
 local g = vim.g
+local tree_cb = require('nvim-tree.config').nvim_tree_callback
+
 g.nvim_tree_side = "right"
 g.nvim_tree_width = 40
-g.nvim_tree_auto_open = 0
+g.nvim_tree_auto_open = 1
 g.nvim_tree_auto_close = 1
 g.nvim_tree_follow = 1
-g.nvim_tree_indent_markets = 1
+g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
+g.nvim_tree_gitignore = 1
 g.nvim_tree_add_trailing = 1
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache", ".DS_Store", ".netrwhist" }
+g.nvim_tree_ignore = { ".git", "node_modules", ".cache", ".DS_Store", ".netrwhist", "dist" }
 g.nvim_tree_special_files = {}
+g.nvim_tree_lsp_diagnostics = 1
+g.nvim_tree_auto_ignore_ft = { 'fzf', 'term', 'terminal' }
 g.nvim_tree_disable_default_keybindings = 1
-
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-vim.g.nvim_tree_bindings = {
+g.nvim_tree_bindings = {
   { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
   { key = "<C-v>",                        cb = tree_cb("vsplit") },
   { key = "R",                            cb = tree_cb("refresh") },
