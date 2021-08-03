2a2a8d8620a9b38e64f30a64ffec33545c30cb17
Mat Jones <mat.jones@agilebits.com>
2021-08-03 12:12:14 -0400

    tweak indent line nvim settings

--- .config/nvim/lua/configure-indent-blankline.lua
+++ .config/nvim/lua/configure-indent-blankline.lua
@@ -1,9 +1,10 @@
 require('indent_blankline').setup({
   buftype_exclude = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'},
   filetype_exclude = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'},
-  context_patterns = {'class', 'function', 'method', 'block', 'list_literal', 'selector', '^if', '^table', 'if_statement', 'while', 'for'},
+  context_patterns = {'class', 'function', 'method', 'block', 'list_literal', 'selector', '^if', '^table', 'if_statement', 'while', 'for', 'object', 'start_tag', 'open_tag', 'element'},
   show_first_indent_level = true,
   show_current_context = true,
+  show_trailing_blankline_indent = false,
 })
 
 -- because lazy load indent-blankline so need readd this autocmd
--- .config/nvim/lua/configure-treesitter.lua
+++ .config/nvim/lua/configure-treesitter.lua
@@ -2,5 +2,11 @@ require('nvim-treesitter.configs').setup({
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = {'gohtmltmpl'}
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
   }
 })
