b63a2bb3beaa22c5a86dd2b26faf8a6443081283
Mat Jones <mat.jones@agilebits.com>
2021-08-03 16:10:54 -0400

    add diagnostics to lualine

--- .config/nvim/lua/configure-lualine.lua
+++ .config/nvim/lua/configure-lualine.lua
@@ -1,9 +1,11 @@
+local icons = require('nvim-nonicons')
+
 local function filepath()
   local path = vim.fn.expand('%')
   if vim.fn.winwidth(0) <= 84 then
     path = vim.fn.pathshorten(path)
   end
-  return path
+  return icons.get('file') .. '  ' .. path
 end
 
 require('lualine').setup({
@@ -13,8 +15,8 @@ require('lualine').setup({
   sections = {
     lualine_a = {{'mode', lower = false}},
     lualine_b = {'branch'},
-    lualine_c = {filepath},
-    lualine_d = {'location'}
+    lualine_c = {{'diagnostics', sources = {'nvim_lsp'}, sections = {'error', 'warn', 'info', 'hint'}}, filepath},
+    lualine_x = {'location'}
   },
   extensions = {'nvim-tree'}
 })
