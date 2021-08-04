d278828a4ba9b6161c56de8f0f028d318ac0bdc4
Mat Jones <mat.jones@agilebits.com>
2021-08-04 16:41:15 -0400

    add jk as escape in fish vi mode

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,4 +2,5 @@
 function fish_vi_cursor; end
 function fish_user_key_bindings
   fish_vi_key_bindings
+  bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
