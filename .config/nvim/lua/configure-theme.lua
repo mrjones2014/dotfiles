f06524fb5842d5bee4602226b31e775eeb3db7a8
Mat Jones <mat.jones@agilebits.com>
2021-08-06 15:51:02 -0400

    remove telescope custom colors

--- .config/nvim/lua/configure-theme.lua
+++ .config/nvim/lua/configure-theme.lua
@@ -2,23 +2,10 @@ local g = vim.g
 
 local sidebarColor = '#15181b'
 g.tokyonight_style = 'night'
-g.tokyonight_sidebars = {'packer', 'NvimTree', 'term', 'terminal', 'TelescopePrompt'}
+g.tokyonight_sidebars = {'packer', 'NvimTree', 'term', 'terminal'}
 g.tokyonight_colors = {bg = '#24292e', bg_dark = '#24292e', bg_sidebar = sidebarColor}
 
 vim.cmd('colorscheme tokyonight')
-
-local telescopeColorAutoCmd = [[
-  function SetTelescopeCustomColors()
-    highlight TelescopeNormal guibg=$sidebarColor
-  endfunction
-
-  augroup TelescopeCustomColorConfig
-    autocmd VimEnter * :call SetTelescopeCustomColors()
-  augroup END
-]]
-
--- Home-grown variable interpolation so I can have the sidebarColor as a variable
-vim.cmd(telescopeColorAutoCmd:gsub('$(%w+)', { sidebarColor = sidebarColor }))
 
 -- has to be required after theme is set up for highlighting
 require('configure-trailing-whitespace')
