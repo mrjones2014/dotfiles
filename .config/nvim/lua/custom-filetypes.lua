4526ddd60bb2c32280dbd5c1db98225dd1581e97
Mat Jones <mat.jones@agilebits.com>
2021-08-09 12:18:58 -0400

    remove 'compiler fish'

--- .config/nvim/lua/custom-filetypes.lua
+++ .config/nvim/lua/custom-filetypes.lua
@@ -10,7 +10,6 @@ vim.cmd([[
   augroup CustomFileTypeDetection
     au! BufEnter *.html :call SetGoHtmlOpts()
     au! FileType html :call SetGoHtmlOpts()
-    au! FileType fish compiler fish
     au! FileType json set conceallevel=0
   augroup END
 ]])
