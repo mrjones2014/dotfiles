b9890a0bfad76b4480c27c9bc64c5aa8d13bf7c7
Mat Jones <mat@mjones.network>
2021-08-16 09:26:18 -0400

    prevent trouble.nvim from hijacking my window movement keys

--- .config/nvim/lua/plugins/configure-trouble.lua
+++ .config/nvim/lua/plugins/configure-trouble.lua
@@ -1,6 +1,5 @@
-require('trouble').setup({})
-local signs = { Error = ' ', Warning = ' ', Hint = ' ', Information = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'LspDiagnosticsSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-end
+require('trouble').setup({
+  action_keys = {
+    hover = {},
+  },
+})
