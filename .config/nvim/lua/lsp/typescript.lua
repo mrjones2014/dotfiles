6633922c6b3cbaecf2c8ba054b3de0669b977a12
Mat Jones <mat.jones@agilebits.com>
2021-08-16 08:02:19 -0400

    remove overriding typescript lsp root

--- .config/nvim/lua/lsp/typescript.lua
+++ .config/nvim/lua/lsp/typescript.lua
@@ -1,7 +1,5 @@
 local utils = require('lsp/utils')
-local rootUtils = require('lspconfig/util')
 
 require('lspconfig').tsserver.setup({
   on_attach = utils.on_attach,
-  root_dir = rootUtils.root_pattern('.git'),
 })
