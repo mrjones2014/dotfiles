24cfb1deab55831221bdd4da528842099faef5b7
Mat Jones <mat@mjones.network>
2021-08-16 09:26:18 -0400

    prevent trouble.nvim from hijacking my window movement keys

--- .config/nvim/lua/lsp-init.lua
+++ .config/nvim/lua/lsp-init.lua
@@ -1,5 +1,12 @@
 local lsp = require('lspconfig')
 
+-- customize LSP icons
+local signs = { Error = ' ', Warning = ' ', Hint = ' ', Information = ' ' }
+for type, icon in pairs(signs) do
+  local hl = 'LspDiagnosticsSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+end
+
 require('load-all')(os.getenv('HOME') .. '/.config/nvim/lua/lsp')
 
 vim.cmd('command! Format :lua require("lsp.utils").formatDocument()')
