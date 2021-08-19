918602feb0fb3e2af2cfa34cc588ff2c53f71d17
Mat Jones <mat@mjones.network>
2021-08-19 12:23:35 -0400

    fix fish vi bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,11 @@ if [ -z "$TMUX" ] && [ "$START_TMUX_PLEASE" = 1 ]
     exec tmux new-session -A -s $USER
 end
 
+# workaround for https://github.com/fish-shell/fish-shell/issues/3481
+function fish_vi_cursor; end
+fish_vi_key_bindings
+bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+
 export GPG_TTY=(tty)
 export EDITOR="nvim"
 
--- .config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,6 +0,0 @@
-# workaround for https://github.com/fish-shell/fish-shell/issues/3481
-function fish_vi_cursor; end
-function fish_user_key_bindings
-  fish_vi_key_bindings
-  bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
-end
