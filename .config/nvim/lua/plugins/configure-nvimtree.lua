b2a3eaf5c448f08d8b8afeca6f8c32a2ed0dd1f1
Mat Jones <mat@mjones.network>
2021-08-24 15:35:30 -0400

    Revert "debugging nvim theme"
    This reverts commit c1e8f9f2a3989adf240d200259c373cc23ce9753.

--- .config/nvim/lua/plugins/configure-nvimtree.lua
+++ .config/nvim/lua/plugins/configure-nvimtree.lua
@@ -1,8 +1,10 @@
+local tree_cb = require('nvim-tree.config').nvim_tree_callback
+
 local lspIcons = require('modules.lsp-icons')
 
 vim.g.nvim_tree_side = 'right'
 vim.g.nvim_tree_width = 40
-vim.g.nvim_tree_auto_open = 0
+vim.g.nvim_tree_auto_open = 1
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
@@ -14,6 +16,15 @@ vim.g.nvim_tree_special_files = {}
 vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_auto_ignore_ft = { 'TelescopePrompt', 'term', 'terminal' }
 vim.g.nvim_tree_disable_default_keybindings = 1
+vim.g.nvim_tree_bindings = {
+  { key = { '<CR>', 'o', '<2-LeftMouse>' }, cb = tree_cb('edit') },
+  { key = '<C-v>', cb = tree_cb('vsplit') },
+  { key = 'R', cb = tree_cb('refresh') },
+  { key = 'a', cb = tree_cb('create') },
+  { key = 'd', cb = tree_cb('remove') },
+  { key = 'r', cb = tree_cb('rename') },
+  { key = '.', cb = tree_cb('cd') },
+}
 vim.g.nvim_tree_icons = {
   lsp = {
     hint = lspIcons.Hint,
