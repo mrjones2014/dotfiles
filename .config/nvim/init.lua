c1e8f9f2a3989adf240d200259c373cc23ce9753
Mat Jones <mat@mjones.network>
2021-08-24 15:32:00 -0400

    debugging nvim theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 local loadAll = require('modules.load-all')
 local luaPath = os.getenv('HOME') .. '/.config/nvim/lua/'
 
+require('modules.theme-manager').configureTheme('catppuccino')
 loadAll(luaPath)
 loadAll(luaPath .. 'plugins')
-require('modules.theme-manager').configureTheme('catppuccino')
