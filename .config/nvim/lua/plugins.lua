7fe106e4f73c9c612b3e6face9f4a120bf7c8e27
Mat Jones <mat@mjones.network>
2021-08-24 15:32:00 -0400

    debugging nvim theme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,6 +2,10 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use('wbthomason/packer.nvim')
 
+  -- Themes
+  use('folke/tokyonight.nvim')
+  use('Pocco81/Catppuccino.nvim')
+
   -- Dependencies of other plugins
   use('nvim-lua/plenary.nvim')
   use('nvim-lua/popup.nvim')
@@ -39,7 +43,22 @@ return require('packer').startup(function()
   -- UI + utils
   use('kyazdani42/nvim-web-devicons')
   use({ 'yamatsum/nvim-nonicons', requires = { 'kyazdani42/nvim-web-devicons' } })
-  use('kyazdani42/nvim-tree.lua')
+  use({
+    'kyazdani42/nvim-tree.lua',
+    cmd = { 'NvimTreeToggle' },
+    config = function()
+      local tree_cb = require('nvim-tree.config').nvim_tree_callback
+      vim.g.nvim_tree_bindings = {
+        { key = { '<CR>', 'o', '<2-LeftMouse>' }, cb = tree_cb('edit') },
+        { key = '<C-v>', cb = tree_cb('vsplit') },
+        { key = 'R', cb = tree_cb('refresh') },
+        { key = 'a', cb = tree_cb('create') },
+        { key = 'd', cb = tree_cb('remove') },
+        { key = 'r', cb = tree_cb('rename') },
+        { key = '.', cb = tree_cb('cd') },
+      }
+    end,
+  })
   use('lukas-reineke/indent-blankline.nvim')
   use('farmergreg/vim-lastplace')
   use('airblade/vim-gitgutter')
@@ -51,8 +70,4 @@ return require('packer').startup(function()
   use('folke/todo-comments.nvim')
   -- yaclt
   use('~/git/yaclt.nvim')
-
-  -- Themes
-  use('folke/tokyonight.nvim')
-  use({ 'Pocco81/Catppuccino.nvim', after = { 'nvim-tree.lua', 'nvim-bufferline', 'lualine.nvim' } })
 end)
