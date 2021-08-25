8c6dad8c5869caad9fd23b89ed587f0c777d9116
Mat Jones <mat@mjones.network>
2021-08-25 08:13:35 -0400

    replace plugin with built-in treesitter functionality

--- .config/nvim/lua/plugins/configure-treesitter.lua
+++ .config/nvim/lua/plugins/configure-treesitter.lua
@@ -15,6 +15,15 @@ return {
         updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
         persist_queries = false, -- Whether the query persists across vim sessions
       },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = 'gnn',
+          node_incremental = 'grn',
+          scope_incremental = 'grc',
+          node_decremental = 'grm',
+        },
+      },
       rainbow = {
         enable = true,
         extended_mode = true,
