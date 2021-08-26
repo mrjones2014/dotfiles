ca29cf3ffb0f7dc2bca246a88238f219ed78e629
Mat Jones <mat@mjones.network>
2021-08-26 08:18:20 -0400

    auto PackerCompile on exit if getcwd() is ~/.config/nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,3 +6,9 @@ require('configure-trailing-whitespace')
 require('configure-plugins-generic')
 require('lsp-init')
 require('plugins')
+
+if vim.fn.getcwd() == (os.getenv('HOME') .. '/.config/nvim') then
+  vim.cmd([[
+    autocmd VimLeavePre * :PackerCompile
+  ]])
+end
