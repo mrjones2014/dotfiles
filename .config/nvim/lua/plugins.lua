dfce296323d3f35f3def11abcd21f99f08e37ba5
Mat Jones <mat@mjones.network>
2021-08-26 08:59:09 -0400

    bootstrap packer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,10 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
+  vim.cmd('packadd packer.nvim')
+end
+
 return require('packer').startup(function()
   -- Packer can manage itself
   use('wbthomason/packer.nvim')
