bb64d2846bf15d4043c41c235c5c2d20395d2642
Mat Jones <mat@mjones.network>
2021-08-30 18:09:53 -0400

    symlink ~/.config/nvim/stylua.toml to ~/.hammerspoon/stylua.toml

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,25 +1,25 @@
 -- bind mouse side buttons to forward/back
 hs.eventtap.new({ hs.eventtap.event.types.otherMouseUp }, function(event)
-	local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
-	if button == 3 then
-		hs.eventtap.keyStroke({ "cmd" }, "[")
-	end
-	if button == 4 then
-		hs.eventtap.keyStroke({ "cmd" }, "]")
-	end
+  local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
+  if button == 3 then
+    hs.eventtap.keyStroke({ 'cmd' }, '[')
+  end
+  if button == 4 then
+    hs.eventtap.keyStroke({ 'cmd' }, ']')
+  end
 end):start()
 
 -- bind shift+ctrl+option+command+s to sleep
-hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "S", function()
-	hs.caffeinate.systemSleep()
+hs.hotkey.bind({ 'cmd', 'alt', 'ctrl', 'shift' }, 'S', function()
+  hs.caffeinate.systemSleep()
 end)
 
 -- bind shift+ctrl+option+l to lock screen
-hs.hotkey.bind({ "alt", "ctrl", "shift" }, "L", function()
-	hs.caffeinate.lockScreen()
+hs.hotkey.bind({ 'alt', 'ctrl', 'shift' }, 'L', function()
+  hs.caffeinate.lockScreen()
 end)
 
 -- bind print screen key (F13 on mac) to screenshot tool
-hs.hotkey.bind({}, "f13", function()
-	hs.eventtap.keyStroke({ "cmd", "shift" }, "5")
+hs.hotkey.bind({}, 'f13', function()
+  hs.eventtap.keyStroke({ 'cmd', 'shift' }, '5')
 end)
