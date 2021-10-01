4574c1a4000f6ef5fb452e7847226a6cf46d75ed
Mat Jones <mat@mjones.network>
2021-10-01 12:40:35 -0400

    use builtin for stylelint

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -1,52 +1,41 @@
 local null_ls = require('null-ls')
 local b = null_ls.builtins
 
-null_ls.config({
-  sources = {
-    b.diagnostics.eslint.with({
-      command = 'eslint_d',
-    }),
-    b.formatting.prettierd.with({
-      filetypes = {
-        'html',
-        'json',
-        'javascript',
-        'typescript',
-        'javascriptreact',
-        'typescriptreact',
-        'scss',
-        'css',
-      },
-    }),
-    b.formatting.stylua,
-    b.diagnostics.luacheck,
-    b.formatting.shfmt,
-    b.diagnostics.shellcheck.with({
-      diagnostics_format = '#{m} [#{s}] [#{c}]',
-    }),
-    -- stylelint is not built-in
-    {
-      name = 'stylelint',
-      method = null_ls.methods.DIAGNOSTICS,
-      filetypes = { 'scss', 'sass', 'css' },
-      generator = null_ls.generator({
-        command = './node_modules/.bin/stylelint',
-        args = { '--formatter', 'json', '--stdin-filename', '%filepath' },
-        format = 'json',
-        check_exit_code = function(code)
-          return code == 0
-        end,
-        on_output = function(params)
-          print(vim.inspect(params))
-          --[[ local parser = require('null-ls.helpers').diagnostics.from_json({
-            attributes = {},
-          })
-
-          return parser({ output = params.output }) ]]
-        end,
-      }),
+local sources = {
+  b.diagnostics.eslint.with({
+    command = 'eslint_d',
+  }),
+  b.formatting.prettierd.with({
+    filetypes = {
+      'html',
+      'json',
+      'javascript',
+      'typescript',
+      'javascriptreact',
+      'typescriptreact',
+      'scss',
+      'css',
     },
-  },
+  }),
+  b.formatting.stylua,
+  b.diagnostics.luacheck,
+  b.formatting.shfmt,
+  b.diagnostics.shellcheck.with({
+    diagnostics_format = '#{m} [#{s}] [#{c}]',
+  }),
+}
+
+if vim.fn.filereadable('./node_modules/.bin/stylelint') then
+  table.insert(
+    sources,
+    b.formatting.stylelint.with({
+      command = './node_modules/.bin/stylelint',
+    })
+  )
+end
+
+null_ls.config({
+  sources = sources,
 })
 
 require('lspconfig')['null-ls'].setup({
