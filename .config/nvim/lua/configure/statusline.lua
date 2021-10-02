fc39f8bdf683a835a93b1a5b1cbd57ac90213780
Mat Jones <mat@mjones.network>
2021-10-02 13:55:56 -0400

    tweak statusline colors

--- .config/nvim/lua/configure/statusline.lua
+++ .config/nvim/lua/configure/statusline.lua
@@ -4,17 +4,17 @@ return {
     local components = require('configure.statusline.components')
     local sections = {
       { components.mode, { 'black', 'ActiveBg' } },
-      { '', { 'ActiveBg', 'InactiveFg' } },
-      { components.branchName, { 'ActiveBg', 'InactiveFg' } },
-      { '', { 'InactiveFg', 'black' } },
+      { '', { 'ActiveBg', 'blue_dark' } },
+      { components.branchName, { 'ActiveBg', 'blue_dark' } },
+      { '', { 'blue_dark', 'black' } },
       { components.filetypeIcon, { 'ActiveBg', 'black' } },
       { components.filename, { 'ActiveBg', 'black' } },
-      { '%=', '' },
+      { '%=', { 'ActiveBg', 'black' } },
       { components.filetypeIcon, { 'ActiveBg', 'black' } },
       { components.filetype, { 'white', 'black' } },
-      { '', { 'InactiveFg', 'black' } },
-      { components.progress, { 'ActiveBg', 'InactiveFg' } },
-      { '', { 'ActiveBg', 'InactiveFg' } },
+      { '', { 'blue_dark', 'black' } },
+      { components.progress, { 'ActiveBg', 'blue_dark' } },
+      { '', { 'ActiveBg', 'blue_dark' } },
       { components.lineCol, { 'black', 'ActiveBg' } },
     }
 
@@ -25,6 +25,9 @@ return {
     }
 
     require('windline').setup({
+      colors_name = function(colors)
+        colors.blue_dark = '#132234'
+      end,
       statuslines = {
         defaultStatusLine,
       },
