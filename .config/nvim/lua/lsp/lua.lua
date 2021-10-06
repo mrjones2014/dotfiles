7b19f87b4bb354c4c77f2a1cd6aef56cae2db68a
Mat Jones <mat@mjones.network>
2021-10-06 10:46:55 -0400

    try adding lua lsp

---
+++ .config/nvim/lua/lsp/lua.lua
@@ -0,0 +1,32 @@
+local binRoot = os.getenv('HOME') .. '/git/personal/lua-language-server/bin/macOS/'
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+require('lspconfig').sumneko_lua.setup({
+  cmd = { binRoot .. 'lua-language-server', '-E', binRoot .. 'main.lua' },
+  on_attach = require('modules.lsp-utils').on_attach,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
--- .config/nvim/lua/lsp/typescript.lua
+++ .config/nvim/lua/lsp/typescript.lua
@@ -1,5 +1,3 @@
-local utils = require('modules.lsp-utils')
-
 require('lspconfig').tsserver.setup({
-  on_attach = utils.on_attach,
+  on_attach = require('modules.lsp-utils').on_attach,
 })
