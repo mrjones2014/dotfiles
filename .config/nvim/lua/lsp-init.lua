050b8d2dcea82ed6e9ce85a0e9c351fdecd8331e
Mat Jones <mat@mjones.network>
2021-10-21 08:03:45 -0400

    make nvim update diagnostics in insert mode

--- .config/nvim/lua/lsp-init.lua
+++ .config/nvim/lua/lsp-init.lua
@@ -5,6 +5,14 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 end
 
+-- set diagnostics to update in insert mode
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+})
+
 require('lsp.css')
 require('lsp.html')
 require('lsp.json')
