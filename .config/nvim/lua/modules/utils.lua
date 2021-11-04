f83654feb8f052f59d3e188b6f676ec9a983b2ef
Mat Jones <mat@mjones.network>
2021-11-04 09:27:29 -0400

    Revert "add rename notifications with vim-notify"
    This reverts commit 1ae33b574247ebd8e8975a6c55b3272ae6d4073f.

--- .config/nvim/lua/modules/utils.lua
+++ .config/nvim/lua/modules/utils.lua
@@ -1,21 +1,14 @@
 local M = {}
 
-function M.get_relative_path(file_path)
-  local plenary_path = require('plenary.path')
-  local parsed_path, _ = file_path:gsub('file://', '')
-  local path = plenary_path:new(parsed_path)
-  local relative_path = path:make_relative(vim.fn.getcwd())
-  return './' .. relative_path
-end
-
-function M.highlight(group, bg, fg, gui)
-  if gui ~= nil and gui ~= '' then
-    vim.api.nvim_command(('hi %s guibg=%s guifg=%s gui=%s'):format(group, bg, fg, gui))
-  elseif bg == nil then
-    vim.api.nvim_command(('hi %s guifg=%s'):format(group, fg))
-  else
-    vim.api.nvim_command(('hi %s guibg=%s guifg=%s'):format(group, bg, fg))
+function M.filter(subject, predicate)
+  local filtered = {}
+  for k, v in pairs(subject) do
+    if predicate(k, v, subject) then
+      table.insert(filtered, v)
+    end
   end
+
+  return filtered
 end
 
 return M
