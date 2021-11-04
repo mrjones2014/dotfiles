29d95381d9f5e0c213601eaf7987d4c2120da86a
Mat Jones <mat@mjones.network>
2021-11-04 09:10:39 -0400

    add rename notifications with vim-notify

--- .config/nvim/lua/modules/utils.lua
+++ .config/nvim/lua/modules/utils.lua
@@ -1,14 +1,21 @@
 local M = {}
 
-function M.filter(subject, predicate)
-  local filtered = {}
-  for k, v in pairs(subject) do
-    if predicate(k, v, subject) then
-      table.insert(filtered, v)
-    end
-  end
+function M.get_relative_path(file_path)
+  local plenary_path = require('plenary.path')
+  local parsed_path, _ = file_path:gsub('file://', '')
+  local path = plenary_path:new(parsed_path)
+  local relative_path = path:make_relative(vim.fn.getcwd())
+  return './' .. relative_path
+end
 
-  return filtered
+function M.highlight(group, bg, fg, gui)
+  if gui ~= nil and gui ~= '' then
+    vim.api.nvim_command(('hi %s guibg=%s guifg=%s gui=%s'):format(group, bg, fg, gui))
+  elseif bg == nil then
+    vim.api.nvim_command(('hi %s guifg=%s'):format(group, fg))
+  else
+    vim.api.nvim_command(('hi %s guibg=%s guifg=%s'):format(group, bg, fg))
+  end
 end
 
 return M
