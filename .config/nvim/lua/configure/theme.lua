c76185e64e69e03aadc11060771edf725a9544ec
Mat Jones <mat@mjones.network>
2021-11-30 15:51:14 -0500

    reorganize theme config

--- .config/nvim/lua/configure/theme.lua
+++ .config/nvim/lua/configure/theme.lua
@@ -1,5 +1,15 @@
+-- use local theme repo if it's cloned
+if vim.fn.isdirectory(os.getenv('HOME') .. '/git/personal/lighthaus.nvim') > 0 then
+  return {
+    '~/git/personal/lighthaus.nvim',
+    config = function()
+      require('lighthaus').setup({ bg_dark = true })
+    end,
+  }
+end
+
 return {
-  'mrjones2014',
+  'mrjones2014/lighthaus.nvim',
   config = function()
     require('lighthaus').setup({ bg_dark = true })
   end,
