126df1f17f426c19c9a7c69ed3d31019509edaa8
Mat Jones <mat@mjones.network>
2021-12-03 09:31:24 -0500

    add Vale to null-ls config for Neovim

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -1,6 +1,8 @@
 local null_ls = require('null-ls')
 local b = null_ls.builtins
 
+local home = os.getenv('HOME')
+
 local sources = {
   -- code actions
   b.code_actions.gitsigns,
@@ -10,6 +12,18 @@ local sources = {
   b.diagnostics.luacheck,
   b.diagnostics.shellcheck.with({
     diagnostics_format = '#{m} [#{s}] [#{c}]',
+  }),
+  b.diagnostics.vale.with({
+    args = function(params)
+      return {
+        '--config=' .. home .. '/.config/vale/vale.ini',
+        '--no-exit',
+        '--output',
+        'JSON',
+        '--ext',
+        '.' .. vim.fn.fnamemodify(params.bufname, ':e'),
+      }
+    end,
   }),
 
   -- formatting
