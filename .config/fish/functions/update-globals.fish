9bcc4646fc4cf375084d0ec6529de0bdfb2ff516
Mat Jones <mat@mjones.network>
2022-01-13 08:11:01 -0500

    update `update-globals` function to use npm instead of yarn

--- .config/fish/functions/update-globals.fish
+++ .config/fish/functions/update-globals.fish
@@ -1,6 +1,9 @@
 function update-globals
+    # update homebrew packages
     brew update
     brew upgrade --fetch-HEAD
+
+    # update sumneko lua-language-server via git and build from source
     pushd ~/git/personal/lua-language-server/
     git fetch
     set -l updates (git log HEAD..origin/master --oneline)
@@ -9,6 +12,16 @@ function update-globals
         pushd 3rd/luamake && compile/install.sh && popd && ./3rd/luamake/luamake rebuild
     end
     popd
-    yarn global upgrade
-    nvim --headless +PackerCompile +qall!
+
+    # update neovim plugins
+    nvim --headless +PackerSync +qall!
+
+    # update npm packages in all node versions
+    # save to variable so we can reset to this version after updating packages
+    set -l current_node_version (nvm current)
+    for node_version in (nvm list | sed 's/.*\(v[0-9]*\.[0-9]*\.[0-9]*\).*/\1/')
+        nvm use "$node_version"
+        npm update -g
+    end
+    nvm use "$current_node_version"
 end
