cf9c8b566f9dca3a24a546d10cb12394380e40d0
Mat Jones <mat@mjones.network>
2022-01-18 15:17:19 -0500

    ensure filename is always relative to cwd in lualine

--- .config/nvim/lua/configure/lualine.lua
+++ .config/nvim/lua/configure/lualine.lua
@@ -10,6 +10,8 @@ return {
 
     local function filepath()
       local path = vim.fn.expand('%')
+      -- ensure path is relative to cwd
+      path = path:gsub(vim.fn.getcwd() .. '/', '')
       if vim.fn.winwidth(0) <= 84 then
         path = vim.fn.pathshorten(path)
       end
