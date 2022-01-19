9c723084cadd0dd86e49b503102a13578f2a0483
Mat Jones <mat@mjones.network>
2022-01-19 07:22:06 -0500

    fix logic in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
-if #vim.v.argv == 2 and vim.fn.isdirectory(vim.v.argv[2]) then
+if #vim.v.argv == 2 and vim.fn.isdirectory(vim.v.argv[2]) > 0 then
   vim.api.nvim_set_current_dir(vim.v.argv[2])
   vim.cmd('bw')
   vim.cmd('intro')
