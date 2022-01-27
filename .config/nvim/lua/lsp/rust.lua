065aafa707fa15dfbf88d16c09aa9d5fb5641271
Mat Jones <mat@mjones.network>
2022-01-27 12:37:44 -0500

    remove explicit root_dir from rust-analyzer settings

--- .config/nvim/lua/lsp/rust.lua
+++ .config/nvim/lua/lsp/rust.lua
@@ -1,8 +1,6 @@
 require('lspconfig').rust_analyzer.setup({
   on_attach = require('lsp.utils').on_attach,
   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  -- set root_dir explicitly so that individual project root is detected in monorepos
-  root_dir = require('lspconfig.util').root_pattern('Cargo.toml', 'rust-project.json'),
   settings = {
     ['rust-analyzer'] = {
       checkOnSave = {
