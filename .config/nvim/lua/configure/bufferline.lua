76b3ecc98d2997311b89a35cd44f35bddfec5bd7
Mat Jones <mat@mjones.network>
2022-01-28 07:57:23 -0500

    add bufdelete.nvim to maintain window layouts

--- .config/nvim/lua/configure/bufferline.lua
+++ .config/nvim/lua/configure/bufferline.lua
@@ -1,10 +1,11 @@
 return {
   'akinsho/nvim-bufferline.lua',
+  requires = { 'famiu/bufdelete.nvim' },
   config = function()
     require('bufferline').setup({
       options = {
         max_name_length = 24,
-        close_command = 'bw! %d',
+        close_command = 'Bdelete %d',
       },
     })
   end,
