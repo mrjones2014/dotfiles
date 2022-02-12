1348f5fa10fff921fea1ada827aa8cf6bfe2441e
Mat Jones <mat@mjones.network>
2022-02-12 14:20:33 -0500

    close neotree if last window

--- .config/nvim/lua/configure/neotree.lua
+++ .config/nvim/lua/configure/neotree.lua
@@ -3,6 +3,7 @@ return {
   requires = { 'MunifTanjim/nui.nvim' },
   config = function()
     require('neo-tree').setup({
+      close_if_last_window = true,
       popup_border_style = 'rounded',
       enable_git_status = true,
       enable_diagnostics = true,
