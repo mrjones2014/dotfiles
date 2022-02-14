26fb652fb6216fee5717dc95ae964abd0b8f718d
Mat Jones <mat@mjones.network>
2022-02-14 10:53:35 -0500

    move treesitter incremental selection keymaps to keymap/init.lua

--- .config/nvim/lua/configure/treesitter.lua
+++ .config/nvim/lua/configure/treesitter.lua
@@ -15,12 +15,8 @@ return {
       },
       incremental_selection = {
         enable = true,
-        keymaps = {
-          init_selection = 'gnn',
-          node_incremental = 'grn',
-          scope_incremental = 'grc',
-          node_decremental = 'grm',
-        },
+        -- keymaps defined in keymaps/init.lua
+        keymaps = {},
       },
       rainbow = {
         enable = true,
