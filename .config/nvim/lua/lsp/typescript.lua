a704daadce0c906d09394a91996d0f6f9cba37e7
Mat Jones <mat@mjones.network>
2022-02-14 08:20:12 -0500

    make tsserver prioritize pnpm workspace roots as root_dir

--- .config/nvim/lua/lsp/typescript.lua
+++ .config/nvim/lua/lsp/typescript.lua
@@ -1,4 +1,16 @@
+local root_dir = require('lspconfig.util').root_pattern('.git', 'pnpm-workspace.yaml', 'package.json')
+-- prioritize workspace roots
+if
+  vim.fn.filereadable(vim.loop.cwd() .. '/pnpm-workspace.yaml') > 0
+  or vim.fn.filereadable(vim.loop.cwd() .. '/pnpm-workspace.yml') > 0
+then
+  root_dir = function()
+    return vim.loop.cwd()
+  end
+end
+
 require('lspconfig').tsserver.setup({
   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
   on_attach = require('lsp.utils').on_attach,
+  root_dir = root_dir,
 })
