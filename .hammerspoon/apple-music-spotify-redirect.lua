aab185b00b37c05b2867c2d7e9a0b7584b24ec5b
Mat Jones <mat@mjones.network>
2022-02-24 09:01:42 -0500

    auto-reload hammerspoon config and split into modules

---
+++ .hammerspoon/apple-music-spotify-redirect.lua
@@ -0,0 +1,12 @@
+-- no, Apple, I didn't mean to open Apple Music instead of Spotify
+local function open_spotify_instead_of_apple_music(app_name, event_type, app_obj)
+  if event_type == hs.application.watcher.launched and app_name == 'Music' then
+    -- kill Apple Music
+    app_obj:kill()
+    -- open Spotify instead
+    hs.application.open('Spotify')
+  end
+end
+
+local apple_music_spotify_redirector = hs.application.watcher.new(open_spotify_instead_of_apple_music)
+apple_music_spotify_redirector:start()
