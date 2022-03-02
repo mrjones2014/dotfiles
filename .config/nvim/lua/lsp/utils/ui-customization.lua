65872dbb3ea66cc015ad5bce270d7a41ae66db1c
Mat Jones <mat@mjones.network>
2022-03-02 09:55:17 -0500

    show eslint rule in message for diagnostics from eslint

--- .config/nvim/lua/lsp/utils/ui-customization.lua
+++ .config/nvim/lua/lsp/utils/ui-customization.lua
@@ -7,8 +7,21 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(legacy_highlight, { text = icon, texthl = legacy_highlight, numhl = legacy_highlight })
 end
 
+local function diagnostic_format(diagnostic)
+  if diagnostic.source == 'eslint' or diagnostic.source == 'eslint_d' then
+    return string.format('%s (%s)', diagnostic.message, diagnostic.code)
+  end
+
+  return diagnostic.message
+end
+
 vim.diagnostic.config({
-  virtual_text = true,
+  virtual_text = {
+    format = diagnostic_format,
+  },
+  float = {
+    format = diagnostic_format,
+  },
   signs = true,
   underline = true,
   update_in_insert = false,
