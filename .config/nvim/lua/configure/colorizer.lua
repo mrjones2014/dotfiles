bfd9313ce39e25cce48f550427ac53e874e60c07
Mat Jones <mat@mjones.network>
2022-03-10 07:52:11 -0500

    lazy load lsp configs

--- .config/nvim/lua/configure/colorizer.lua
+++ .config/nvim/lua/configure/colorizer.lua
@@ -1,14 +1,17 @@
+local filetypes = {
+  'css',
+  'scss',
+  'javascript',
+  'javascriptreact',
+  'typescript',
+  'typescriptreact',
+  'html',
+}
 return {
   'norcalli/nvim-colorizer.lua',
+  ft = filetypes,
+  cmd = 'ColorizerAttachToBuffer',
   config = function()
-    require('colorizer').setup({
-      'css',
-      'scss',
-      'javascript',
-      'javascriptreact',
-      'typescript',
-      'typescriptreact',
-      'html',
-    })
+    require('colorizer').setup(filetypes)
   end,
 }
