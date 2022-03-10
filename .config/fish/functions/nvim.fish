9d1ae80459db948577e713d0713d873fbf19a0e6
Mat Jones <mat@mjones.network>
2022-03-10 07:57:25 -0500

    move lua lsp version detection back to nvim config now that it's being lazy loaded

--- .config/fish/functions/nvim.fish
+++ .config/fish/functions/nvim.fish
@@ -1,16 +1,15 @@
 function nvim
     set -l NVIM (which nvim)
-    set -l LUA_LSP_VERSION (readlink (which lua-language-server) | grep -o "[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+")
 
     if test (count $argv) -lt 1
-        LUA_LSP_VERSION="$LUA_LSP_VERSION" "$NVIM" --startuptime /tmp/nvim-startuptime
+        "$NVIM" --startuptime /tmp/nvim-startuptime
         return
     end
 
     if test -d $argv[1]
-        pushd $argv[1] && LUA_LSP_VERSION="$LUA_LSP_VERSION" "$NVIM" --startuptime /tmp/nvim-startuptime && popd
+        pushd $argv[1] && "$NVIM" --startuptime /tmp/nvim-startuptime && popd
         return
     end
 
-    LUA_LSP_VERSION="$LUA_LSP_VERSION" "$NVIM" --startuptime /tmp/nvim-startuptime $argv
+    "$NVIM" --startuptime /tmp/nvim-startuptime $argv
 end
