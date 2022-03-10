164e7e8da3b1e4359c3fb1935679635ed1c1a762
Mat Jones <mat@mjones.network>
2022-03-10 07:24:11 -0500

    just ignore the whole js/ dir for rust-analyzer

--- .config/nvim/lua/lsp/rust.lua
+++ .config/nvim/lua/lsp/rust.lua
@@ -1,23 +1,3 @@
--- if we're in a pnpm workspace, ignore all node_modules
-local utils = require('utils')
-local ignored_dirs = {}
-local pnpm_workspace_config = ''
-
-if vim.fn.filereadable(vim.loop.cwd() .. '/pnpm-workspace.yaml') > 0 then
-  pnpm_workspace_config = vim.fn.system({ 'cat', 'pnpm-workspace.yaml' })
-elseif vim.fn.filereadable(vim.loop.cwd() .. '/pnpm-workspace.yml') > 0 then
-  pnpm_workspace_config = vim.fn.system({ 'cat', 'pnpm-workspace.yaml' })
-end
-
-if #pnpm_workspace_config > 0 then
-  for _, s in pairs(utils.split_to_lines(pnpm_workspace_config)) do
-    local trimmed = utils.trim_str(s)
-    if trimmed:sub(1, 1) == '-' and not string.find(trimmed, '*') then
-      table.insert(ignored_dirs, string.format('./%s/node_modules', utils.trim_str(trimmed:sub(2)):gsub('"', '')))
-    end
-  end
-end
-
 require('lspconfig').rust_analyzer.setup({
   on_attach = require('lsp.utils').on_attach,
   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
@@ -27,7 +7,7 @@ require('lspconfig').rust_analyzer.setup({
         command = 'clippy',
       },
       files = {
-        excludeDirs = ignored_dirs,
+        excludeDirs = { './js/' },
       },
     },
   },
