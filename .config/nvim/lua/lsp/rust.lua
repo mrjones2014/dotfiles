885916f6dad68011a2548e293d4397f9802b221c
Mat Jones <mat@mjones.network>
2022-03-11 17:13:53 -0500

    ignore node_modules in rust-analyzer

--- .config/nvim/lua/lsp/rust.lua
+++ .config/nvim/lua/lsp/rust.lua
@@ -7,7 +7,7 @@ require('lspconfig').rust_analyzer.setup({
         command = 'clippy',
       },
       files = {
-        excludeDirs = { './js/' },
+        excludeDirs = { './js/', './node_modules/' },
       },
     },
   },
