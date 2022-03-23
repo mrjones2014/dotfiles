9ec7470e3e96d5409ef57412664d8183d6094c5e
Mat Jones <mat@mjones.network>
2022-03-23 09:10:45 -0400

    load bufferline on bufleave

--- .config/nvim/lua/configure/bufferline.lua
+++ .config/nvim/lua/configure/bufferline.lua
@@ -1,6 +1,7 @@
 return {
   'akinsho/nvim-bufferline.lua',
   requires = { 'famiu/bufdelete.nvim' },
+  event = 'BufLeave',
   config = function()
     require('bufferline').setup({
       options = {
