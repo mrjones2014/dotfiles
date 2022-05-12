06b6d1f890b818440443c820f248b8337ba693a6
Mat Jones <mat@mjones.network>
2022-05-12 07:49:19 -0400

    remove nvm

--- .config/fish/functions/update-globals.fish
+++ .config/fish/functions/update-globals.fish
@@ -8,16 +8,7 @@ function update-globals --description "Update brew packages, some cargo packages
 
     # update neovim plugins
     update-nvim-plugins
-
-    # update npm packages in all node versions
-    # save to variable so we can reset to this version after updating packages
-    set -l current_node_version (nvm current)
-    for node_version in (nvm list | sed 's/.*\(v[0-9]*\.[0-9]*\.[0-9]*\).*/\1/')
-        nvm use "$node_version"
-        npm update -g
-    end
-    nvm use "$current_node_version"
-
+    pnpm update -g
     pushd ~/.config/hammerspoon/Spoons/VimMode.spoon/ && git pull && popd
 
     # update homebrew packages
