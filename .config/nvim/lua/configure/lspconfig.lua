eabb72ba3fc885d6ea08abd68d133da33c849252
Mat Jones <mat@mjones.network>
2022-07-26 08:59:40 -0400

    remove lsp_lines.nvim

--- .config/nvim/lua/configure/lspconfig.lua
+++ .config/nvim/lua/configure/lspconfig.lua
@@ -2,7 +2,6 @@ return {
   'neovim/nvim-lspconfig',
   requires = {
     'jose-elias-alvarez/null-ls.nvim',
-    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   },
   config = function()
     require('lsp')
--- .config/nvim/lua/lsp/utils/ui-customization.lua
+++ .config/nvim/lua/lsp/utils/ui-customization.lua
@@ -36,5 +36,3 @@ vim.diagnostic.config({
 
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
-
-require('lsp_lines').setup()
