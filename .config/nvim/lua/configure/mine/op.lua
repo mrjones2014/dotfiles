b30ad11f146fd1b12592023993dfb0c4404631e7
Mat Jones <mat@mjones.network>
2022-08-03 14:30:21 -0400

    add op.nvim

---
+++ .config/nvim/lua/configure/mine/op.lua
@@ -0,0 +1,15 @@
+-- use local theme repo if it's cloned
+local paths = require('paths')
+local plugin_path = 'mrjones2014/op.nvim'
+if vim.fn.isdirectory(paths.join(paths.home, 'git/personal/op.nvim')) > 0 then
+  plugin_path = '~/git/personal/op.nvim'
+end
+
+return {
+  plugin_path,
+  config = function()
+    require('op').setup({
+      account_uuid = '3UBYV6PWJZAS7HTEKHDSQ7HPUA',
+    })
+  end,
+}
