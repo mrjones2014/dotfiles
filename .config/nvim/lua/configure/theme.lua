4397f13e32fc08be41eb75236a87d5b78cafd933
Mat Jones <mat@mjones.network>
2022-08-10 11:29:50 -0400

    tweak colors

--- .config/nvim/lua/configure/theme.lua
+++ .config/nvim/lua/configure/theme.lua
@@ -3,15 +3,18 @@ return {
   config = function()
     local onedarkpro = require('onedarkpro')
     local utils = require('onedarkpro.utils')
+    local gray = utils.lighten('#000000', 0.1, '#1b1b1b')
     onedarkpro.setup({
       dark_theme = 'onedark_vivid',
       colors = {
         bg = '#000000',
-        color_column = utils.lighten('#000000', 0.1, '#1b1b1b'),
+        color_column = gray,
         telescope_prompt = utils.lighten('#000000', 0.01, '#101010'),
         telescope_results = '#000000',
       },
       hlgroups = {
+        LineNr = gray,
+        SignColumn = gray,
         TelescopeBorder = {
           fg = '${telescope_results}',
           bg = '${telescope_results}',
