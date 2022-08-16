e813e57414cec17cb877f44bae030d2d72b718c3
Mat Jones <mat@mjones.network>
2022-08-16 10:02:31 -0400

    silence stderr for exit wrapper

--- .config/fish/functions/exit.fish
+++ .config/fish/functions/exit.fish
@@ -1,5 +1,5 @@
 function exit
-    if test -n "$TMUX" && test -n "$TMUX_PANE" && test -n (tmux list-sessions | grep "float-$TMUX_PANE")
+    if test -n "$TMUX" && test -n "$TMUX_PANE" && test -n (tmux list-sessions 2> /dev/null | grep "float-$TMUX_PANE")
         tmux kill-session -t "float-$TMUX_PANE" 2>/dev/null 1>&2
     end
 
