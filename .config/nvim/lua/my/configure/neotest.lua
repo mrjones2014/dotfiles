98e3d349484d1abd28f6829dbdcfb85fa03f20e6
Mat Jones <mat@mjones.network>
2022-08-17 15:06:34 -0400

    add command to open neotest summary

--- .config/nvim/lua/my/configure/neotest.lua
+++ .config/nvim/lua/my/configure/neotest.lua
@@ -8,6 +8,20 @@ return {
   config = function()
     require('neotest').setup({
       discovery = { enabled = false },
+      diagnostic = { enabled = true },
+      icons = {
+        expanded = '',
+        child_prefix = '',
+        child_indent = '',
+        final_child_prefix = '',
+        non_collapsible = '',
+        collapsed = '',
+
+        passed = '',
+        running = '',
+        failed = '',
+        unknown = '',
+      },
       adapters = {
         require('neotest-plenary'),
         require('neotest-rust'),
