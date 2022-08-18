79f57eece8f6027d3a74a657c5890167b5a1c6c3
Mat Jones <mat@mjones.network>
2022-08-18 09:11:42 -0400

    silence stderr for _ctrlg_get_related_panes

--- .config/fish/functions/_ctrlg_get_related_panes.fish
+++ .config/fish/functions/_ctrlg_get_related_panes.fish
@@ -1,5 +1,5 @@
 function _ctrlg_get_related_panes
-    if tmux has-session -t "float-$TMUX_PANE"
+    if tmux has-session -t "float-$TMUX_PANE" 2>/dev/null
         echo "float-$TMUX_PANE"
     end
 end
