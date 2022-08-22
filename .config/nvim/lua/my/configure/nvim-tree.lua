e8f0595a4caa4030df6daccb3afc758d4ae5ea6e
Mat Jones <mat@mjones.network>
2022-08-22 14:26:04 -0400

    Update nvim-tree

--- .config/nvim/lua/my/configure/nvim-tree.lua
+++ .config/nvim/lua/my/configure/nvim-tree.lua
@@ -11,67 +11,121 @@ return {
     'NvimTreeResize',
     'NvimTreeToggle',
   },
-  -- nvim-tree keeps releasing breaking changes and it's pissing me off,
-  -- so just stop updating nvim-tree
-  commit = '9563a11',
-  setup = function()
-    -- TODO update these once they're implemented in the setup function
-    vim.g.nvim_tree_git_hl = 1
-    vim.g.nvim_tree_add_trailing = 1
-    vim.g.nvim_tree_auto_ignore_ft = { 'TelescopePrompt', 'term', 'terminal' }
-    vim.g.nvim_tree_special_files = {}
-
-    local lsp_icons = require('my.lsp.icons')
-    vim.g.nvim_tree_icons = {
-      lsp = {
-        hint = lsp_icons.Hint,
-        info = lsp_icons.Information,
-        warning = lsp_icons.Warning,
-        error = lsp_icons.Error,
-      },
-    }
-  end,
   config = function()
     local icons = require('my.lsp.icons')
-    local tree_cb = require('nvim-tree.config').nvim_tree_callback
     require('nvim-tree').setup({
-      renderer = { indent_markers = { enable = true } },
       filters = {
-        custom = { '.git', 'node_modules', '.cache', '.DS_Store', '.netrwhist', 'dist', 'packer_compiled.lua' },
+        dotfiles = false,
       },
-      git = {
-        ignore = true,
-      },
-      view = {
-        mappings = {
-          custom_only = true,
-          list = {
-            { key = { '<CR>', 'o', '<2-LeftMouse>' }, cb = tree_cb('edit') },
-            { key = '<C-v>', cb = tree_cb('vsplit') },
-            { key = 'R', cb = tree_cb('refresh') },
-            { key = 'a', cb = tree_cb('create') },
-            { key = 'd', cb = tree_cb('remove') },
-            { key = 'r', cb = tree_cb('rename') },
-            { key = '.', cb = tree_cb('cd') },
-          },
-        },
-        side = 'right',
-        width = 40,
-        auto_resize = false,
-      },
+      disable_netrw = true,
+      hijack_netrw = true,
+      open_on_setup = false,
+      ignore_ft_on_setup = { 'alpha' },
+      hijack_cursor = true,
+      hijack_unnamed_buffer_when_opening = false,
+      update_cwd = true,
       update_focused_file = {
         enable = true,
         update_cwd = false,
       },
-      diagnostics = {
+      view = {
+        adaptive_size = true,
+        side = 'right',
+        width = 25,
+        hide_root_folder = true,
+      },
+      git = {
+        enable = false,
+        ignore = true,
+      },
+      filesystem_watchers = {
         enable = true,
+      },
+      actions = {
+        open_file = {
+          resize_window = true,
+        },
+      },
+      renderer = {
+        highlight_git = false,
+        highlight_opened_files = 'none',
+
+        indent_markers = {
+          enable = false,
+        },
+
         icons = {
-          hint = icons.Hint,
-          info = icons.Info,
-          warning = icons.Warning,
-          error = icons.Error,
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = false,
+          },
+
+          -- glyphs = {
+          --   default = '',
+          --   symlink = '',
+          --   folder = {
+          --     default = '',
+          --     empty = '',
+          --     empty_open = '',
+          --     open = '',
+          --     symlink = '',
+          --     symlink_open = '',
+          --     arrow_open = '',
+          --     arrow_closed = '',
+          --   },
+          --   git = {
+          --     unstaged = '✗',
+          --     staged = '✓',
+          --     unmerged = '',
+          --     renamed = '➜',
+          --     untracked = '★',
+          --     deleted = '',
+          --     ignored = '◌',
+          --   },
+          -- },
         },
       },
     })
+    -- require('nvim-tree').setup({
+    --   renderer = { indent_markers = { enable = true } },
+    --   filters = {
+    --     custom = { '.git', 'node_modules', '.cache', '.DS_Store', '.netrwhist', 'dist', 'packer_compiled.lua' },
+    --   },
+    --   git = {
+    --     ignore = true,
+    --   },
+    --   view = {
+    --     mappings = {
+    --       custom_only = true,
+    --       list = {
+    --         { key = { '<CR>', 'o', '<2-LeftMouse>' }, cb = tree_cb('edit') },
+    --         { key = '<C-v>', cb = tree_cb('vsplit') },
+    --         { key = 'R', cb = tree_cb('refresh') },
+    --         { key = 'a', cb = tree_cb('create') },
+    --         { key = 'd', cb = tree_cb('remove') },
+    --         { key = 'r', cb = tree_cb('rename') },
+    --         { key = '.', cb = tree_cb('cd') },
+    --       },
+    --     },
+    --     side = 'right',
+    --     width = 40,
+    --     auto_resize = false,
+    --   },
+    --   update_focused_file = {
+    --     enable = true,
+    --     update_cwd = false,
+    --   },
+    --   diagnostics = {
+    --     enable = true,
+    --     icons = {
+    --       hint = icons.Hint,
+    --       info = icons.Info,
+    --       warning = icons.Warning,
+    --       error = icons.Error,
+    --     },
+    --   },
+    -- })
   end,
 }
