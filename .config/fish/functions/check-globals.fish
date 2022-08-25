6bf97d5e860e646c01b1ac503de760a816ebea90
Mat Jones <mat@mjones.network>
2022-08-25 09:28:01 -0400

    add lazygit to globals

--- .config/fish/functions/check-globals.fish
+++ .config/fish/functions/check-globals.fish
@@ -47,6 +47,10 @@ function check-globals --description "Check for missing command line tools and p
         echo "Install pnpm: brew install pnpm"
     end
 
+    if ! type lazygit &>/dev/null
+        echo "Install lazygit: brew install lazygit"
+    end
+
     if ! type cargo &>/dev/null
         echo "Install rustup: curl --proto \"=https\" --tlsv1.2 -sSf https://sh.rustup.rs | sh"
     end
