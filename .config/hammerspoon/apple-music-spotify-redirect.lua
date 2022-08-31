9430a43a49ecf7d0ab2f6a6fa603fce9534bf012
Mat Jones <mat@mjones.network>
2022-08-31 14:20:11 -0400

    make hammerspoon 'apple-music-spotify-redirect.lua' work better

--- .config/hammerspoon/apple-music-spotify-redirect.lua
+++ .config/hammerspoon/apple-music-spotify-redirect.lua
@@ -1,3 +1,8 @@
+local toggle_spotify_playpause = [[
+tell application "Spotify"
+  playpause
+end tell
+]]
 -- no, Apple, I didn't mean to open Apple Music instead of Spotify
 local function open_spotify_instead_of_apple_music(app_name, event_type, app_obj)
   if event_type == hs.application.watcher.launched and app_name == 'Music' then
@@ -5,6 +10,7 @@ local function open_spotify_instead_of_apple_music(app_name, event_type, app_obj
     app_obj:kill()
     -- open Spotify instead
     hs.application.open('Spotify')
+    hs.osascript.applescript(toggle_spotify_playpause)
   end
 end
 
