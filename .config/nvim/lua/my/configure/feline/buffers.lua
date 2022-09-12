ee069353aa075306a86cfd51a9b7f74e548d5057
Mat Jones <mat@mjones.network>
2022-09-12 18:30:50 -0400

    add ignored filetypes for bufline

--- .config/nvim/lua/my/configure/feline/buffers.lua
+++ .config/nvim/lua/my/configure/feline/buffers.lua
@@ -1,3 +1,13 @@
+local ignored_ft = {
+  'nofile',
+  'TelescopePrompt',
+  'NvimTree',
+  'quickfix',
+  'qf',
+  'Trouble',
+  '1PasswordSidebar',
+}
+
 -- Get the names of all current listed buffers
 local function get_current_filenames()
   local listed_buffers = vim.tbl_filter(function(bufnr)
@@ -59,6 +69,7 @@ local function buf_with_icon(buf)
   local icon, hl = require('nvim-web-devicons').get_icon(vim.api.nvim_buf_get_name(buf), nil, { default = true })
 
   return {
+    id = buf,
     name = get_unique_filename(vim.api.nvim_buf_get_name(buf)),
     icon = icon,
     icon_color = hl,
@@ -70,6 +81,7 @@ local function get_buffers()
   local listed_bufs = vim.tbl_filter(function(buf)
     return vim.bo[buf].buflisted and vim.api.nvim_buf_is_loaded(buf)
   end, vim.api.nvim_list_bufs())
+
   return vim.tbl_map(function(buf)
     return buf_with_icon(buf)
   end, listed_bufs)
@@ -77,11 +89,20 @@ end
 
 vim.cmd.hi('BufLineActive guifg=#abb2bf guibg=#000000 gui=bold,italic')
 vim.cmd.hi('BufLineInactive guifg=#434852 guibg=#000000')
+vim.cmd.hi('BufLineBg guibg=#0D0D0D')
 
 return function()
+  if
+    vim.tbl_contains(
+      ignored_ft,
+      vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(tonumber(vim.g.statusline_winid or 0)), 'filetype')
+    )
+  then
+    return ''
+  end
   local bufs = get_buffers()
   -- TODO figure out how to ensure current buffer is visible
-  return table.concat(
+  local winbar = table.concat(
     vim.tbl_map(function(buf_icon)
       return string.format(
         '%%#%s#%s %%#%s#%s',
@@ -93,4 +114,6 @@ return function()
     end, bufs),
     ' │ '
   )
+
+  return string.format('%s │%%#BufLineBg#', winbar)
 end
