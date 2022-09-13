96ee08da298d99d55175568fef8d43653fbb25ca
Mat Jones <mat@mjones.network>
2022-09-13 12:06:02 -0400

    lazy load diffview.nvim

--- .config/nvim/lua/my/configure/diffview.lua
+++ .config/nvim/lua/my/configure/diffview.lua
@@ -1,5 +1,14 @@
 return {
   'sindrets/diffview.nvim',
+  cmd = {
+    'DiffviewLog',
+    'DiffviewOpen',
+    'DiffviewClose',
+    'DiffviewRefresh',
+    'DiffviewFocusFiles',
+    'DiffviewFileHistory',
+    'DiffviewToggleFiles',
+  },
   config = function()
     require('diffview').setup({
       enhanced_diff_hl = true,
