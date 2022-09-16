2fed211f9ed60d7fd4988ff97c5180d1a24052ba
Mat Jones <mat@mjones.network>
2022-09-16 10:04:25 -0400

    use my fork of barbar.nvim for now

---
+++ .config/nvim/lua/my/configure/barbar.lua
@@ -0,0 +1,14 @@
+-- use local while my PR is open
+-- use local theme repo if it's cloned
+local paths = require('my.paths')
+local plugin_path = 'mrjones2014/barbar.nvim'
+if vim.fn.isdirectory(paths.join(paths.home, 'git/github/op.nvim')) > 0 then
+  plugin_path = '~/git/github/barbar.nvim'
+end
+
+return {
+  plugin_path,
+  config = function()
+    require('bufferline').setup({ use_winbar = true })
+  end,
+}
