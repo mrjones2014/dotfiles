45c1e7a3025ae6a010d15b4a85083a3408d23b25
Mat Jones <mat@mjones.network>
2022-09-30 08:25:42 -0400

    fix lazy loading for goto-preview

--- .config/nvim/lua/my/configure/goto-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'rmagatti/goto-preview',
-  ft = require('my.lsp.filetypes').filetypes,
-  config = function()
-    require('goto-preview').setup({
-      border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
-    })
-  end,
-}
