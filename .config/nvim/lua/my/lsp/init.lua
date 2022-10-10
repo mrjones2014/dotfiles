90e986198a9804d54ab8822fd2d923a0795b57d1
Mat Jones <mat@mjones.network>
2022-10-10 11:14:08 -0400

    use regular autocmds to speed up loading lspconfig

--- .config/nvim/lua/my/lsp/init.lua
+++ .config/nvim/lua/my/lsp/init.lua
@@ -4,20 +4,16 @@ require('my.lsp.null-ls')
 -- lazy-load the rest of the configs with
 -- an autocommand that runs only once
 -- for each lsp config
-local legendary = require('legendary')
 for filetype, file_patterns in pairs(require('my.lsp.filetypes').filetype_patterns) do
-  legendary.bind_autocmds({
-    'BufReadPre',
-    function()
+  vim.api.nvim_create_autocmd('BufReadPre', {
+    callback = function()
       require('my.lsp.' .. filetype)
       local snippets = require('my.lsp.snippets')[filetype]
       if snippets then
         snippets()
       end
     end,
-    opts = {
-      pattern = file_patterns,
-      once = true,
-    },
+    pattern = file_patterns,
+    once = true,
   })
 end
