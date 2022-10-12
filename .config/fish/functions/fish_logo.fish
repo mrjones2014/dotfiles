74f67abd94202b37851cc7b1e3e6622cf594ffc7
Mat Jones <mat@mjones.network>
2022-10-12 13:43:40 -0400

    update git ch alias to correct master/main based on current repo's default branch

--- .config/fish/functions/fish_logo.fish
+++ .config/fish/functions/fish_logo.fish
@@ -1,46 +1,17 @@
-function fish_logo \
-    --description="Fish-shell colorful ASCII-art logo" \
-    --argument-names outer_color medium_color inner_color mouth eye
-
-    # defaults:
-    [ $outer_color ]; or set outer_color red
-    [ $medium_color ]; or set medium_color f70
-    [ $inner_color ]; or set inner_color yellow
-    [ $mouth ]; or set mouth '['
-    [ $eye ]; or set eye O
-
-    set usage 'Usage: fish_logo <outer_color> <medium_color> <inner_color> <mouth> <eye>
-See set_color --help for more on available colors.'
-
-    if contains -- $outer_color --help -h -help
-        echo $usage
-        return 0
-    end
-
-    # shortcuts:
-    set o (set_color $outer_color)
-    set m (set_color $medium_color)
-    set i (set_color $inner_color)
-
-    if test (count $o) != 1; or test (count $m) != 1; or test (count $i) != 1
-        echo 'Invalid color argument'
-        echo $usage
-        return 1
-    end
-
-    echo '                  '$o'___
-   ___======____='$m'-'$i'-'$m'-='$o')
- /T            \_'$i'--='$m'=='$o')
- '$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')
-  \      / )J'$m'~~    '$o'\\'$i'-='$o')
-   \\\\___/  )JJ'$m'~'$i'~~   '$o'\)
-    \_____/JJJ'$m'~~'$i'~~    '$o'\\
-    '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\
-   (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_
-   '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__
-    '$o'\V    '$m'\\\\'$o'\) =='$m'=_____   '$i'\\\\\\\\'$m'\\\\
-           '$o'\V)     \_) '$m'\\\\'$i'\\\\JJ\\'$m'J\)
-                       '$o'/'$m'J'$i'\\'$m'J'$o'T\\'$m'JJJ'$o'J)
-                       (J'$m'JJ'$o'| \UUU)
-                        (UU)'(set_color normal)
+function fish_logo
+    echo '                 '(set_color F00)'___
+  ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
+/T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')
+[ \ '(set_color FF7F00)'('(set_color FF0)'0'(set_color FF7F00)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color FF7F00)'='(set_color F00)')   HERE COME
+ \      / )J'(set_color FF7F00)'~~    \\'(set_color FF0)'-='(set_color F00)')   DAT FISH
+  \\\\___/  )JJ'(set_color FF7F00)'~'(set_color FF0)'~~   '(set_color F00)'\)
+   \_____/JJJ'(set_color FF7F00)'~~'(set_color FF0)'~~    '(set_color F00)'\\
+   '(set_color FF7F00)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color FF7F00)'~~~'(set_color FF0)'~~     '(set_color FF7F00)'\\
+  (-'(set_color FF0)'\)'(set_color F00)'\='(set_color FF7F00)'|'(set_color FF0)'\\\\\\'(set_color FF7F00)'~~'(set_color FF0)'~~       '(set_color FF7F00)'L_'(set_color FF0)'_
+  '(set_color FF7F00)'('(set_color F00)'\\'(set_color FF7F00)'\\)  ('(set_color FF0)'\\'(set_color FF7F00)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color FF7F00)'__
+   '(set_color F00)'\V    '(set_color FF7F00)'\\\\'(set_color F00)'\) =='(set_color FF7F00)'=_____   '(set_color FF0)'\\\\\\\\'(set_color FF7F00)'\\\\
+          '(set_color F00)'\V)     \_) '(set_color FF7F00)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color FF7F00)'J\)
+  O SHIT              '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
+  WADDUP              (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
+                       (UU)'(set_color normal)
 end
--- scripts/git-ch.sh
+++ scripts/git-ch.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 if test "$#" -ne 0; then
-  git checkout "$@"
+  if [[ "$*" = "master" ]] || [[ "$*" = "main" ]]; then
+    git checkout "$(git branch --format '%(refname:short)' --sort=-committerdate --list master main | head -n1)"
+  else
+    git checkout "$@"
+  fi
 else
   git branch -a --format="%(refname:short)" | sed 's|origin/||g' | grep -v "HEAD" | sort | uniq | fzf-tmux -p -x 15 | xargs git checkout
 fi
