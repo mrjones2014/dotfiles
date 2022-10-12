4e3f57290976075cc4f3143a5e440500f5b2fa46
Mat Jones <mat@mjones.network>
2022-10-12 14:39:55 -0400

    add emptytrash.fish function

---
+++ .config/fish/functions/emptytrash.fish
@@ -0,0 +1,10 @@
+# source: https://github.com/rkalis/dotfiles/blob/master/fish/functions/emptytrash.fish
+# Empty the Trash on all mounted volumes and the main HDD.
+# Also, clear Apple’s System Logs to improve shell startup speed.
+# Finally, clear download history from quarantine. https://mths.be/bum
+function emptytrash
+    sudo rm -rfv /Volumes/*/.Trashes
+    sudo rm -rfv ~/.Trash
+    sudo rm -rfv /private/var/log/asl/*.asl
+    sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'
+end
