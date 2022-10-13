39f5af415a7dcb4f53e899e3e8c64bdb1f468f19
Mat Jones <mat@mjones.network>
2022-10-13 15:00:56 -0400

    enable tmux integration on smart-splits.nvim

--- .config/nvim/lua/my/configure/mine/smart-splits.lua
+++ .config/nvim/lua/my/configure/mine/smart-splits.lua
@@ -1,1 +1,8 @@
-return { localplugin('mrjones2014/smart-splits.nvim') }
+return {
+  localplugin('mrjones2014/smart-splits.nvim'),
+  config = function()
+    require('smart-splits').setup({
+      tmux_integration = true,
+    })
+  end,
+}
