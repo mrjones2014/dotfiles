9ac7309dd6aec0d46fb510a6073251fc1b3b7af4
Mat Jones <mat@mjones.network>
2022-10-13 08:18:29 -0400

    remove applescript ft from comment.nvim since it is now in core

--- .config/nvim/lua/my/configure/comments.lua
+++ .config/nvim/lua/my/configure/comments.lua
@@ -30,8 +30,5 @@ return {
     require('Comment').setup({
       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
     })
-
-    local ft = require('Comment.ft')
-    ft.set('applescript', '#%s')
   end,
 }
