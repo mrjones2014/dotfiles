080d25f5875ea867573c80216c8a1282023e621a
Mat Jones <mat@mjones.network>
2022-10-13 07:27:34 -0400

    switch to comment.nvim

--- .config/nvim/lua/my/configure/comments.lua
+++ .config/nvim/lua/my/configure/comments.lua
@@ -1,10 +1,37 @@
 return {
-  'terrortylor/nvim-comment',
-  cmd = 'CommentToggle',
+  'numToStr/Comment.nvim',
+  after = 'nvim-treesitter',
   config = function()
-    require('nvim_comment').setup({
-      comment_empty = false,
-      create_mappings = false,
+    -- default mappings:
+    -- {
+    --   toggler = {
+    --     ---Line-comment toggle keymap
+    --     line = 'gcc',
+    --     ---Block-comment toggle keymap
+    --     block = 'gbc',
+    --   },
+    --   ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+    --   opleader = {
+    --       ---Line-comment keymap
+    --       line = 'gc',
+    --       ---Block-comment keymap
+    --       block = 'gb',
+    --   },
+    --   ---LHS of extra mappings
+    --   extra = {
+    --       ---Add comment on the line above
+    --       above = 'gcO',
+    --       ---Add comment on the line below
+    --       below = 'gco',
+    --       ---Add comment at the end of line
+    --       eol = 'gcA',
+    --   }
+    -- }
+    require('Comment').setup({
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
     })
+
+    local ft = require('Comment.ft')
+    ft.set('applescript', '#%s')
   end,
 }
