4d2fef9f4fe2b8eb2440be3836ad684cda843145
Mat Jones <mat@mjones.network>
2022-10-17 07:40:50 -0400

    remove redundant settings

--- .config/nvim/lua/my/lsp/typescript.lua
+++ .config/nvim/lua/my/lsp/typescript.lua
@@ -10,7 +10,5 @@ then
 end
 
 return {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  on_attach = require('my.lsp.utils').on_attach,
   root_dir = root_dir,
 }
