4a1b19eb2e5a7609820ffe3eca84ccad83602730
Mat Jones <mat@mjones.network>
2022-10-18 11:37:20 -0400

    refactor my.lsp.filetypes.lua

--- .config/nvim/lua/my/configure/lspconfig.lua
+++ .config/nvim/lua/my/configure/lspconfig.lua
@@ -12,29 +12,9 @@ return {
     require('mason').setup()
     require('mason-tool-installer').setup({
       auto_update = true,
-      ensure_installed = {
-        -- LSP servers
-        'css-lsp',
-        'html-lsp',
-        'json-lsp',
-        'typescript-language-server',
-        'rust-analyzer',
-        'svelte-language-server',
-        'gopls',
-        'lua-language-server',
-        'teal-language-server',
-
-        -- linters
-        'eslint_d',
-        'shellcheck',
-        'luacheck',
-        'codespell',
-
-        -- formatters
-        'prettierd',
-        'stylua',
-        'shfmt',
-      },
+      ensure_installed = vim.tbl_map(function(config)
+        return config.mason
+      end, require('my.lsp.filetypes').config),
     })
     require('my.lsp').setup()
   end,
--- .config/nvim/lua/my/lsp/filetypes.lua
+++ .config/nvim/lua/my/lsp/filetypes.lua
@@ -1,30 +1,53 @@
 local M = {}
 
-M.filetype_patterns = {
-  ['css'] = { '*.css', '*.scss' },
-  ['html'] = { '*.html' },
-  ['json'] = { '*.json', '*.jsonc' },
-  ['typescript'] = { '*.ts', '*.tsx', '*.js', '*.jsx' },
-  ['lua'] = { '*.lua' },
-  ['rust'] = { '*.rs' },
-  ['svelte'] = { '*.svelte' },
-  ['teal'] = { '*.tl', '*.d.tl' },
-  ['go'] = { '*.go', 'go.mod' },
+M.config = {
+  ['css'] = {
+    patterns = { '*.css', '*.scss' },
+    lspconfig = 'cssls',
+    mason = 'css-lsp',
+  },
+  ['html'] = {
+    patterns = { '*.html' },
+    lspconfig = 'html',
+    mason = 'html-lsp',
+  },
+  ['json'] = {
+    patterns = { '*.json', '*.jsonc' },
+    lspconfig = 'jsonls',
+    mason = 'json-lsp',
+  },
+  ['typescript'] = {
+    patterns = { '*.ts', '*.tsx', '*.js', '*.jsx' },
+    lspconfig = 'tsserver',
+    mason = 'typescript-language-server',
+  },
+  ['lua'] = {
+    patterns = { '*.lua' },
+    lspconfig = 'sumneko_lua',
+    mason = 'lua-language-server',
+  },
+  ['rust'] = {
+    patterns = { '*.rs' },
+    lspconfig = 'rust_analyzer',
+    mason = 'rust-analyzer',
+  },
+  ['svelte'] = {
+    patterns = { '*.svelte' },
+    lspconfig = 'svelte',
+    mason = 'svelte-language-server',
+  },
+  ['teal'] = {
+    patterns = { '*.tl', '*.d.tl' },
+    lspconfig = 'teal_ls',
+    mason = 'teal-language-server',
+  },
+  ['go'] = {
+    patterns = { '*.go', 'go.mod' },
+    lspconfig = 'gopls',
+    mason = 'gopls',
+  },
 }
 
-M.filetypes = vim.tbl_keys(M.filetype_patterns)
-
-M.servers = {
-  ['csharp'] = 'csharp_ls',
-  ['css'] = 'cssls',
-  ['go'] = 'gopls',
-  ['html'] = 'html',
-  ['json'] = 'jsonls',
-  ['lua'] = 'sumneko_lua',
-  ['rust'] = 'rust_analyzer',
-  ['svelte'] = 'svelte',
-  ['teal'] = 'teal_ls',
-  ['typescript'] = 'tsserver',
-}
+M.filetypes = vim.tbl_keys(M.config)
 
 return M
