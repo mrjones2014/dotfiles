5a21febc0c5d47fcdf9e4d8dff963361dcf0e00f
Mat Jones <mat@mjones.network>
2022-10-18 11:37:20 -0400

    refactor my.lsp.filetypes.lua

--- .config/nvim/lua/my/lsp/init.lua
+++ .config/nvim/lua/my/lsp/init.lua
@@ -7,15 +7,15 @@ function M.setup()
   -- lazy-load the rest of the configs with
   -- an autocommand that runs only once
   -- for each lsp config
-  for filetype, file_patterns in pairs(require('my.lsp.filetypes').filetype_patterns) do
+  for filetype, filetype_config in pairs(require('my.lsp.filetypes').config) do
+    local file_patterns = filetype_config.patterns
     vim.api.nvim_create_autocmd('BufReadPre', {
       callback = function()
         local has_config, config = pcall(require, 'my.lsp.' .. filetype)
         config = has_config and config or {}
         config.capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
         config.on_attach = require('my.lsp.utils').on_attach
-        local server = require('my.lsp.filetypes').servers[filetype]
-        require('lspconfig')[server].setup(config)
+        require('lspconfig')[filetype_config.lspconfig].setup(config)
         local snippets = require('my.lsp.snippets')[filetype]
         if snippets then
           snippets()
