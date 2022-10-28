a64c55a8492ac957963018c3668266afeb652ccd
Mat Jones <mat@mjones.network>
2022-10-28 08:09:40 -0400

    add marksman

--- .config/nvim/lua/my/lsp/init.lua
+++ .config/nvim/lua/my/lsp/init.lua
@@ -9,21 +9,23 @@ function M.setup()
   -- for each lsp config
   for filetype, filetype_config in pairs(require('my.lsp.filetypes').config) do
     local file_patterns = filetype_config.patterns
-    vim.api.nvim_create_autocmd('BufReadPre', {
-      callback = function()
-        local has_config, config = pcall(require, 'my.lsp.' .. filetype)
-        config = has_config and config or {}
-        config.capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-        config.on_attach = require('my.lsp.utils').on_attach
-        require('lspconfig')[filetype_config.lspconfig].setup(config)
-        local snippets = require('my.lsp.snippets')[filetype]
-        if snippets then
-          snippets()
-        end
-      end,
-      pattern = file_patterns,
-      once = true,
-    })
+    if file_patterns then
+      vim.api.nvim_create_autocmd('BufReadPre', {
+        callback = function()
+          local has_config, config = pcall(require, 'my.lsp.' .. filetype)
+          config = has_config and config or {}
+          config.capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+          config.on_attach = require('my.lsp.utils').on_attach
+          require('lspconfig')[filetype_config.lspconfig].setup(config)
+          local snippets = require('my.lsp.snippets')[filetype]
+          if snippets then
+            snippets()
+          end
+        end,
+        pattern = file_patterns,
+        once = true,
+      })
+    end
   end
 end
 
