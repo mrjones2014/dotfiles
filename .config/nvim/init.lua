a8433c8084419d6918fd0da5d43be7cb4db6f5cc
Mat Jones <mat@mjones.network>
2022-11-14 14:52:50 -0500

    set colorscheme immediately

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,19 @@
+require('my.globals')
+
 -- impatient has to be loaded before anything else
 local present, impatient = pcall(require, 'impatient')
 if present then
   impatient.enable_profile()
 end
 
+-- force set colorscheme immediately
+pcall(vim.cmd.colorscheme, 'onedarkpro')
+
 require('my.disable-builtins')
-require('my.globals')
 require('my.settings')
 require('my.plugins').setup()
 require('my.whitespace')
+
 if #vim.fn.argv() == 0 then
   require('my.startup').show()
 end
