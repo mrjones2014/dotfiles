b62a94657d13ae8f41d5b1339905c84f2ddffd9e
Mat Jones <mat@mjones.network>
2022-11-22 09:40:45 -0500

    fix gitsigns autocmds

--- .config/nvim/lua/my/globals.lua
+++ .config/nvim/lua/my/globals.lua
@@ -1,25 +1,60 @@
 -- Global helper functions
 
--- utility for debugging lua stuff
-_G.p = function(any)
-  print(vim.inspect(any))
+---Debug Lua stuff and print a nice debug message via `vim.inspect`.
+---@param ... any
+_G.p = function(...)
+  local info = debug.getinfo(2, 'S')
+  local source = info.source:sub(2)
+  source = vim.loop.fs_realpath(source) or source
+  source = vim.fn.fnamemodify(source, ':~:.') .. ':' .. info.linedefined
+  local what = { ... }
+  if vim.tbl_islist(what) and vim.tbl_count(what) <= 1 then
+    what = what[1]
+  end
+  local msg = vim.inspect(vim.deepcopy(what))
+  require('notify').notify(msg, vim.log.levels.INFO, {
+    title = 'Debug: ' .. source,
+    on_open = function(win)
+      vim.wo[win].conceallevel = 3
+      vim.wo[win].concealcursor = ''
+      vim.wo[win].spell = false
+      local buf = vim.api.nvim_win_get_buf(win)
+      vim.treesitter.start(buf, 'lua')
+    end,
+  })
 end
 
+--- A collection of filesystem path related utils
 _G.Path = {
+  ---Join two or more paths together
+  ---@param ... string
+  ---@return string
   join = function(...)
-    return table.concat({ ... }, '/')
+    return vim.fn.simplify(table.concat({ ... }, '/'))
   end,
+
+  ---Get path relative to current working directory,
+  ---replacing `$HOME` with `~` if applicable.
+  ---@param path string
+  ---@return string
   relative = function(path)
     return vim.fn.fnamemodify(path, ':~:.')
   end,
 }
 
+---Utils for copying to clipboard
 _G.Clipboard = {
+  ---Copy string to system clipboard
+  ---@param str string
   copy = function(str)
     vim.fn.jobstart(string.format('echo -n %q | pbcopy', str), { detach = true })
   end,
 }
 
+---Insert multiple items to a table at once.
+---@generic T
+---@param tbl T[]
+---@param ... T
 table.insert_all = function(tbl, ...)
   local items = { ... }
   for _, item in ipairs(items) do
@@ -27,6 +62,9 @@ table.insert_all = function(tbl, ...)
   end
 end
 
+---Join two or more lists together
+---@param ... table
+---@return table
 table.join_lists = function(...)
   local lists = { ... }
   local result = {}
@@ -37,6 +75,12 @@ table.join_lists = function(...)
   return result
 end
 
+---Utility for plugin paths of plugins I work on.
+---Returns `~/git/github/[plugin_name]` with the `username/`
+---part stripped off if that directory exists,
+---and the given string otherwise.
+---@param name any
+---@return string
 _G.localplugin = function(name)
   local local_repo_name = string.gsub(name, '.*/', '')
   local local_path = string.format('%s/git/github/%s', vim.env.HOME, local_repo_name)
