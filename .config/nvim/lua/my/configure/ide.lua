3a58f812ff73a8372e2679dcc4d10fff96d5ee78
Mat Jones <mat@mjones.network>
2022-11-29 08:07:39 -0500

    tweak telescope and nvim-ide config

--- .config/nvim/lua/my/configure/ide.lua
+++ .config/nvim/lua/my/configure/ide.lua
@@ -5,19 +5,14 @@ return {
     local explorer = require('ide.components.explorer')
     local outline = require('ide.components.outline')
     local callhierarchy = require('ide.components.callhierarchy')
-    local timeline = require('ide.components.timeline')
-    local terminal = require('ide.components.terminal')
-    local terminalbrowser = require('ide.components.terminal.terminalbrowser')
-    local changes = require('ide.components.changes')
-    local commits = require('ide.components.commits')
-    local branches = require('ide.components.branches')
     local bookmarks = require('ide.components.bookmarks')
+    local bufferlist = require('ide.components.bufferlist')
     require('ide').setup({
       components = {
         [explorer.Name] = { list_directories_first = true, show_file_permissions = false },
       },
       panel_groups = {
-        explorer = { explorer.Name, outline.Name, bookmarks.Name, callhierarchy.Name, terminalbrowser.Name },
+        explorer = { bufferlist.Name, explorer.Name, outline.Name, callhierarchy.Name },
       },
     })
   end,
