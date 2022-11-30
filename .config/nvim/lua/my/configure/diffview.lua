102810bb6efd3990bc9a9ffb5388edde93b55840
Mat Jones <mat@mjones.network>
2022-11-30 09:40:42 -0500

    show diffview.nvim keymaps in tabline while diffview.nvim is open

--- .config/nvim/lua/my/configure/diffview.lua
+++ .config/nvim/lua/my/configure/diffview.lua
@@ -19,6 +19,19 @@ return {
           },
         },
       },
+      hooks = {
+        view_opened = function()
+          local offset = string.rep(' ', 36)
+          local keymaps_str =
+            '[x = prev conflict, ]x = next conflict, <leader>co = choose ours, <leader>ct = choose theirs, <leader>cb = choose base, <leader>ca = choose all, dx = choose none'
+          vim.opt.tabline = offset .. keymaps_str
+          vim.opt.showtabline = 2
+        end,
+        view_closed = function()
+          vim.opt.tabline = nil
+          vim.opt.showtabline = 0
+        end,
+      },
     })
   end,
 }
