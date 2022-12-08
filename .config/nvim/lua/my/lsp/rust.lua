943ace51a5a459fcfb5c6a9aa6176438485a27bb
Mat Jones <mat@mjones.network>
2022-12-08 08:52:40 -0500

    make rust_analyzer use its own target directory

--- .config/nvim/lua/my/lsp/rust.lua
+++ .config/nvim/lua/my/lsp/rust.lua
@@ -1,4 +1,24 @@
 return {
+  before_init = function(_, config)
+    vim.notify('test')
+    -- Override clippy to run in its own directory to avoid clobbering caches
+    -- but only if target-dir isn't already set in either the command or the extraArgs
+    local checkOnSave = config.settings['rust-analyzer'].checkOnSave
+    local needle = '%-%-target%-dir'
+    if string.find(checkOnSave.command, needle) then
+      return
+    end
+
+    local extraArgs = checkOnSave.extraArgs
+    for _, v in pairs(extraArgs) do
+      if string.find(v, needle) then
+        return
+      end
+    end
+
+    local target_dir = config.root_dir .. '/target/ide-clippy'
+    table.insert(extraArgs, '--target-dir=' .. target_dir)
+  end,
   settings = {
     ['rust-analyzer'] = {
       checkOnSave = {
