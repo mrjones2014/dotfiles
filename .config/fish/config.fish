2c384d7ae7501b0b241ab148dac23ff2575195b6
Mat Jones <mat@mjones.network>
2022-12-14 11:25:57 -0500

    tweak octo config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,6 +35,8 @@ if status is-interactive
     fish_vi_key_bindings
     bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 
+    alias cat="bat"
+
     op completion fish | source
     thefuck --alias | source
     atuin init fish | source
