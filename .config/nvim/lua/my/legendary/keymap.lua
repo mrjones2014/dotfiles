ee44d2ae2102825b6f5bf063fae83dfb02e10d67
Mat Jones <mat@mjones.network>
2023-02-13 10:58:29 -0500

    update smart-splits.nvim keymaps

--- .config/nvim/lua/my/legendary/keymap.lua
+++ .config/nvim/lua/my/legendary/keymap.lua
@@ -150,9 +150,15 @@ function M.default_keymaps()
 
     -- h/j/k/l mappings, split resizing
     { '<A-h>', h.lazy_required_fn('smart-splits', 'resize_left') },
-    { '<A-l>', h.lazy_required_fn('smart-splits', 'resize_right') },
     { '<A-j>', h.lazy_required_fn('smart-splits', 'resize_down') },
     { '<A-k>', h.lazy_required_fn('smart-splits', 'resize_up') },
+    { '<A-l>', h.lazy_required_fn('smart-splits', 'resize_right') },
+
+    -- h/j/k/l mappings, swapping buffers
+    { '<leader><leader>h', h.lazy_required_fn('smart-splits', 'swap_buf_left') },
+    { '<leader><leader>j', h.lazy_required_fn('smart-splits', 'swap_buf_down') },
+    { '<leader><leader>k', h.lazy_required_fn('smart-splits', 'swap_buf_up') },
+    { '<leader><leader>l', h.lazy_required_fn('smart-splits', 'swap_buf_right') },
 
     -- h/j/k/l mappings, text moving
     {
