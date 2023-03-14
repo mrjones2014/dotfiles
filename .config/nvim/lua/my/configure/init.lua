fa139dde6aaa96fbe535fd87fd642acf2b0666c9
Mat Jones <mat@mjones.network>
2023-03-14 10:15:14 -0400

    update vim-eunuch config and remove unused syntax file

--- .config/nvim/lua/my/configure/init.lua
+++ .config/nvim/lua/my/configure/init.lua
@@ -1,6 +1,6 @@
 return {
   { 'nvim-lua/plenary.nvim' },
-  { 'tpope/vim-eunuch', cmd = { 'Delete', 'Move', 'Cmhod', 'SudoWrite' } },
+  { 'tpope/vim-eunuch', cmd = { 'Delete', 'Move', 'Cmhod', 'SudoWrite', 'Rename' } },
   { 'tpope/vim-sleuth', lazy = false },
   {
     'folke/lazy.nvim',
