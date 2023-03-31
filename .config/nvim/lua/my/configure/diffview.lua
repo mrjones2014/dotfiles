5d5e430d7e0f5b2ccae558e4289870cea99c621e
Mat Jones <mat@mjones.network>
2023-03-31 13:14:48 -0400

    use diffview.nvim legendary.nvim extension

--- .config/nvim/lua/my/configure/diffview.lua
+++ .config/nvim/lua/my/configure/diffview.lua
@@ -19,21 +19,6 @@ return {
           },
         },
       },
-      hooks = {
-        view_opened = function()
-          vim.g._diffview_open = true
-          -- local offset = string.rep(' ', 36)
-          -- local keymaps_str =
-          --   '[x = prev conflict, ]x = next conflict, <leader>co = choose ours, <leader>ct = choose theirs, <leader>cb = choose base, <leader>ca = choose all, dx = choose none'
-          -- vim.opt.tabline = offset .. keymaps_str
-          -- vim.opt.showtabline = 2
-        end,
-        view_closed = function()
-          vim.g._diffview_open = false
-          -- vim.opt.tabline = nil
-          -- vim.opt.showtabline = 0
-        end,
-      },
     })
   end,
 }
