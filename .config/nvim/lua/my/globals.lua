76f499f31ca07b15641967ea56cce511c57c3370
Mat Jones <mat@mjones.network>
2023-04-02 15:20:38 -0400

    make some stuff work better on Linux

--- .config/nvim/lua/my/globals.lua
+++ .config/nvim/lua/my/globals.lua
@@ -47,7 +47,11 @@ _G.Clipboard = {
   ---Copy string to system clipboard
   ---@param str string
   copy = function(str)
-    vim.fn.jobstart(string.format('echo -n %q | pbcopy', str), { detach = true })
+    if vim.fn.has('macunix') then
+      vim.fn.jobstart(string.format('echo -n %q | pbcopy', str), { detach = true })
+    else
+      vim.fn.jobstart(string.format('echo -n %q | xclip -sel clip', str), { detach = true })
+    end
   end,
 }
 
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,25 +1,41 @@
-local w = require('wezterm')
-local keymaps = require('keymaps')
+local w = require("wezterm")
+local keymaps = require("keymaps")
 
-w.on('window-config-reloaded', function(window)
-  window:toast_notification('wezterm', 'configuration reloaded!', nil, 4000)
-end)
+local os_name
+local function os()
+	if os_name then
+		return os_name
+	end
+	local binary_format = package.cpath:match("%p[\\|/]?%p(%a+)")
+	if binary_format == "dll" then
+		os_name = "Windows"
+	elseif binary_format == "so" then
+		os_name = "Linux"
+	elseif binary_format == "dylib" then
+		os_name = "MacOS"
+	end
+end
 
-return {
-  window_decorations = 'RESIZE',
-  cursor_blink_rate = 0,
-  font = w.font('FiraCode Nerd Font'),
-  font_size = 14,
-  use_fancy_tab_bar = true,
-  tab_bar_at_bottom = true,
-  hide_tab_bar_if_only_one_tab = true,
-  window_padding = {
-    top = 0,
-    bottom = 0,
-    left = 0,
-    right = 0,
-  },
-  debug_key_events = true,
-  leader = keymaps.leader,
-  keys = keymaps.keys,
+local config = {
+	cursor_blink_rate = 0,
+	font = w.font("FiraCode Nerd Font"),
+	font_size = 14,
+	use_fancy_tab_bar = true,
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = true,
+	window_padding = {
+		top = 0,
+		bottom = 0,
+		left = 0,
+		right = 0,
+	},
+	debug_key_events = true,
+	leader = keymaps.leader,
+	keys = keymaps.keys,
 }
+
+if os() == "MacOS" then
+	config.window_decorations = "RESIZE"
+end
+
+return config
