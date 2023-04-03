9536885cecf6e5a5ee5daef8084525196c43ffa2
Mat Jones <mat@mjones.network>
2023-04-03 14:45:42 -0400

    tweak atuin config

--- .config/nvim/lua/my/configure/init.lua
+++ .config/nvim/lua/my/configure/init.lua
@@ -1,6 +1,6 @@
 return {
   { 'nvim-lua/plenary.nvim' },
-  { 'tpope/vim-eunuch', cmd = { 'Delete', 'Move', 'Cmhod', 'SudoWrite', 'Rename' } },
+  { 'tpope/vim-eunuch', cmd = { 'Delete', 'Move', 'Chmod', 'SudoWrite', 'Rename' } },
   { 'tpope/vim-sleuth', lazy = false },
   { 'tenxsoydev/karen-yank.nvim', config = true },
   { 'mrjones2014/iconpicker.nvim' },
