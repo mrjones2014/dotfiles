0d5e3f2ad8a6051997a6ce2cd64caec361518ff4
Mat Jones <mat@mjones.network>
2023-04-04 07:47:18 -0400

    adjust window borders in neovim

--- .config/nvim/lua/my/configure/fterm.lua
+++ .config/nvim/lua/my/configure/fterm.lua
@@ -2,7 +2,7 @@ return {
   'numToStr/FTerm.nvim',
   config = function()
     require('FTerm').setup({
-      border = 'none',
+      border = 'single',
       dimensions = {
         height = 0.9,
         width = 0.9,
