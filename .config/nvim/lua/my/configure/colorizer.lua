9a3bd4aeac4a765fe5d680c45bf3e1f212ca6bc6
Mat Jones <mat@mjones.network>
2023-04-04 07:15:37 -0400

    switch to maintained fork of nvim-colorizer

--- .config/nvim/lua/my/configure/colorizer.lua
+++ .config/nvim/lua/my/configure/colorizer.lua
@@ -2,10 +2,17 @@
 local filetypes = vim.list_extend(vim.deepcopy(require('my.lsp.filetypes').filetypes), { 'conf', 'tmux', 'Onedarkpro' })
 
 return {
-  'norcalli/nvim-colorizer.lua',
+  'NvChad/nvim-colorizer.lua',
   ft = filetypes,
   cmd = 'ColorizerAttachToBuffer',
   config = function()
-    require('colorizer').setup(filetypes)
+    require('colorizer').setup({
+      filetypes = filetypes,
+      user_default_options = {
+        css = true,
+        sass = { enable = true, parsers = { 'css' } },
+        always_update = true,
+      },
+    })
   end,
 }
