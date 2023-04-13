c4976afe6c0df2fd51aaa413f9587a42728c41b8
Mat Jones <mat@mjones.network>
2023-04-13 11:50:26 -0400

    fix autolist.nvim

--- .config/nvim/lua/my/configure/autolist.lua
+++ .config/nvim/lua/my/configure/autolist.lua
@@ -2,6 +2,10 @@ return {
   'gaoDean/autolist.nvim',
   ft = { 'markdown', 'text' },
   config = function()
-    require('autolist').setup()
+    local al = require('autolist')
+    al.setup()
+    al.create_mapping_hook('i', '<CR>', al.new)
+    al.create_mapping_hook('i', '<Tab>', al.indent)
+    al.create_mapping_hook('i', '<S-Tab>', al.indent, '<C-d>')
   end,
 }
