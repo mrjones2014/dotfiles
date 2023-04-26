a02d07df2c55f521f510fc0d1e24f3440a275c29
Mat Jones <mat@mjones.network>
2023-04-26 13:01:10 -0400

    add heirline for statuscolumn

--- .config/nvim/lua/my/lsp/icons.lua
+++ .config/nvim/lua/my/lsp/icons.lua
@@ -1,9 +1,18 @@
 local signs = {
   Error = '󰅚 ',
-  Warning = '󰀪 ',
-  Warn = '󰀪 ',
-  Hint = '󰌶 ',
-  Information = ' ',
-  Info = ' ',
+  Warning = ' ',
+  Warn = ' ',
+  Hint = ' ',
+  Information = ' ',
+  Info = ' ',
 }
+
+-- add ability to lookup by Neovim's internal nomenclature
+signs.DiagnosticSignError = signs.Error
+signs.DiagnosticSignWarn = signs.Warn
+signs.DiagnosticSignWarning = signs.Warning
+signs.DiagnosticSignHint = signs.Hint
+signs.DiagnosticSignInformation = signs.Information
+signs.DiagnosticSignInfo = signs.Info
+
 return signs
