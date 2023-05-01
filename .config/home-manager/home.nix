31a70c7a5c88d52ea0c7961d26d9f2732cfb87eb
Mat Jones <mat@mjones.network>
2023-05-01 10:06:25 -0400

    experimenting with nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -4,7 +4,7 @@
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "mat";
-  home.homeDirectory = "/home/mat";
+  home.homeDirectory = if pkgs.stdenv.isLinux then "/home/mat" else "/Users/mat";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -53,9 +53,8 @@
     pkgs.bat
     pkgs.glow
     pkgs.exa
-    pkgs.librewolf
+    pkgs.neovim
     # pkgs.op, # no up to date 1Password CLI nix package
-    # pkgs.bob # no bob-nvim nix package
     #pkgs.hammerspoon, no package
   ];
 
@@ -72,8 +71,8 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-    "${config.xdg.configHome}" = { source = ~/git/dotfiles/.config; recursive = true; };
-    ".ssh/config".source = ~/git/dotfiles/.ssh/config;
+    # "${config.xdg.configHome}" = { source = ~/git/dotfiles/.config; recursive = true; };
+    # ".ssh/config".source = ~/git/dotfiles/.ssh/config;
   };
 
   # You can also manage environment variables but you will have to manually
@@ -87,11 +86,13 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
     DOTNET_CLI_TELEMETRY_OPTOUT = "1";
     HOMEBREW_NO_ANALYTICS = "1";
     CARGO_NET_GIT_FETCH_WITH_CLI = "true";
     GOPATH = "$HOME/go";
+    EDITOR = "nvim";
+    GIT_MERGE_AUTOEDIT = "no";
+    MANPAGER = "nvim -c 'Man!' -o -";
   };
 
   # Let Home Manager install and manage itself.
@@ -99,7 +100,8 @@
   programs.fish = {
    enable = true;
 
-   plugins = [{
+   plugins = [
+    {
        name="foreign-env";
        src = pkgs.fetchFromGitHub {
            owner = "oh-my-fish";
@@ -107,7 +109,73 @@
            rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
            sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
        };
-   }];
+    }
+   ];
+
+   shellAliases = {
+    clear = "clear && tput cup \\$LINES";
+    sourcefish = "source ~/.config/fish/config.fish && fish_logo";
+    dots = "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME";
+    vim = "nvim";
+    vi = "nvim";
+    v = "nvim";
+    # lol, sometimes I'm stupid
+    ":q" = "exit";
+    ":Q" = "exit";
+    # I swear I'm an idiot sometimes
+    ":e" = "nvim";
+    update-nvim-plugins = "nvim --headless '+Lazy! sync' +qa";
+    cat = "bat";
+    gogit = "cd ~/git";
+    "!!" = "eval \\$history[1]";
+    ls = "exa -a --icons --color=always -s type -F";
+    la = "ls -a";
+    ll = "ls -l --git";
+    l = "ls -laH";
+    lg = "ls -lG";
+   };
+
+   shellInit = ''
+    set -g fish_prompt_pwd_dir_length 20
+    set -u fish_greeting ""
+
+    # Setting up SSH_AUTH_SOCK here rather than ~/.ssh/config
+    # because that overrides the environment variables,
+    # meaning I can't easily switch between the production and
+    # debug auth sockets while working on the 1Password desktop app
+    set -g -x SSH_TTY (tty)
+    if [ "$(uname)" = Darwin ]
+        set -g -x SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    else
+        set -g -x SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
+    end
+
+    # for local-only, non-sync'd stuff
+    if test -f $HOME/.config/fish/local.fish
+        source $HOME/.config/fish/local.fish
+    end
+   '';
+
+   interactiveShellInit = ''
+    fish_vi_key_bindings
+    bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+
+    thefuck --alias | source
+    starship init fish | source
+    atuin init fish | source
+    fnm completions | source
+    fnm env | source
+
+    for mode in insert default normal
+        bind -M insert \e\[A "_atuin_search; tput cup \$LINES"
+        bind -M $mode \a _project_jump
+    end
+
+    # I like to keep the prompt at the bottom rather than the top
+    # of the terminal window so that running `clear` doesn't make
+    # me move my eyes from the bottom back to the top of the screen;
+    # keep the prompt consistently at the bottom
+    tput cup $LINES
+   '';
   };
 }
-
