af5f7ec4bf7ee1712e57efbe2be5ea24271c1901
Mat Jones <mat@mjones.network>
2023-05-02 14:00:00 -0400

    update wezterm keymaps

--- scripts/git-ch.bash
+++ scripts/git-ch.bash
@@ -7,5 +7,5 @@ if test "$#" -ne 0; then
     git checkout "$@"
   fi
 else
-  git branch -a --format="%(refname:short)" | sed 's|origin/||g' | grep -v "HEAD" | sort | uniq | fzf-tmux -p -x 15 | xargs git checkout
+  git branch -a --format="%(refname:short)" | sed 's|origin/||g' | grep -v "HEAD" | grep -v "origin" | sort | uniq | fzf-tmux -p -x 15 | xargs git checkout
 fi
