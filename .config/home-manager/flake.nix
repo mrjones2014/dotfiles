ba4ee090d70eaf4a1c50b02a44e48072d9ff2ba5
Mat Jones <mat@mjones.network>
2023-05-02 21:15:56 -0400

    add missing nix files

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My dotfiles managed with nix as a flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin.url = "github:lnl7/nix-darwin";
+    mkalias.url = "github:reckenrode/mkalias";
+  };
+
+  outputs = { nixpkgs, home-manager, darwin, mkalias, ... }: {
+    defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+
+    homeConfigurations = {
+      "mat" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+}
