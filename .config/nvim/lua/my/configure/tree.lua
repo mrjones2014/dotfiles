957ed9a2a708d8370b2459e59eeaaac776110f87
Mat Jones <mat@mjones.network>
2023-05-04 07:39:44 -0400

    tweaking tree setup

--- .config/nvim/lua/my/configure/tree.lua
+++ .config/nvim/lua/my/configure/tree.lua
@@ -12,12 +12,14 @@ return {
           min_width = { width },
           placement = 'edge',
         },
+        filter_kind = false,
+        attach_mode = 'global',
         keymaps = {
           ['<C-j>'] = false,
           ['<C-k>'] = false,
           ['<C-y>'] = 'actions.up_and_scroll',
           ['<C-e>'] = 'actions.down_and_scroll',
-        }
+        },
       })
     end,
   },
@@ -27,17 +29,26 @@ return {
       function()
         local a = require('aerial')
         local tree = require('nvim-tree.api').tree
-        a.open({ focus = true, direction = 'right' })
+        if a.is_open() or tree.is_visible() then
+          a.close()
+          tree.close_in_all_tabs()
+          return
+        end
+        local curwin = vim.api.nvim_get_current_win()
+        tree.open({ path = vim.loop.cwd() })
         local orig = vim.opt.splitbelow
         vim.opt.splitbelow = false
         vim.cmd(('%ssp'):format(width))
+        vim.cmd('wincmd j')
         vim.opt.splitbelow = orig
-        tree.open({current_window = true, path = vim.loop.cwd() })
         vim.schedule(function()
+          a.open_in_win(0, curwin)
           vim.api.nvim_win_set_width(0, width)
+          -- put cursor focus back where it was
+          vim.api.nvim_set_current_win(curwin)
         end)
-      end
+      end,
     })
-    require('nvim-tree').setup()
-  end
+    require('nvim-tree').setup({ view = { side = 'right' } })
+  end,
 }
