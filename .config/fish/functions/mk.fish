beea18f3ae078a5e7c7d44dd9bdfda8fb969181a
Mat Jones <mat@mjones.network>
2023-05-05 13:02:35 -0400

    fix mk fish function

--- .config/fish/functions/mk.fish
+++ .config/fish/functions/mk.fish
@@ -1,6 +1,6 @@
 function mk --wraps make
     if test (count $argv) -lt 1
-        set -l cmd (popup.bash 'make -rpn | sed -n -e \'/^$/ { n ; /^[^ .#][^ ][^/]*:/ { s/:.*$// ; p ; } ; }\' | sort -u | fzf')
+        set -l cmd (make -rpn | sed -n -e '/^$/ { n ; /^[^ .#][^ ][^/]*:/ { s/:.*$// ; p ; } ; }' | sort -u | fzf)
         if test -z "$cmd"
             return
         end
