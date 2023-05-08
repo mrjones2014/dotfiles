439c728ab81eff1c6116260093f975c75a6be3ba
Mat Jones <mat@mjones.network>
2023-05-08 08:42:54 -0400

    fix flake for crossplatform

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -11,25 +11,17 @@
     mkalias.url = "github:reckenrode/mkalias";
   };
 
-  outputs = { nixpkgs, home-manager, darwin, mkalias, nixgl, ... }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ nixgl.overlay ];
-      };
-    in {
-      defaultPackage.aarch64-darwin =
-        home-manager.defaultPackage.aarch64-darwin;
-      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-      nixpkgs.callPkg nixgl.auto.nixGLDefault;
-      homeConfigurations = {
-        "mat" = home-manager.lib.homeManagerConfiguration {
-          pkgs = if builtins.currentSystem == "darwin" then
-            nixpkgs.legacyPackages.aarch64-darwin
-          else
-            nixpkgs.legacyPackages.x86_64-linux;
-          modules = [ ./home.nix ];
-        };
+  outputs = { nixpkgs, home-manager, darwin, mkalias, ... }: {
+    defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    homeConfigurations = {
+      "mat" = home-manager.lib.homeManagerConfiguration {
+        pkgs = if builtins.currentSystem == "aarch64-darwin" then
+          nixpkgs.legacyPackages.aarch64-darwin
+        else
+          nixpkgs.legacyPackages.x86_64-linux;
+        modules = [ ./home.nix ];
       };
     };
+  };
 }
