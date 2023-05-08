91604ef1757c7d1580222bcbe4a4aeda09ebf400
Mat Jones <mat@mjones.network>
2023-05-08 19:42:22 -0400

    fix nix-shell

---
+++ .config/fish/functions/nix-shell.fish
@@ -0,0 +1,10 @@
+function nix-shell --wraps nix-shell
+    for ARG in $argv
+        if [ "$ARG" = --run ]
+            command nix-shell $argv
+            return $status
+        end
+    end
+
+    command nix-shell $argv --run "exec fish"
+end
