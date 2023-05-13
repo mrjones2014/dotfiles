fa4880ff6407930d4bd0bb36cffce950c792a2af
Mat Jones <mat@mjones.network>
2023-05-13 12:27:48 -0400

    fix: install nixGL

--- home-manager/modules/wezterm.nix
+++ home-manager/modules/wezterm.nix
@@ -1,7 +1,17 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   programs.wezterm.enable = true;
   # I have my own config
   xdg.configFile."wezterm/wezterm.lua".enable = false;
+  xdg.desktopEntries."org.wezfurlong.wezterm" = {
+    name = "Wezterm";
+    exec =
+      if pkgs.stdenv.isLinux then ''"nixGL wezterm || wezterm"'' else "wezterm";
+    icon = "org.wezfurlong.wezterm";
+    settings = {
+      Keywords = "shell;prompt;command;commandline;cmd";
+      StartupWMClass = "org.wezfurlong.wezterm";
+    };
+  };
   home.sessionVariables = { TERM = "wezterm"; };
   home.file."${config.xdg.configHome}/wezterm" = {
     source = ../../wezterm;
