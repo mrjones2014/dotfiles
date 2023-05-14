755325d02c6e40e8ae26d07e5a5d247f985fa615
Mat Jones <mat@mjones.network>
2023-05-13 20:19:57 -0400

    fix(nix): use x11 instead of Wayland

--- nixos-modules/gaming.nix
+++ nixos-modules/gaming.nix
@@ -12,10 +12,17 @@
   ];
 
   # use proprietary nvidia drivers
+  # some gaming stuff doesn't work good on wayland
+  services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.defaultSession = "gnome-xorg";
+  services.xserver.displayManager.gdm.wayland = false;
   hardware.opengl.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.nvidiaPersistenced = true;
 
   users.users.mat = { shell = pkgs.fish; };
   nixpkgs.config.firefox.enableGnomeExtensions = true;
