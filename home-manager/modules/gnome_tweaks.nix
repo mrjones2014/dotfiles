bdc308acf01d5aa12ad2e211a87c0d014fc7ba74
Mat Jones <mat@mjones.network>
2023-05-15 19:05:58 -0400

    feat(gnome): set favorite apps

--- home-manager/modules/gnome_tweaks.nix
+++ home-manager/modules/gnome_tweaks.nix
@@ -3,5 +3,21 @@
     "org/gnome/desktop/wm/preferences" = {
       button-layout = "close,minimize,maximize:";
     };
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "org.gnome.Settings.desktop"
+        "org.gnome.Nautilus.desktop"
+        "org.wezfurlong.wezterm.desktop"
+        "1password.desktop"
+        "signal-desktop.desktop"
+        "librewolf.desktop"
+        "discord.desktop"
+        "steam.desktop"
+      ];
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
   };
 }
