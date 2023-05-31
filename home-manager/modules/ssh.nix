14c983bd0c6dcd883cdf3c895ae36a90ffef19aa
Mat Jones <mat@mjones.network>
2023-05-31 10:25:54 -0400

    fix(git): SSH auth

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -2,5 +2,15 @@
   programs.ssh = {
     enable = true;
     forwardAgent = true;
+    matchBlocks = {
+      "gitlab.1password.io" = {
+        port = 2227;
+        hostname = "ssh.gitlab.1password.io";
+      };
+      "*.gitlab.1password.io" = {
+        port = 2227;
+        hostname = "ssh.gitlab.1password.io";
+      };
+    };
   };
 }
