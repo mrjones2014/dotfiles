92c3b7c601d61557e62c46bdf58c5fda9bf182b1
Mat Jones <mat@mjones.network>
2023-06-12 11:24:47 -0400

    chore(nix): Remove commented out lines

--- nixos-modules/_1password.nix
+++ nixos-modules/_1password.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }: {
-  programs._1password = {
-    enable = true;
-    # package = pkgs.unstable._1password;
-  };
+  programs._1password = { enable = true; };
   programs._1password-gui = {
     enable = true;
-    # package = pkgs.unstable._1password-gui;
     polkitPolicyOwners = [ "mat" ];
   };
 }
