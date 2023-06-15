d443ece624b2a086ccb30199038848685ac5cd6b
Mat Jones <mat@mjones.network>
2023-06-15 14:38:02 -0400

    fix(NixOS): Start 1Password silently on login

--- home-manager/modules/pantheon_config.nix
+++ home-manager/modules/pantheon_config.nix
@@ -1,27 +1,25 @@
 { pkgs, config, ... }:
 let
-  autostartPrograms =
-    if pkgs.stdenv.isDarwin then [ ] else [ pkgs._1password-gui ];
   wallpaperImg = pkgs.fetchurl {
     url =
       "https://user-images.githubusercontent.com/8648891/246180692-b8144052-e947-47b0-b59c-ea1022b9b629.jpg";
     hash = "sha256-itnhNPYvQLfCULNrEZqP+3VBQVmEmvh9wv6C2F3YKQU=";
   };
 in {
-  # autostart 1Password; workaround for https://github.com/nix-community/home-manager/issues/3447
-  home.file = builtins.listToAttrs (map (pkg: {
-    name = ".config/autostart/" + pkg.pname + ".desktop";
-    value = if pkg ? desktopItem then {
-      # Application has a desktopItem entry.
-      # Assume that it was made with makeDesktopEntry, which exposes a
-      # text attribute with the contents of the .desktop file
-      text = pkg.desktopItem.text;
-    } else {
-      # Application does *not* have a desktopItem entry. Try to find a
-      # matching .desktop name in /share/apaplications
-      source = (pkg + "/share/applications/" + pkg.pname + ".desktop");
-    };
-  }) autostartPrograms);
+  # workaround for https://github.com/nix-community/home-manager/issues/3447
+  # autostart 1Password in the background so I can use the SSH agent without manually opening the app first
+  xdg.configFile."autostart/1password.desktop".text = ''
+     [Desktop Entry]
+    Name=1Password
+    Exec=1password --silent
+    Terminal=false
+    Type=Application
+    Icon=1password
+    StartupWMClass=1Password
+    Comment=Password manager and secure wallet
+    MimeType=x-scheme-handler/onepassword;
+    Categories=Office;
+  '';
 
   # see  https://github.com/wimpysworld/nix-config/blob/b8a260ddea1bbf088461f7272382d99acbf86ce7/home-manager/_mixins/desktop/pantheon.nix
   dconf.settings = {
@@ -43,11 +41,16 @@ in {
     "org/pantheon/desktop/gala/appearance" = {
       button-layout = "close,minimize,maximize,appmenu:";
     };
+    "org/pantheon/desktop/gala/mask-corners" = { enable = false; };
+    # key bindings
+    "org/pantheon/desktop/gala/behavior" = {
+      overlay-action =
+        "io.elementary.wingpanel --toggle-indicator=app-launcher";
+    };
     "org/gnome/mutter/keybindings" = {
       toggle-tiled-left = [ "<Ctrl><Shift><Alt><Super>h" ];
       toggle-tiled-right = [ "<Ctrl><Shift><Alt><Super>l" ];
     };
-    "org/pantheon/desktop/gala/mask-corners" = { enable = false; };
   };
   gtk = {
     gtk2 = {
