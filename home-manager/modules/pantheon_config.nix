de82186fd96020f4da5fe339cfcde00945a0a98a
Mat Jones <mat@mjones.network>
2023-06-15 13:03:14 -0400

    feat(NixOS): More dconf settings for Pantheon desktop

--- home-manager/modules/pantheon_config.nix
+++ home-manager/modules/pantheon_config.nix
@@ -1,7 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   autostartPrograms =
     if pkgs.stdenv.isDarwin then [ ] else [ pkgs._1password-gui ];
+  wallpaperImg = pkgs.fetchurl {
+    url =
+      "https://user-images.githubusercontent.com/8648891/246180692-b8144052-e947-47b0-b59c-ea1022b9b629.jpg";
+    hash = "sha256-itnhNPYvQLfCULNrEZqP+3VBQVmEmvh9wv6C2F3YKQU=";
+  };
 in {
   # autostart 1Password; workaround for https://github.com/nix-community/home-manager/issues/3447
   home.file = builtins.listToAttrs (map (pkg: {
@@ -18,9 +23,47 @@ in {
     };
   }) autostartPrograms);
 
+  # see  https://github.com/wimpysworld/nix-config/blob/b8a260ddea1bbf088461f7272382d99acbf86ce7/home-manager/_mixins/desktop/pantheon.nix
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      cursor-theme = "elementary";
+      icon-theme = "elementary";
+      enable-hot-corners = false;
+      clock-show-weekday = true;
+    };
+    "org/gnome/desktop/background" = {
+      picture-uri = "file://${wallpaperImg}";
+    };
+    "org/gnome/desktop/datetime" = { automatic-timezone = true; };
+    "io/elementary/files/preferences" = { singleclick-select = true; };
+    "io/elementary/settings-daemon/housekeeping" = {
+      cleanup-downloads-folder = true;
+    };
     "org/pantheon/desktop/gala/appearance" = {
       button-layout = "close,minimize,maximize,appmenu:";
+    };
+    "org/gnome/mutter/keybindings" = {
+      toggle-tiled-left = [ "<Ctrl><Shift><Alt><Super>h" ];
+      toggle-tiled-right = [ "<Ctrl><Shift><Alt><Super>l" ];
+    };
+    "org/pantheon/desktop/gala/mask-corners" = { enable = false; };
+  };
+  gtk = {
+    gtk2 = {
+      configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+      extraConfig = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk3 = { extraConfig = { gtk-application-prefer-dark-theme = 1; }; };
+
+    gtk4 = { extraConfig = { gtk-application-prefer-dark-theme = 1; }; };
+
+    iconTheme = {
+      name = "elementary";
+      package = pkgs.pantheon.elementary-icon-theme;
     };
   };
 }
