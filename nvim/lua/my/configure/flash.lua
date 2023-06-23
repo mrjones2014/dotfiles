faf34c2e5829971ac5c771e70734090009d98057
Mat Jones <mat@mjones.network>
2023-06-23 07:26:35 -0400

    feat(nvim): Move some keymaps into lazy.nvim plugin specs

--- nvim/lua/my/configure/flash.lua
+++ nvim/lua/my/configure/flash.lua
@@ -1,5 +1,23 @@
 return {
   'folke/flash.nvim',
+  keys = {
+    {
+      's',
+      function()
+        require('flash').jump()
+      end,
+      mode = { 'n', 'x', 'o' },
+      desc = 'Jump forwards',
+    },
+    {
+      'S',
+      function()
+        require('flash').jump({ search = { forward = false } })
+      end,
+      mode = { 'n', 'x', 'o' },
+      desc = 'Jump backwards',
+    },
+  },
   config = function()
     require('flash').setup({
       jump = { nohlsearch = true },
