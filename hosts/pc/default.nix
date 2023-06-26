1c10b551a6ce2e9f9a1104ddca9b171f788e1a8d
Mat Jones <mat@mjones.network>
2023-06-26 17:22:07 -0400

    fix(NixOS): Rollback NVIDIA driver until NVIDIA or Valve fix Steam

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -12,8 +12,11 @@
 
   # use proprietary nvidia drivers
   hardware.opengl.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # these are needed for Wayland to work
+  # workaround for Steam new UI bug, change this back to `config.boot.kernelPackages.nvidiaPackages.stable` in the future.
+  # see: https://wiki.archlinux.org/title/Steam/Troubleshooting#Steam_window_does_not_show_on_Nvidia_GPUs_after_the_June_14,_2023_update
+  # see last answer here: https://discourse.nixos.org/t/using-older-revisions-of-nvidia-drivers/28645
+  hardware.nvidia.package =
+    config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.nvidiaPersistenced = true;
 
