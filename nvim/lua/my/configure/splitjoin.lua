9b4c21fd90bbf0e5da163040d701d257741f0c6d
Mat Jones <mat@mjones.network>
2023-06-27 12:45:43 -0400

    feat(nvim): Add mini.files

---
+++ nvim/lua/my/configure/splitjoin.lua
@@ -0,0 +1,10 @@
+return {
+  'echasnovski/mini.splitjoin',
+  keys = {
+    {
+      'gS',
+      desc = 'Toggle arrays/lists/etc. between single and multi line formats.',
+    },
+  },
+  config = true,
+}
---
+++ nvim/lua/my/configure/surround.lua
@@ -0,0 +1,16 @@
+return {
+  'echasnovski/mini.surround',
+  keys = { '<leader>s' },
+  config = true,
+  opts = {
+    mappings = {
+      add = '<leader>sa',
+      delete = '<leader>sd',
+      replace = '<leader>sr',
+      find = '',
+      find_left = '',
+      highlight = '',
+      update_n_lines = '',
+    },
+  },
+}
