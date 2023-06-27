6fb1deb617055b6c20da308a34ba13311fcb1e44
Mat Jones <mat@mjones.network>
2023-06-27 18:41:49 -0400

    fix(nvim): Fix mini.files config

--- nvim/lua/my/configure/mini_files.lua
+++ nvim/lua/my/configure/mini_files.lua
@@ -1,20 +1,20 @@
 return {
   'echasnovski/mini.files',
+  config = true,
   init = function()
     -- set up ability to confirm changes with :w
     vim.api.nvim_create_autocmd('User', {
       pattern = 'MiniFilesBufferCreate',
-      callback = function()
-        vim.schedule(function()
-          vim.api.nvim_buf_set_option(0, 'buftype', 'acwrite')
-          vim.api.nvim_buf_set_name(0, require('mini.files').get_fs_entry(0, 1).path)
-          vim.api.nvim_create_autocmd('BufWriteCmd', {
-            buffer = 0,
-            callback = function()
-              require('mini.files').synchronize()
-            end,
-          })
-        end)
+      callback = function(args)
+        local buf = args.data.buf_id
+        vim.api.nvim_buf_set_option(buf, 'buftype', 'acwrite')
+        vim.api.nvim_buf_set_name(buf, string.format('mini.files-%s', vim.loop.hrtime()))
+        vim.api.nvim_create_autocmd('BufWriteCmd', {
+          buffer = buf,
+          callback = function()
+            require('mini.files').synchronize()
+          end,
+        })
       end,
     })
   end,
@@ -36,5 +36,4 @@ return {
       width_preview = 120,
     },
   },
-  config = true,
 }
