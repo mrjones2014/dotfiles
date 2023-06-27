8e4757fdf9287deb53c18ad3607a7935ab797f4b
Mat Jones <mat@mjones.network>
2023-06-27 12:45:43 -0400

    feat(nvim): Add mini.files

--- nvim/lua/my/configure/init.lua
+++ nvim/lua/my/configure/init.lua
@@ -8,46 +8,6 @@ return {
   { 'mrjones2014/lua-gf.nvim', dev = true, ft = 'lua' },
   { 'echasnovski/mini.pairs', event = 'InsertEnter', config = true },
   { 'echasnovski/mini.trailspace', event = 'BufRead', config = true },
-  {
-    'echasnovski/mini.files',
-    keys = {
-      {
-        '<F3>',
-        function()
-          require('mini.files').open(vim.loop.cwd(), true)
-        end,
-        desc = 'Open mini.files',
-      },
-    },
-    opts = { windows = { preview = true, width_preview = 120 } },
-    config = true,
-  },
-  {
-    'echasnovski/mini.splitjoin',
-    keys = {
-      {
-        'gS',
-        desc = 'Toggle arrays/lists/etc. between single and multi line formats.',
-      },
-    },
-    config = true,
-  },
-  {
-    'echasnovski/mini.surround',
-    keys = { '<leader>s' },
-    config = true,
-    opts = {
-      mappings = {
-        add = '<leader>sa',
-        delete = '<leader>sd',
-        replace = '<leader>sr',
-        find = '',
-        find_left = '',
-        highlight = '',
-        update_n_lines = '',
-      },
-    },
-  },
   { 'max397574/better-escape.nvim', event = 'InsertEnter', config = true },
   {
     'saecki/crates.nvim',
