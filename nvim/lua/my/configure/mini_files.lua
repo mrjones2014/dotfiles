aacc6d656ac5557b59d32a4d9f0b2cf418c45148
Mat Jones <mat@mjones.network>
2023-06-28 09:36:48 -0400

    feat(nvim): Reveal current file in mini.files on open

--- nvim/lua/my/configure/mini_files.lua
+++ nvim/lua/my/configure/mini_files.lua
@@ -27,10 +27,47 @@ return {
     {
       '<F3>',
       function()
+        local minifiles = require('mini.files')
         if vim.bo.filetype == 'minifiles' then
-          require('mini.files').close()
+          minifiles.close()
         else
-          require('mini.files').open(vim.loop.cwd(), true)
+          local current_file = vim.api.nvim_buf_get_name(0)
+          local is_file = not vim.bo.buftype or vim.bo.buftype == ''
+          minifiles.open(vim.loop.cwd(), true)
+          -- reveal current buffer in file tree
+          if is_file then
+            vim.schedule(function()
+              local line = 1
+              local entry = minifiles.get_fs_entry(0, line)
+              while entry do
+                if not entry then
+                  return
+                end
+                -- if path matches exactly, we found the file, just set cursor to the right line
+                if current_file == entry.path then
+                  vim.api.nvim_win_set_cursor(0, { line, 1 })
+                  return
+                -- if buffer file name has the entry path as a prefix, open the directory
+                elseif
+                  current_file:find(
+                    -- add trailing slash on directory path to avoid matching substrings
+                    -- like `directory` matching `directory_other`;
+                    -- we make it so `directory/` doesn't match `directory_other/`
+                    string.format('%s/', entry.path),
+                    1,
+                    true
+                  ) == 1
+                then
+                  vim.api.nvim_win_set_cursor(0, { line, 1 })
+                  require('mini.files').go_in()
+                  line = 1
+                else
+                  line = line + 1
+                end
+                entry = minifiles.get_fs_entry(0, line)
+              end
+            end)
+          end
         end
       end,
       desc = 'Open mini.files',
