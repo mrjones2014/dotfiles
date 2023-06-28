966f6a19cee4eab691a83c41cfe0855034a67fbd
Mat Jones <mat@mjones.network>
2023-06-28 06:49:50 -0400

    fix(nvim): Add mini.files keymaps

--- nvim/lua/my/configure/mini_files.lua
+++ nvim/lua/my/configure/mini_files.lua
@@ -2,11 +2,17 @@ return {
   'echasnovski/mini.files',
   config = true,
   init = function()
-    -- set up ability to confirm changes with :w
     vim.api.nvim_create_autocmd('User', {
       pattern = 'MiniFilesBufferCreate',
       callback = function(args)
         local buf = args.data.buf_id
+
+        -- close with <ESC> as well as q
+        vim.keymap.set('n', '<ESC>', function()
+          require('mini.files').close()
+        end, { buffer = buf })
+
+        -- set up ability to confirm changes with :w
         vim.api.nvim_buf_set_option(buf, 'buftype', 'acwrite')
         vim.api.nvim_buf_set_name(buf, string.format('mini.files-%s', vim.loop.hrtime()))
         vim.api.nvim_create_autocmd('BufWriteCmd', {
@@ -28,6 +34,11 @@ return {
     },
   },
   opts = {
+    content = {
+      filter = function(entry)
+        return entry.name ~= '.DS_Store' and entry.name ~= '.git'
+      end,
+    },
     mappings = {
       go_in_plus = '<CR>',
     },
