c2ba4cb17f0b28757289927e38b73d8dbdea43a3
Mat Jones <mat@mjones.network>
2023-06-28 07:02:35 -0400

    feat(nvim): Add `mini.bracketed` plugin

---
+++ nvim/lua/my/configure/mini_bracketed.lua
@@ -0,0 +1,16 @@
+return {
+  'echasnovski/mini.bracketed',
+  keys = {
+    { '[c', desc = 'Jump to previous comment block' },
+    { ']c', desc = 'Jump to next comment block' },
+    { '[x', desc = 'Jump to previous conflict marker' },
+    { ']x', desc = 'Jump to next conflict marker' },
+    { '[d', desc = 'Jump to previous diagnostic' },
+    { ']d', desc = 'Jump to next diagnostic' },
+    { '[q', desc = 'Jump to previous Quickfix list entry' },
+    { ']q', desc = 'Jump to next Quickfix list entry' },
+    { '[t', desc = 'Jump to previous Treesitter node' },
+    { ']t', desc = 'Jump to next Treesitter node' },
+  },
+  opts = {},
+}
