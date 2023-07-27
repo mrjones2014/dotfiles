b27fc26649320fde286e116044b4a8bf6ce16cb7
Mat Jones <mat@mjones.network>
2023-07-27 11:37:25 -0400

    fix(nvim): cbfmt working

--- nvim/lua/efmls-configs/formatters/cbfmt.lua
+++ nvim/lua/efmls-configs/formatters/cbfmt.lua
@@ -1,4 +1,3 @@
--- TODO this one isn't working
 local fs = require('efmls-configs.fs')
 local formatter = fs.executable('cbfmt')
 local command = string.format('%s --stdin-filepath ${INPUT} --best-effort', formatter)
