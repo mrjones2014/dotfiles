7d161228f8fa6f9316845657a2e72d195827902e
Mat Jones <mat@mjones.network>
2023-08-22 12:54:17 -0400

    chore(nvim): reformat

--- nvim/lua/my/lsp/lua.lua
+++ nvim/lua/my/lsp/lua.lua
@@ -7,8 +7,6 @@ if string.find(assert(vim.loop.cwd()), 'hammerspoon') then
   globals = { 'hs' }
 end
 
-require('neodev').setup()
-
 return {
   root_dir = require('lspconfig.util').root_pattern('.git', '.luacheckrc', 'stylua.toml'),
   settings = {
