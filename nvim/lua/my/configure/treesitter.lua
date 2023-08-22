4350901e4f39eba0946d539f1b92c7213da076ca
Mat Jones <mat@mjones.network>
2023-08-22 12:52:16 -0400

    feat(nvim): Add hmts.nvim for syntax highlighting of Nix sub-languages
    note: it doesn't work yet
    
    https://github.com/calops/hmts.nvim/issues/9

--- nvim/lua/my/configure/treesitter.lua
+++ nvim/lua/my/configure/treesitter.lua
@@ -1,106 +1,115 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    {
-      'hiphish/rainbow-delimiters.nvim',
-      config = function()
-        local rainbow = require('rainbow-delimiters')
-        vim.g.rainbow_delimiters = {
-          strategy = {
-            [''] = rainbow.strategy['global'],
-            vim = rainbow.strategy['local'],
-          },
-          query = {
-            [''] = 'rainbow-delimiters',
-            lua = 'rainbow-blocks',
-            html = 'rainbow-tags',
-          },
-          highlight = {
-            'RainbowDelimiterRed',
-            'RainbowDelimiterYellow',
-            'RainbowDelimiterBlue',
-            'RainbowDelimiterOrange',
-            'RainbowDelimiterGreen',
-            'RainbowDelimiterViolet',
-            'RainbowDelimiterCyan',
-          },
-        }
-      end,
-    },
-    'windwp/nvim-ts-autotag',
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    'andymass/vim-matchup',
-    {
-      'ziontee113/query-secretary',
-      keys = {
-        {
-          '<leader>qs',
-          function()
-            require('query-secretary').query_window_initiate()
-          end,
-          desc = 'Open Query Secretary',
-        },
-      },
-      config = function()
-        require('query-secretary').setup({})
-      end,
-    },
+  {
+    'calops/hmts.nvim',
+    version = '*',
+    filetype = 'nix',
+    dev = true,
+    lazy = false,
   },
-  event = 'BufRead',
-  cmd = { 'TSInstall', 'TSUpdate', 'TSUpdateSync' },
-  build = function()
-    if #vim.api.nvim_list_uis() == 0 then
-      -- update sync if running headless
-      vim.cmd.TSUpdateSync()
-    else
-      -- otherwise update async
-      vim.cmd.TSUpdate()
-    end
-  end,
-  init = function()
-    vim.g.matchup_matchparen_offscreen = {
-      method = 'popup',
-    }
-  end,
-  config = function()
-    vim.filetype.add({
-      extension = {
-        mdx = 'mdx',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      {
+        'hiphish/rainbow-delimiters.nvim',
+        config = function()
+          local rainbow = require('rainbow-delimiters')
+          vim.g.rainbow_delimiters = {
+            strategy = {
+              [''] = rainbow.strategy['global'],
+              vim = rainbow.strategy['local'],
+            },
+            query = {
+              [''] = 'rainbow-delimiters',
+              lua = 'rainbow-blocks',
+              html = 'rainbow-tags',
+            },
+            highlight = {
+              'RainbowDelimiterRed',
+              'RainbowDelimiterYellow',
+              'RainbowDelimiterBlue',
+              'RainbowDelimiterOrange',
+              'RainbowDelimiterGreen',
+              'RainbowDelimiterViolet',
+              'RainbowDelimiterCyan',
+            },
+          }
+        end,
       },
-    })
-    -- highlight mdx with markdown -- it's close enough. We also do JSX injection via ./after/queries/markdown/injections.scm
-    vim.treesitter.language.register('markdown', 'mdx')
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = require('my.lsp.filetypes').treesitter_parsers(),
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        -- not sure why but these don't work correctly
-        -- if they're defined in keymaps.lua instead of here
-        keymaps = {
-          init_selection = '<CR>',
-          node_incremental = '<CR>',
-          scope_incremental = '<S-CR>',
-          node_decremental = '<BS>',
+      'windwp/nvim-ts-autotag',
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      'andymass/vim-matchup',
+      {
+        'ziontee113/query-secretary',
+        keys = {
+          {
+            '<leader>qs',
+            function()
+              require('query-secretary').query_window_initiate()
+            end,
+            desc = 'Open Query Secretary',
+          },
         },
+        config = function()
+          require('query-secretary').setup({})
+        end,
       },
-      -- treesitter parser for Swift requires treesitter-cli and only really works on mac
-      additional_vim_regex_highlighting = { 'swift' },
-      autotag = {
-        enable = true,
-      },
-      context_commentstring = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-        include_match_words = true,
-      },
-    })
-  end,
+    },
+    event = 'BufRead',
+    cmd = { 'TSInstall', 'TSUpdate', 'TSUpdateSync' },
+    build = function()
+      if #vim.api.nvim_list_uis() == 0 then
+        -- update sync if running headless
+        vim.cmd.TSUpdateSync()
+      else
+        -- otherwise update async
+        vim.cmd.TSUpdate()
+      end
+    end,
+    init = function()
+      vim.g.matchup_matchparen_offscreen = {
+        method = 'popup',
+      }
+    end,
+    config = function()
+      vim.filetype.add({
+        extension = {
+          mdx = 'mdx',
+        },
+      })
+      -- highlight mdx with markdown -- it's close enough. We also do JSX injection via ./after/queries/markdown/injections.scm
+      vim.treesitter.language.register('markdown', 'mdx')
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = require('my.lsp.filetypes').treesitter_parsers(),
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          -- not sure why but these don't work correctly
+          -- if they're defined in keymaps.lua instead of here
+          keymaps = {
+            init_selection = '<CR>',
+            node_incremental = '<CR>',
+            scope_incremental = '<S-CR>',
+            node_decremental = '<BS>',
+          },
+        },
+        -- treesitter parser for Swift requires treesitter-cli and only really works on mac
+        additional_vim_regex_highlighting = { 'swift' },
+        autotag = {
+          enable = true,
+        },
+        context_commentstring = {
+          enable = true,
+        },
+        matchup = {
+          enable = true,
+          include_match_words = true,
+        },
+      })
+    end,
+  },
 }
