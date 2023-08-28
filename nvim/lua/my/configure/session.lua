762721c0e51087ce2578754be5fc469dd8e7aa7f
Mat Jones <mat@mjones.network>
2023-08-28 10:20:12 -0400

    fix(nvim): Load smart-splits.nvim if a session is loaded

--- nvim/lua/my/configure/session.lua
+++ nvim/lua/my/configure/session.lua
@@ -12,10 +12,15 @@ return {
       end
     end
 
+    local resession = require('resession')
+    resession.add_hook('post_load', function()
+      -- if loading a session, there are likely multiple windows, and loading smart-splits.nvim is inexpensive anyway
+      require('smart-splits')
+    end)
     -- Only load the session if nvim was started with no args, or if command was to open a directory, not a file
     local num_args = vim.fn.argc(-1)
     if num_args == 0 or (num_args == 1 and vim.fn.isdirectory(vim.fn.argv(0) or '') ~= 0) then
-      require('resession').load(get_session_name(), { dir = 'dirsession', silence_errors = true, notify = false })
+      resession.load(get_session_name(), { dir = 'dirsession', silence_errors = true, notify = false })
     end
 
     vim.api.nvim_create_autocmd('VimLeavePre', {
