359d08ee0876188ff04a9e8948d3f97c00b42749
Mat Jones <mat@mjones.network>
2023-09-07 10:38:24 -0400

    fix(init): Clarify setup instructions in init.bash

--- init.bash
+++ init.bash
@@ -48,9 +48,9 @@ elif ! command -v nixos-version >/dev/null 2>&1; then
 fi
 
 echo
-echo "Everything should be set up. Run one of the following commands (depending on your current system) to apply configuration. After applied, you can use \`nix-apply\` (a shell alias) going forward."
+echo "Everything should be set up. Run one of the following commands (depending on your current system) to apply configuration. After applied the first time, you shouldn't need to set the \$NIX_CONFIG variable and you can use \`nix-apply\` (a shell alias) going forward."
 echo
 
 # output value of the shell alias, highlighted
-grep "switch --flake" "$SCRIPT_DIR/home-manager/modules/fish.nix" | sed -e 's/\(.*\)/\x1b[38;5;166m\1\x1b[0m/' | awk '{$1:$1;print}'
+echo "NIX_CONFIG=\"experimental-features = nix-command flakes\" $(grep "switch --flake" "$SCRIPT_DIR/home-manager/modules/fish.nix") | sed -e 's/\(.*\)/\x1b[38;5;166m\1\x1b[0m/' | awk '{$1:$1;print}'
 echo
