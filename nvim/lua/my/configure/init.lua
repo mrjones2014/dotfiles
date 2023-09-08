b0d1ca19a8f4ecebcde64db75ff5ddab281f46b4
Mat Jones <mat@mjones.network>
2023-09-08 14:14:32 -0400

    fix(nvim): Don't complete pairs if the matching character is already present in the next position

--- nvim/lua/my/configure/init.lua
+++ nvim/lua/my/configure/init.lua
@@ -6,7 +6,26 @@ return {
   { 'tpope/vim-sleuth', event = 'BufReadPre' },
   { 'mrjones2014/iconpicker.nvim' },
   { 'mrjones2014/lua-gf.nvim', dev = true, ft = 'lua' },
-  { 'echasnovski/mini.pairs', event = 'InsertEnter', opts = {} },
+  {
+    'echasnovski/mini.pairs',
+    event = 'InsertEnter',
+    opts = {
+      mappings = {
+        -- source: https://old.reddit.com/r/neovim/comments/163rzex/how_to_avoid_autocompleting_right_parentheses/jy4zwp8/
+        -- disable if a matching character is in an adjacent position (eg. fixes
+        -- markdown triple ticks) neigh_pattern: a pattern for *two* neighboring
+        -- characters (before and after). Use dot `.` to allow any character.
+        -- Here, we disable the functionality instead of inserting a matching quote
+        -- if there is an adjacent non-space character
+        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^%S][^%S]', register = { cr = false } },
+        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^%S][^%S]', register = { cr = false } },
+        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%S][^%S]', register = { cr = false } },
+        ['['] = { action = 'closeopen', pair = '[]', neigh_pattern = '[^%S][^%S]', register = { cr = false } },
+        ['{'] = { action = 'closeopen', pair = '{}', neigh_pattern = '[^%S][^%S]', register = { cr = false } },
+        ['('] = { action = 'closeopen', pair = '()', neigh_pattern = '[^%S][^%S]', register = { cr = false } },
+      },
+    },
+  },
   { 'echasnovski/mini.trailspace', event = 'BufRead', opts = {} },
   { 'max397574/better-escape.nvim', event = 'InsertEnter', opts = {} },
   {
