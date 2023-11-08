bd65f99946831fdf456ce1481407b4b1661e75ab
Mat Jones <mat@mjones.network>
2023-11-08 16:28:11 -0500

    fix(nixos): Switch graphics drivers

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -15,8 +15,7 @@
   # workaround for Steam new UI bug, change this back to `config.boot.kernelPackages.nvidiaPackages.stable` in the future.
   # see: https://wiki.archlinux.org/title/Steam/Troubleshooting#Steam_window_does_not_show_on_Nvidia_GPUs_after_the_June_14,_2023_update
   # see last answer here: https://discourse.nixos.org/t/using-older-revisions-of-nvidia-drivers/28645
-  hardware.nvidia.package =
-    config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.nvidiaPersistenced = true;
 
