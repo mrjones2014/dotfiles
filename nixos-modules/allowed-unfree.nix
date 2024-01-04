147f6ffd7f73d8c3f32955890d70b6a53f9b6e0a
Mat Jones <mat@mjones.network>
2024-01-04 16:28:05 -0500

    chore(nix): Move allowedUnfree predicates to eliminate code duplication

---
+++ nixos-modules/allowed-unfree.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, ... }: {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "obsidian"
+      "discord"
+      "spotify"
+      "nvidia-persistenced"
+      "nvidia-x11"
+      "nvidia-settings"
+      "1password"
+      "1password-cli"
+      "steam"
+      "steam-run"
+      "steam-original"
+      "parsec-bin"
+      # This is required for pkgs.nodePackages_latest.vscode-langservers-extracted on NixOS
+      # however VS Code should NOT be installed on this system!
+      # Use VS Codium instead: https://github.com/VSCodium/vscodium
+      "vscode"
+    ];
+}
+
