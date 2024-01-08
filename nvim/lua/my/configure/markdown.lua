f40c618e9e164a55a4c7315aac4ee89e88b62a55
Mat Jones <mat@mjones.network>
2024-01-08 07:51:32 -0500

    fix(nvim): Remove autolist plugin breaking my enter key on markdown files

--- nvim/lua/my/configure/markdown.lua
+++ nvim/lua/my/configure/markdown.lua
@@ -5,18 +5,4 @@ return {
     ft = 'markdown',
     dependencies = { 'norcalli/nvim-terminal.lua', config = true },
   },
-  {
-    'gaoDean/autolist.nvim',
-    ft = 'markdown',
-    version = '2.3.0',
-    config = function()
-      local al = require('autolist')
-      al.setup()
-      al.create_mapping_hook('i', '<CR>', al.new)
-      al.create_mapping_hook('i', '<Tab>', al.indent)
-      al.create_mapping_hook('i', '<S-Tab>', al.indent, '<C-d>')
-      al.create_mapping_hook('n', 'o', al.new)
-      al.create_mapping_hook('n', 'O', al.new_before)
-    end,
-  },
 }
