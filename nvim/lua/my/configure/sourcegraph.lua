a5bf2e996d1c6e2962160655dfe9d9efd3a629c2
Mat Jones <mat@mjones.network>
2024-01-12 10:46:18 -0500

    fix(nvim): Use `require('op').get_secret` utility to authenticate sg.nvim

--- nvim/lua/my/configure/sourcegraph.lua
+++ nvim/lua/my/configure/sourcegraph.lua
@@ -2,20 +2,14 @@ return {
   'sourcegraph/sg.nvim',
   dependencies = { 'mrjones2014/op.nvim' },
   config = function()
-    local op = require('op.api') ---@type OpApi
-    local stdout, stderr, exit_code = op.item.get({ 'yz4q4rfnfzsvikdx4ppsze4tti', '--format', 'json' })
-    if exit_code ~= 0 then
-      error(string.format('Failed to get Sourcegraph token from 1Password\n%s', table.concat(stderr, '\n')))
-    end
-    local item = vim.json.decode(table.concat(stdout, ''))
-    local token = vim.iter(item.fields):find(function(field)
-      return field.label == 'token'
-    end)
-    if not token then
+    local output = require('op').get_secret('yz4q4rfnfzsvikdx4ppsze4tti', 'token,endpoint')
+    if output then
       error('Failed to get Sourcegraph token from 1Password')
     end
-    token = token.value
-    vim.env.SRC_ENDPOINT = item.urls[1].href
+    local secrets = vim.split(output, ',')
+    local token = secrets[1]
+    local endpoint = secrets[2]
+    vim.env.SRC_ENDPOINT = endpoint
     vim.env.SRC_ACCESS_TOKEN = token
     require('sg').setup({
       enable_cody = false,
