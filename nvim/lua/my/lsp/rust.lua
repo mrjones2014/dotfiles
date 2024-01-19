e29f99c1f2fd57fa99039a16d1ded7a725961e1f
Mat Jones <mat@mjones.network>
2024-01-19 14:24:11 -0500

    fix(nvim): Re-enable rust_analyzer optimizations

--- nvim/lua/my/lsp/rust.lua
+++ nvim/lua/my/lsp/rust.lua
@@ -1,25 +1,23 @@
 return {
-  -- before_init = function(_, config)
-  --   -- Override clippy to run in its own directory to avoid clobbering caches
-  --   -- but only if target-dir isn't already set in either the command or the extraArgs
-  --   local checkOnSave = config.settings['rust-analyzer'].checkOnSave
-  --   local needle = '--target-dir'
-  --   if string.find(checkOnSave.command, needle, nil, true) then
-  --     return
-  --   end
-  --
-  --   checkOnSave.extraArgs = checkOnSave.extraArgs or {}
-  --   for _, v in pairs(checkOnSave.extraArgs or {}) do
-  --     if string.find(v, needle, nil, true) then
-  --       return
-  --     end
-  --   end
-  --
-  --   p({ 'before', checkOnSave.extraArgs, checkOnSave.command })
-  --   local target_dir = config.root_dir .. '/target/ide-clippy'
-  --   table.insert(checkOnSave.extraArgs, '--target-dir=' .. target_dir)
-  --   p({ 'after', checkOnSave.extraArgs })
-  -- end,
+  before_init = function(_, config)
+    -- Override clippy to run in its own directory to avoid clobbering caches
+    -- but only if target-dir isn't already set in either the command or the extraArgs
+    local checkOnSave = config.settings['rust-analyzer'].checkOnSave
+    local needle = '--target-dir'
+    if string.find(checkOnSave.command, needle, nil, true) then
+      return
+    end
+
+    checkOnSave.extraArgs = checkOnSave.extraArgs or {}
+    for _, v in pairs(checkOnSave.extraArgs or {}) do
+      if string.find(v, needle, nil, true) then
+        return
+      end
+    end
+
+    local target_dir = config.root_dir .. '/target/ide-clippy'
+    table.insert(checkOnSave.extraArgs, '--target-dir=' .. target_dir)
+  end,
   settings = {
     ['rust-analyzer'] = {
       checkOnSave = {
