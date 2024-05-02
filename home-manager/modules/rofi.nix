9567f93a8deb26ec0e92adc8c670628835b986a3
Mat Jones <mat@mjones.network>
2024-05-02 19:39:08 -0400

    feat(nixos): Finalize Rofi config

--- home-manager/modules/rofi.nix
+++ home-manager/modules/rofi.nix
@@ -1,6 +1,24 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   programs.rofi = {
     enable = true;
-    plugins = with pkgs; [ rofimoji rofi-top rofi-calc rofi-power-menu ];
+    catppuccin.enable = true;
+    plugins = with pkgs; [ rofi-top rofi-calc rofi-power-menu ];
+    terminal = "${config.programs.wezterm.package}/bin/wezterm";
+    cycle = true;
+    extraConfig = { modi = "combi,window,drun,calc"; };
+  };
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+      terminal = "";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" =
+      {
+        binding = "<Super>space";
+        command = "rofi -show combi";
+        name = "rofi";
+      };
   };
 }
