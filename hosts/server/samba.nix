58ffc7f39a724694f8c034470eb8c9d2421f4062
Mat Jones <mat@mjones.network>
2024-05-05 16:43:58 -0400

    fix(nixos): Fix samba share config

--- hosts/server/samba.nix
+++ hosts/server/samba.nix
@@ -6,15 +6,9 @@
       securityType = "user";
       openFirewall = true;
       extraConfig = ''
-        workgroup = WORKGROUP
-        server string = smbnix
-        netbios name = smbnix
-        security = user
-        # note: localhost is the ipv6 localhost ::1
         hosts allow = 192.168. 127.0.0.1 localhost
         hosts deny = 0.0.0.0/0
-        guest account = nobody
-        map to guest = bad user
+        browseable = yes
       '';
       shares = {
         jellyfin = {
@@ -24,8 +18,6 @@
           "guest ok" = "yes";
           "create mask" = "0644";
           "directory mask" = "0755";
-          "force user" = "nobody";
-          "force group" = "groupname";
         };
       };
     };
