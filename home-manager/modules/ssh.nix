b7e9de38f0dc7f88a36ae0b97ab7e07faebf3933
Mat Jones <mat@mjones.network>
2024-05-05 14:28:20 -0400

    fix(ssh): Set IP address on nixos-server

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -3,7 +3,10 @@
     enable = true;
     forwardAgent = true;
     matchBlocks = {
-      "nixos-server" = { port = 6969; };
+      "nixos-server" = {
+        port = 6969;
+        hostname = "192.168.146.119";
+      };
       "gitlab.1password.io" = {
         port = 2227;
         hostname = "ssh.gitlab.1password.io";
