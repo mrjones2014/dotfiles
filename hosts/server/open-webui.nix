15d96cb36380399ebe344464d60271059c5273ff
Mat Jones <mat@mjones.network>
2024-05-10 14:31:20 -0400

    fix(nixos): Declaratively create ollama webui data dir

--- hosts/server/open-webui.nix
+++ hosts/server/open-webui.nix
@@ -1,23 +1,29 @@
 # Download LLMs per api
 # curl http://localhost:11434/api/pull -d '{ "name": "llama2" }'
-{ config, ... }: {
+{ config, ... }:
+let dataDir = "${config.users.users.mat.home}/open-webui";
+in {
+  systemd.tmpfiles.rules = [ "d ${dataDir} 055 nobody nobody - -" ];
   virtualisation = {
     podman = {
       enable = true;
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
     };
-  };
-  virtualisation.oci-containers.backend = "podman";
-  virtualisation.oci-containers.containers.open-webui = {
-    autoStart = true;
-    image = "ghcr.io/open-webui/open-webui";
-    ports = [ "3000:8080" ];
-    # TODO figure out how to create the data directory declaratively
-    volumes = [ "${config.users.users.mat.home}/open-webui:/app/backend/data" ];
-    extraOptions =
-      [ "--network=host" "--add-host=host.containers.internal:host-gateway" ];
-    environment = { OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api"; };
+    oci-containers = {
+      backend = "podman";
+      containers.open-webui = {
+        autoStart = true;
+        image = "ghcr.io/open-webui/open-webui";
+        ports = [ "3000:8080" ];
+        volumes = [ "${dataDir}:/app/backend/data" ];
+        extraOptions = [
+          "--network=host"
+          "--add-host=host.containers.internal:host-gateway"
+        ];
+        environment = { OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api"; };
+      };
+    };
   };
   networking.firewall = { allowedTCPPorts = [ 80 443 8080 11434 3000 ]; };
 }
