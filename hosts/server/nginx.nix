b5c62e0093a5c7079fbfbee91be28ae45939e6b7
Mat Jones <mat@mjones.network>
2024-05-10 19:31:20 -0400

    fix(nixos): Set extra headers on reverse proxied services

--- hosts/server/nginx.nix
+++ hosts/server/nginx.nix
@@ -17,6 +17,9 @@
           extraConfig = ''
             proxy_set_header Host $host;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection $http_connection;
           '';
         };
       };
