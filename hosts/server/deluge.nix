125ac3a41be797cfb6bd23d66c3beca5f5d59c9c
Mat Jones <mat@mjones.network>
2024-05-10 17:50:07 -0400

    feat(nixos): Deluge behind mullvad VPN

--- hosts/server/deluge.nix
+++ hosts/server/deluge.nix
@@ -1,22 +1,28 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 let
-  configDir = "/var/delugevpn";
+  configDir = "${config.users.users.mat.home}/delugevpn";
   wireguardConfigPath = config.age.secrets.mullvad_wireguard.path;
 in {
+
   systemd.tmpfiles.rules = [
     "d ${configDir} 055 delugevpn delugevpn - -"
     "d ${configDir}/wireguard 055 delugevpn delugevpn - -"
   ];
   system.activationScripts.copyWireguardConfigIntoContainer.text = ''
-    ${pkgs.podman}/bin/podman cp ${wireguardConfigPath} delugevpn:/config/wireguard/mullvad_wireguard.conf
+    mkdir -p ${configDir}/wireguard && cp ${wireguardConfigPath} ${configDir}/wireguard/mullvad.conf
   '';
+  networking.firewall = {
+    allowedTCPPorts = [ 8112 8118 58846 58946 ];
+    allowedUDPPorts = [ 8112 8118 58846 58946 ];
+  };
   virtualisation.oci-containers = {
     backend = "podman";
     containers = {
       delugevpn = {
-        user = "delugevpn:delugevpn";
-        image = "binhex/arch-delugevpn";
-        extraOptions = [ "--cap-add=NET_ADMIN" ];
+        autoStart = true;
+        image = "ghcr.io/binhex/arch-delugevpn";
+        extraOptions =
+          [ "--sysctl=net.ipv4.conf.all.src_valid_mark=1" "--privileged=true" ];
         ports = [ "8112:8112" "8118:8118" "58846:58846" "58946:58946" ];
         volumes = [ "/mnt/jellyfin:/data" "${configDir}:/config" ];
         environment = {
@@ -26,7 +32,8 @@ in {
           STRICT_PORT_FORWARD = "yes";
           ENABLE_PRIVOXY = "yes";
           LAN_NETWORK = "192.168.1.0/24";
-          NAME_SERVERS = "1.1.1.1";
+          NAME_SERVERS =
+            "84.200.69.80,37.235.1.174,1.1.1.1,37.235.1.177,84.200.70.40,1.0.0.1";
           DELUGE_DAEMON_LOG_LEVEL = "info";
           DELUGE_WEB_LOG_LEVEL = "info";
           DELUGE_ENABLE_WEBUI_PASSWORD = "yes";
