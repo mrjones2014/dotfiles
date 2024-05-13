6bf4de67428e9f68d99e036dd75037bf65feebb0
Mat Jones <mat@mjones.network>
2024-05-13 11:45:46 -0400

    fix(nixos): Allow SSH_AUTH_SOCK to be forwarded on server from ssh client

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,19 +1,19 @@
-{ isLinux, ... }: {
+{ isLinux, isServer, lib, ... }:
+let
+  sshAuthSock = "~/${
+      if isLinux then
+        ".1password"
+      else
+        "Library/Group Containers/2BUA8C4S2C.com.1password/t"
+    }/agent.sock";
+in {
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "$HOME/${lib.strings.removePrefix "~/" sshAuthSock}";
+  };
   programs.ssh = {
     enable = true;
     forwardAgent = true;
     matchBlocks = {
-      "*" = {
-        extraOptions = {
-          IdentityAgent = ''
-            "~/${
-              if isLinux then
-                ".1password"
-              else
-                "Library/Group Containers/2BUA8C4S2C.com.1password/t"
-            }/agent.sock"'';
-        };
-      };
       "nixos-server" = {
         port = 6969;
         hostname = "192.168.146.119";
@@ -26,6 +26,9 @@
         port = 2227;
         hostname = "ssh.gitlab.1password.io";
       };
+    } // lib.optionalAttrs (!isServer) {
+      # allow SSH_AUTH_SOCK to be forwarded on server from SSH client
+      "*" = { extraOptions = { IdentityAgent = ''"${sshAuthSock}"''; }; };
     };
   };
 }
