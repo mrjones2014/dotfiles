610d4e8a47dc408ced9b1119a1378127a25865e5
Mat Jones <mat@mjones.network>
2024-05-13 13:00:56 -0400

    fix(ssh): Use `config.home.homeDirectory` instead of hacky string replace

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,15 +1,14 @@
-{ isLinux, isServer, lib, ... }:
+{ config, isLinux, isServer, lib, ... }:
 let
-  sshAuthSock = "~/${
+  sshAuthSock = "${config.home.homeDirectory}/${
       if isLinux then
         ".1password"
       else
         "Library/Group Containers/2BUA8C4S2C.com.1password/t"
     }/agent.sock";
 in {
-  home.sessionVariables = { } // lib.optionalAttrs (!isServer) {
-    SSH_AUTH_SOCK = "$HOME/${lib.strings.removePrefix "~/" sshAuthSock}";
-  };
+  home.sessionVariables = { }
+    // lib.optionalAttrs (!isServer) { SSH_AUTH_SOCK = sshAuthSock; };
   programs.ssh = {
     enable = true;
     forwardAgent = true;
