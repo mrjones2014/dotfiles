a1d4df920b3ec2a0b86b8c59dd1d8528784491ff
Mat Jones <mat@mjones.network>
2024-05-13 10:54:10 -0400

    feat(nixos): Add systemd job to update oci-containers

---
+++ hosts/server/containers.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+let
+  update-containers = pkgs.writeShellScriptBin "update-containers" ''
+    	SUDO=""
+    	if [[ $(id -u) -ne 0 ]]; then
+    		SUDO="sudo"
+    	fi
+
+        images=$($SUDO ${pkgs.podman}/bin/podman ps -a --format="{{.Image}}" | sort -u)
+
+        for image in $images
+        do
+          $SUDO ${pkgs.podman}/bin/podman pull $image
+        done
+  '';
+in {
+  environment.systemPackages = [ update-containers ];
+  # update oci-containers every Monday
+  systemd = {
+    timers.updatecontainers = {
+      timerConfig = {
+        Unit = "updatecontainers.service";
+        OnCalendar = "Mon 02:00";
+      };
+      wantedBy = [ "timers.target" ];
+    };
+    services = {
+      updatecontainers = {
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "update-containers";
+        };
+      };
+    };
+  };
+}
--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -24,6 +24,7 @@
     ./nginx.nix
     ./content.nix
     ./nas.nix
+    ./containers.nix
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
