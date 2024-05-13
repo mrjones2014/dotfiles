6d6b1efe992cd77b7d7173b7911bf86b989984ef
Mat Jones <mat@mjones.network>
2024-05-13 09:34:56 -0400

    fix(wezterm): Set SSH auth sock in SSH config so that `wezterm ssh` works

--- home-manager/modules/_1password-shell.nix
+++ home-manager/modules/_1password-shell.nix
@@ -17,49 +17,11 @@ in {
   imports = [ inputs._1password-shell-plugins.hmModules.default ];
   programs = {
     fish = {
-      shellInit = ''
-        # Setting up SSH_AUTH_SOCK here rather than ~/.ssh/config
-        # because that overrides the environment variables,
-        # meaning I can't easily switch between the production and
-        # debug auth sockets while working on the 1Password desktop app
-        set -g -x SSH_TTY (tty)
-        if [ "$(uname)" = Darwin ]
-          set -g -x SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-        else
-          set -g -x SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
-        end
-      '';
       interactiveShellInit = if isDarwin then ''
         export SUDO_ASKPASS="${op_sudo_password_script}"
         alias sudo="sudo -A"
       '' else
         "";
-      functions.opauthsock = {
-        argumentNames = [ "mode" ];
-        description =
-          "Configure 1Password SSH agent to use production or debug socket path";
-        body = ''
-          # complete -c opauthsock -n __fish_use_subcommand -xa prod -d 'use production socket path'
-          # complete -c opauthsock -n __fish_use_subcommand -xa debug -d 'use debug socket path'
-          # complete -c opauthsock -n 'not __fish_use_subcommand' -f
-
-            if test -z $mode
-              echo $SSH_AUTH_SOCK
-            else
-              set -f prefix "$HOME/.1password"
-              if [ "$(uname)" = Darwin ]
-                set -f prefix "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password"
-              end
-              echo "setting ssh auth sock to: $mode"
-              switch $mode
-                case prod
-                  set -g -x SSH_AUTH_SOCK "$prefix/t/agent.sock"
-                case debug
-                  set -g -x SSH_AUTH_SOCK "$prefix/t/debug/agent.sock"
-              end
-            end
-        '';
-      };
     };
     _1password-shell-plugins = {
       enable = true;
--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,8 +1,19 @@
-{
+{ isLinux, ... }: {
   programs.ssh = {
     enable = true;
     forwardAgent = true;
     matchBlocks = {
+      "*" = {
+        extraOptions = {
+          IdentityAgent = ''
+            "~/${
+              if isLinux then
+                ".1password"
+              else
+                "Library/Group Containers/2BUA8C4S2C.com.1password/t"
+            }/agent.sock"'';
+        };
+      };
       "nixos-server" = {
         port = 6969;
         hostname = "192.168.146.119";
