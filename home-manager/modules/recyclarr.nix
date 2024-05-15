9746d5c7e8a2f6297770d65fe4b83c186d8b2fec
Mat Jones <mat@mjones.network>
2024-05-15 19:55:49 -0400

    feat(nixos): Recyclarr

--- home-manager/modules/recyclarr.nix
+++ home-manager/modules/recyclarr.nix
@@ -3,8 +3,8 @@
     pkgs.recyclarr
     (pkgs.writeScriptBin "recyclarr-sync" ''
       op inject -i ~/git/dotfiles/recyclarr.yml -o ~/git/dotfiles/recyclarr-tmp.yml \
-        && recyclarr sync --config ~/git/dotfiles/recyclarr-tmp.yml \
-        && rm ~/git/dotfiles/recyclarr-tmp.yml
+        && recyclarr sync --config ~/git/dotfiles/recyclarr-tmp.yml
+      rm ~/git/dotfiles/recyclarr-tmp.yml
     '')
   ];
 }
