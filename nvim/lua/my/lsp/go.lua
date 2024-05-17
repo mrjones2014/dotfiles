ff69f9d9a0a7b7d3c3228830678e49a5e7ee811e
Mat Jones <mat@mjones.network>
2024-05-17 12:58:54 -0400

    feat(nvim): Enable inlay hints for gopls

---
+++ nvim/lua/my/lsp/go.lua
@@ -0,0 +1,14 @@
+return {
+  settings = {
+    gopls = {
+      hints = {
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+}
