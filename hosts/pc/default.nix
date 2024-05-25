c9e84b0c664f4c0398b6b15fa63d835f40f4f13e
Mat Jones <mat@mjones.network>
2024-05-25 18:12:33 -0400

    Revert "feat(nixos): Add nix-ld to support Nexus Mods AppImage"
    This reverts commit 605ddf8dc6e125cc60617a801baf913595924b86.

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -34,19 +34,6 @@
   programs = {
     fish.enable = true;
 
-    # For Nexus Mods AppImage
-    nix-ld = {
-      enable = true;
-      libraries = with pkgs; [
-        icu
-        fuse
-        fontconfig
-        xorg.libX11
-        xorg.libICE
-        xorg.libSM
-      ];
-    };
-
     steam = {
       enable = true;
       remotePlay.openFirewall = true;
