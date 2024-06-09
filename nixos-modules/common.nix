262e1b5743210da82358ec8622787caefa694827
Mat Jones <mat@mjones.network>
2024-06-09 14:03:19 -0400

    fix(nixos): Remove unneeded DNS config

--- nixos-modules/common.nix
+++ nixos-modules/common.nix
@@ -56,16 +56,6 @@
       services.sudo.sshAgentAuth = true;
     };
   };
-  services.resolved = {
-    enable = true;
-    dnsovertls = true;
-    extraConfig = ''
-      DNS=45.90.28.0#a6416b.dns.nextdns.io
-      DNS=2a07:a8c0::#a6416b.dns.nextdns.io
-      DNS=45.90.30.0#a6416b.dns.nextdns.io
-      DNS=2a07:a8c1::#a6416b.dns.nextdns.io
-    '';
-  };
 
   # Set your time zone.
   time.timeZone = "America/New_York";
