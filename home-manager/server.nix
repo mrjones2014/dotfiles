02beb7bfbeb0301c68d6237596b6e2cf5d6fc2ce
Mat Jones <mat@mjones.network>
2024-07-12 10:15:21 -0400

    feat(nixos): Add wireguard-tools on server

--- home-manager/server.nix
+++ home-manager/server.nix
@@ -14,6 +14,7 @@
       TERM = "xterm-256color";
       COLORTERM = "truecolor";
     };
+    packages = [ pkgs.wireguard-tools ];
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
     # introduces backwards incompatible changes.
