7c54c7ccc8c9d9485970985cc94b61feb9842ec7
Mat Jones <mat@mjones.network>
2024-11-25 13:02:30 -0500

    fix(nvim): set cursor_follows_swapped_bufs in smart-splits.nvim

--- nvim/lua/my/configure/smart-splits.lua
+++ nvim/lua/my/configure/smart-splits.lua
@@ -2,5 +2,5 @@ return {
   'mrjones2014/smart-splits.nvim',
   dev = true,
   lazy = false,
-  opts = { ignored_buftypes = { 'nofile' } },
+  opts = { ignored_buftypes = { 'nofile' }, cursor_follows_swapped_bufs = true },
 }
