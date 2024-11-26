0362ca575e1495512eb88310154f1c290ee1df15
Mat <mat@mjones.network>
2024-11-26 08:07:04 -0500

    fix(nixos): fix typo option in sshd.nix

--- nixos-modules/sshd.nix
+++ nixos-modules/sshd.nix
@@ -12,7 +12,7 @@
     };
   };
 
-  user.users.mat.openssh.authorizedKeys.keys = [
+  users.users.mat.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsT6GLG7sY8YKX7JM+jqS3EAti3YMzwHKWViveqkZvu"
   ];
 }
