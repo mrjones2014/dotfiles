a89f932c35ca3985d5776d64b14c9702289ea38a
Mat Jones <mat@mjones.network>
2024-12-02 13:52:40 -0500

    chore(nixos,server): Move secrets to secrets/ directory

--- hosts/server/secrets.nix
+++ hosts/server/secrets.nix
@@ -1,9 +1,9 @@
 {
   age = {
     secrets = {
-      mullvad_wireguard.file = ../../mullvad_wireguard.age;
-      homepage.file = ../../homepage.age;
-      wireguard_server.file = ../../wireguard_server.age;
+      mullvad_wireguard.file = ../../secrets/mullvad_wireguard.age;
+      homepage.file = ../../secrets/homepage.age;
+      wireguard_server.file = ../../secrets/wireguard_server.age;
     };
   };
 }
