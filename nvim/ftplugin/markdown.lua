db88dd06401d86cba508730397fcf14773af4564
Mat Jones <mat@mjones.network>
2024-12-04 13:15:43 -0500

    fix(nvim): Fix markdown ftplugin

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,2 +1,15 @@
-vim.api.nvim_set_option_value('wrap', true, { buf = 0 })
-vim.api.nvim_set_option_value('linebreak', true, { buf = 0 })
+vim.api.nvim_set_option_value('wrap', true, { win = 0 })
+vim.api.nvim_set_option_value('linebreak', true, { win = 0 })
+
+-- set up autocmd to revert the above options when another filetype is focused
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  callback = function()
+    if vim.bo.ft == 'markdown' then
+      vim.api.nvim_set_option_value('wrap', true, { win = 0 })
+      vim.api.nvim_set_option_value('linebreak', true, { win = 0 })
+    else
+      vim.api.nvim_set_option_value('wrap', false, { win = 0 })
+      vim.api.nvim_set_option_value('linebreak', false, { win = 0 })
+    end
+  end,
+})
