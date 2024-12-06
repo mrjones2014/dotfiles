bb3f5eb94555da5dad252134a7d172d7cd3d33fd
Mat Jones <mat@mjones.network>
2024-12-06 12:54:44 -0500

    chore: move recyclarr.yml -> conf.d/recyclarr.yml

--- home-manager/modules/recyclarr.nix
+++ home-manager/modules/recyclarr.nix
@@ -2,9 +2,9 @@
   home.packages = [
     pkgs.recyclarr
     (pkgs.writeScriptBin "recyclarr-sync" ''
-      op inject -i ~/git/dotfiles/recyclarr.yml -o ~/git/dotfiles/recyclarr-tmp.yml \
-        && recyclarr sync --config ~/git/dotfiles/recyclarr-tmp.yml
-      rm ~/git/dotfiles/recyclarr-tmp.yml
+      op inject -i ~/git/dotfiles/conf.d/recyclarr.yml -o ~/git/dotfiles/recyclarr-tmp.yml \
+        && recyclarr sync --config ~/git/dotfiles/conf.d/recyclarr-tmp.yml
+      rm -f ~/git/dotfiles/conf.d/recyclarr-tmp.yml
     '')
   ];
 }
