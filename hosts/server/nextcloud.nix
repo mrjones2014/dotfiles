0f445a678d9e82489889e05ea87fdb2e0f5536f5
Mat Jones <mat@mjones.network>
2025-01-06 17:09:02 -0500

    feat(nextcloud): Tweak some settings

--- hosts/server/nextcloud.nix
+++ hosts/server/nextcloud.nix
@@ -14,11 +14,29 @@ in {
         calendar cospend tasks groupfolders memories notes previewgenerator;
     };
     extraAppsEnable = true;
+    autoUpdateApps.enable = true;
     configureRedis = true;
     maxUploadSize = "1G";
     hostName = "192.168.189.2";
     home = "/mnt/nextcloud";
-    config.adminpassFile = config.age.secrets.nextcloud_admin_pass.path;
+    config = { adminpassFile = config.age.secrets.nextcloud_admin_pass.path; };
+    settings = {
+      default_phone_region = "EN";
+      enabled_preview_providers = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
+      maintenance_window_start = 6;
+    };
   };
   # see https://nixos.wiki/wiki/Nextcloud#Change_default_listening_port
   services.nginx.virtualHosts."192.168.189.2".listen = [{
