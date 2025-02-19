8debc19e9cb9c3761d8f31041c09da4f99fb0d59
Mat Jones <mat@mjones.network>
2025-02-19 13:31:39 -0500

    fix(nvim): Disable auto pairs in pickers

--- nvim/lua/my/configure/autopairs.lua
+++ nvim/lua/my/configure/autopairs.lua
@@ -6,7 +6,9 @@ return {
       local Rule = require('nvim-autopairs.rule')
       local npairs = require('nvim-autopairs')
       local cond = require('nvim-autopairs.conds')
-      npairs.setup({})
+      npairs.setup({
+        disable_in_filetype = { 'snacks_picker_input' },
+      })
       -- <> pair for generics and stuff,
       -- complete <> if the preceding text is alphanumeric or :: for Rust
       npairs.add_rule(Rule('<', '>', {
