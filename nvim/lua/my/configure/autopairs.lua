3ba1382349daeea76f32895ae294fbcf128cfddd
Mat Jones <mat@mjones.network>
2025-02-24 13:56:56 -0500

    fix(nvim): Fix autopairs config

--- nvim/lua/my/configure/autopairs.lua
+++ nvim/lua/my/configure/autopairs.lua
@@ -7,7 +7,7 @@ return {
       local npairs = require('nvim-autopairs')
       local cond = require('nvim-autopairs.conds')
       npairs.setup({
-        disable_in_filetype = { 'snacks_picker_input' },
+        disable_filetype = { 'snacks_picker_input' },
       })
       -- <> pair for generics and stuff,
       -- complete <> if the preceding text is alphanumeric or :: for Rust
