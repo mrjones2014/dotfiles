75ffb09ffe60cef18c3b22b2ec4c65a0a942f348
Mat Jones <mat@mjones.network>
2025-04-13 21:24:26 -0400

    fix(nixos-server): Fix host keys config

--- secrets.nix
+++ secrets.nix
@@ -2,6 +2,14 @@
 # it is only used by the agenix CLI to determine which
 # keys to use to encrypt secrets.
 let
+  # my public key
+  users = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXHRx83f5MWdhcEHXduTINyUu6yqd2eOgZHE0XNYFlO mat@nixos-server"
+  ];
+  # server host key
+  systems = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUa3f8x3mb2fHF5JXjGKdWF5EUX8GQj7hMhEUn7LffI root@nixos-server"
+  ];
   secrets = [
     "secrets/mullvad_wireguard.age"
     "secrets/wireguard_server.age"
@@ -12,6 +20,6 @@ in
 builtins.listToAttrs (map
   (secret: {
     name = secret;
-    value = { publicKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUa3f8x3mb2fHF5JXjGKdWF5EUX8GQj7hMhEUn7LffI root@nixos-server" ]; };
+    value = { publicKeys = users ++ systems; };
   })
   secrets)
