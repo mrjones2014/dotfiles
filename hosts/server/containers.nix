f56f86b5ed301dbdbc1a6c7d00af11d23baa69d9
Mat Jones <mat@mjones.network>
2025-04-13 20:23:16 -0400

    chore: update podman config

--- hosts/server/containers.nix
+++ hosts/server/containers.nix
@@ -15,7 +15,15 @@ let
   '';
 in
 {
-  virtualisation.oci-containers.backend = "podman";
+  virtualisation = {
+    oci-containers.backend = "podman";
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
   environment.systemPackages = [ update-containers ];
   # update oci-containers every Monday
   systemd = {
