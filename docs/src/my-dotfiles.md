8961f67726fa70d7b1f40f4fdda6d7fb422c8153
Mat Jones <mat@mjones.network>
2025-04-15 16:48:37 -0400

    fix: Update docs and set default Hammerspoon config file via nix-darwin

--- docs/src/my-dotfiles.md
+++ docs/src/my-dotfiles.md
@@ -13,31 +13,20 @@ Once you've got your disks partitioned and NixOS installed, come back here and c
 since we'll be installing GNOME anyway.
 
 Simply run `NIX_CONFIG="experimental-features = nix-command flakes" sudo nixos-rebuild switch ~/git/dotfiles/.#pc` (replacing `.#pc` with your desired flake output target).
-After the first time, you won't need the `NIX_CONFIG=` part, as the flake will set that up in `$XDG_CONFIG_HOME/nix/nix.conf`.
+After the first time, you can just run `sudo nixos-rebuild switch --flake $HOME/git/dotfiles/.#pc` (or the `nix-apply` shell alias).
 
 ## macOS
 
-On macOS, you will need to [install the `home-manager` CLI manually](https://nix-community.github.io/home-manager/index.xhtml#sec-install-standalone).
-Then, you can run `home-manager switch --flake ~/git/dotfiles/.#pc` (replacing `.#pc` with your desired flake output target).
+On macOS, for the first install, you'll need to run `nix-darwin` via `nix run`:
 
-On macOS, you should then also run the following:
-
-```sh
-# configure Hammerspoon.app to look for its config file in `$XDG_CONFIG_HOME/hammerspoon` instead of `$HOME/.hammerspoon`
-defaults write org.hammerspoon.Hammerspoon MJConfigFile "${XDG_CONFIG_HOME:-$HOME/.config}/hammerspoon/init.lua"
-# Run a script to set up a lot of macOS settings to more sensible defaults
-~/git/dotfiles/conf.d/setup-macos-defaults.bash
+```bash
+nix run nix-darwin/master#darwin-rebuild -- switch --extra-experimental-features "nix-command flakes" --flake $HOME/git/dotfiles
 ```
 
-On macOS, you also have to manually change the default shell to Fish.
-
-- Run `which fish` to get the filepath to the Fish binary (should be something like `$HOME/.nix-profile/bin/fish`)
-- Run `sudo vi /etc/shells` and add the output of `which fish` (usually this should be something like `$HOME/.nix-profile/bin/fish`) as an entry, then write the file and quit
-- Run `chsh -s $(which fish)`
-- Log out and back in
+After that, you can just run `darwin-rebuild switch --flake $HOME/git/dotfiles` (or the `nix-apply` shell alias).
 
 ## Managing Dotfiles
 
 Dotfiles are managed via [Nix](https://nixos.org/), using `flake.nix` and [`home-manager`](https://github.com/nix-community/home-manager).
 On NixOS, the `home-manager` configuration is managed as a NixOS module, so simply rebuilding your NixOS config also applies the new
-`home-manager` config. On macOS, you only have `nix` and `home-manager` so use the `home-manager` CLI directly.
+`home-manager` config. On macOS, the same behavior is achieved via [nix-darwin](https://github.com/nix-darwin/nix-darwin).
--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,61 +1,68 @@
-{ config, ... }: {
-  system.stateVersion = 6;
+{ config, pkgs, ... }: {
   imports = [ ../../nixos-modules/nix-conf.nix ];
   nixpkgs.hostPlatform = "aarch64-darwin";
   users.users.mat = {
     name = "mat";
     home = "/Users/mat";
+    shell = pkgs.fish;
   };
+  system = {
+    stateVersion = 6;
 
-  system.defaults = {
-    screencapture = {
-      # store screenshots in ~/Downloads by default
-      location = "${config.users.users.mat.home}/Downloads";
-      # only copy screenshots to clipboard by default
-      target = "clipboard";
-    };
-    NSGlobalDomain = {
-      # expanded save panel by default
-      NSNavPanelExpandedStateForSaveMode = true;
-      NSNavPanelExpandedStateForSaveMode2 = true;
-      # do not save to icloud by default
-      NSDocumentSaveNewDocumentsToCloud = false;
-      # Display ASCII control characters using caret notation in standard text views
-      # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
-      NSTextShowsControlCharacters = true;
-      # Disable "natural" scrolling (it should be called unnatural scrolling)
-      "com.apple.swipescrolldirection" = false;
-      # Trackpad: enable tap to click
-      "com.apple.mouse.tapBehavior" = 1;
-      # Enable full keyboard access for all controls
-      # (e.g. enable Tab in modal dialogs)
-      AppleKeyboardUIMode = 3;
-    };
-    finder = {
-      AppleShowAllFiles = true;
-      AppleShowAllExtensions = true;
-      ShowPathbar = true;
-    };
-    dock = {
-      show-process-indicators = false;
-      show-recents = false;
-      # Don't show dashboard as a space
-      dashboard-in-overlay = true;
-      # Don't rearrange spaces based on most recently used
-      mru-spaces = false;
-      autohide = true;
-      # Disable all hot corners
-      wvous-tl-corner = 1;
-      wvous-tr-corner = 1;
-      wvous-bl-corner = 1;
-      wvous-br-corner = 1;
-    };
-    ActivityMonitor = {
-      OpenMainWindow = true;
-      # show CPU usage graph on icon
-      IconType = 5;
-      # Show all processes
-      ShowCategory = 100;
+    activationScripts.hammerspoonConfigFilePath.text = ''
+      defaults write org.hammerspoon.Hammerspoon MJConfigFile "''${XDG_CONFIG_HOME:-$HOME/.config}/hammerspoon/init.lua"
+    '';
+
+    defaults = {
+      screencapture = {
+        # store screenshots in ~/Downloads by default
+        location = "${config.users.users.mat.home}/Downloads";
+        # only copy screenshots to clipboard by default
+        target = "clipboard";
+      };
+      NSGlobalDomain = {
+        # expanded save panel by default
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSNavPanelExpandedStateForSaveMode2 = true;
+        # do not save to icloud by default
+        NSDocumentSaveNewDocumentsToCloud = false;
+        # Display ASCII control characters using caret notation in standard text views
+        # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
+        NSTextShowsControlCharacters = true;
+        # Disable "natural" scrolling (it should be called unnatural scrolling)
+        "com.apple.swipescrolldirection" = false;
+        # Trackpad: enable tap to click
+        "com.apple.mouse.tapBehavior" = 1;
+        # Enable full keyboard access for all controls
+        # (e.g. enable Tab in modal dialogs)
+        AppleKeyboardUIMode = 3;
+      };
+      finder = {
+        AppleShowAllFiles = true;
+        AppleShowAllExtensions = true;
+        ShowPathbar = true;
+      };
+      dock = {
+        show-process-indicators = false;
+        show-recents = false;
+        # Don't show dashboard as a space
+        dashboard-in-overlay = true;
+        # Don't rearrange spaces based on most recently used
+        mru-spaces = false;
+        autohide = true;
+        # Disable all hot corners
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+      };
+      ActivityMonitor = {
+        OpenMainWindow = true;
+        # show CPU usage graph on icon
+        IconType = 5;
+        # Show all processes
+        ShowCategory = 100;
+      };
     };
   };
 }
