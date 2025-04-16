74550cf4bfd9757fc8f2f6d559467c9dfc89060a
Mat Jones <mat@mjones.network>
2025-04-16 09:40:09 -0400

    feat(nvim): Add grug-far.nvim
    Fixes #195

---
+++ nvim/lua/my/configure/grug_far.lua
@@ -0,0 +1,32 @@
+return {
+  'MagicDuck/grug-far.nvim',
+  cmd = { 'GrugFar', 'GrugFarWithin' },
+  init = function()
+    vim.g.maplocalleader = ';'
+  end,
+  keys = {
+    {
+      '<leader><leader>s',
+      function()
+        require('grug-far').open({})
+      end,
+      mode = 'n',
+      desc = 'Search and replace',
+    },
+    {
+      '<leader><leader>f',
+      function()
+        require('grug-far').open({ prefills = { paths = vim.fn.expand('%') } })
+      end,
+      desc = 'Search and replace in current file',
+    },
+    {
+      '<leader><leader>s',
+      function()
+        require('grug-far').open({ visualSelectionUsage = 'operate-within-range' })
+      end,
+      mode = 'v',
+      desc = 'Search and replace visual selection',
+    },
+  },
+}
