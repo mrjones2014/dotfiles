54eb0e56e2561ba05127ef12661bc6b4a6eeec99
Mat Jones <mat@mjones.network>
2025-04-22 16:33:39 -0400

    fix(nvim): Fix highlight flashing

--- nvim/init.lua
+++ nvim/init.lua
@@ -92,7 +92,6 @@ end
 if vim.env.SSH_TTY then
   vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-      vim.highlight.on_yank()
       local copy_to_unnamedplus = require('vim.ui.clipboard.osc52').copy('+')
       copy_to_unnamedplus(vim.v.event.regcontents)
       local copy_to_unnamed = require('vim.ui.clipboard.osc52').copy('*')
