d710840ece18e3fc93548874b445873ab0a5e212
Mat Jones <mat@mjones.network>
2025-04-22 16:32:36 -0400

    fix(nvim): (kinda) use OSC52 to copy over SSH

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,5 +87,19 @@ vim.ui.open = function(uri) ---@diagnostic disable-line: duplicate-set-field
   open(uri)
 end
 
+-- if in SSH session, copy to local system clipboard using OSC52
+-- See: https://github.com/neovim/neovim/discussions/28010#discussioncomment-9529001
+if vim.env.SSH_TTY then
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+      vim.highlight.on_yank()
+      local copy_to_unnamedplus = require('vim.ui.clipboard.osc52').copy('+')
+      copy_to_unnamedplus(vim.v.event.regcontents)
+      local copy_to_unnamed = require('vim.ui.clipboard.osc52').copy('*')
+      copy_to_unnamed(vim.v.event.regcontents)
+    end,
+  })
+end
+
 -- set up UI tweaks on load
 require('my.utils.lsp').apply_ui_tweaks()
