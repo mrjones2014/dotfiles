f1bbb3a60a928cfc6e8c9f1941c5b8a3f09ae26c
Mat Jones <mat@mjones.network>
2025-04-23 15:40:59 -0400

    fix(nixos): Fix use of makeDesktopItem

--- home-manager/components/vencord.nix
+++ home-manager/components/vencord.nix
@@ -1,11 +1,11 @@
-{ isLinux, pkgs, lib, ... }: {
+{ isLinux, pkgs, lib, makeDesktopItem, ... }: {
   xdg.dataFile."icons/discord.png".source = ./discord.png;
   home.packages = lib.lists.optionals isLinux [
     # vesktop discord client, I don't like
     # vesktop's icon, so override it
     (pkgs.vesktop.overrideAttrs (oldAttrs: {
       desktopItems = [
-        (lib.makeDesktopItem {
+        (makeDesktopItem {
           name = "vesktop";
           desktopName = "Discord";
           exec = "vesktop %U";
