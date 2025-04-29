411fb838696ad9fea884025f38ad547ab8c93043
Mat Jones <mat@mjones.network>
2025-04-29 16:31:58 -0400

    feat(darwin): Add Cody CLI via brew

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -7,7 +7,12 @@
     home = "/Users/mat";
     shell = pkgs.fish;
   };
-  system = {
-    stateVersion = 6;
+  environment.variables.HOMEBREW_NO_ANALYTICS = "1";
+  environment.systemPath = [ "/opt/homebrew/bin" ];
+  homebrew = {
+    enable = true;
+    taps = [ "kiraum/tap" ];
+    brews = [ "kiraum/tap/cody" ];
   };
+  system.stateVersion = 6;
 }
