2af26c0de383971853dcd083ed2f0bf281a0d27b
Mat Jones <mat@mjones.network>
2025-04-29 11:44:50 -0400

    fix(nixos-server): Fix silverbullet offline mode

--- hosts/server/silverbullet.nix
+++ hosts/server/silverbullet.nix
@@ -5,7 +5,6 @@ in
 {
   systemd.tmpfiles.rules = [ "d /var/lib/silverbullet 0770 root podman - -" ];
   age.secrets.silverbullet_env.file = ../../secrets/silverbullet_env.age;
-  services.nginx.subdomains.silverbullet.port = port;
   virtualisation.oci-containers.containers.silverbullet = {
     image = "ghcr.io/silverbulletmd/silverbullet:v2";
     autoStart = true;
@@ -15,6 +14,51 @@ in
     environment = {
       SB_HOSTNAME = "0.0.0.0";
       SB_PORT = toString port;
+    };
+  };
+
+  # custom nginx virtual host to ensure PWA works offline
+  services.nginx = {
+    enable = true;
+    virtualHosts."silverbullet.mjones.network" = {
+      forceSSL = true;
+      useACMEHost = "mjones.network";
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:${toString port}";
+          proxyWebsockets = true;
+          extraConfig = ''
+            add_header Service-Worker-Allowed "/";
+          '';
+        };
+
+        # Cache config otherwise offline does not work
+        "/.config" = {
+          proxyPass = "http://127.0.0.1:${toString port}";
+          extraConfig = ''
+            add_header Cache-Control "public, max-age=86400, immutable";
+            add_header Access-Control-Allow-Origin "*";
+          '';
+        };
+
+        # Cache static assets
+        "~ \\.(css|js|png|jpg|woff2|svg)$" = {
+          proxyPass = "http://127.0.0.1:${toString port}";
+          extraConfig = ''
+            add_header Cache-Control "public, max-age=604800";
+          '';
+        };
+
+        # Cacheable service worker with update control
+        "/service-worker.js" = {
+          proxyPass = "http://127.0.0.1:${toString port}";
+          # This allows caching but checks for updates when online
+          extraConfig = ''
+            add_header Cache-Control "public, max-age=0";
+            add_header Service-Worker-Allowed "/";
+          '';
+        };
+      };
     };
   };
 }
