a686d63fa59f24694fb528510e0143d7435ed578
Mat Jones <mat@mjones.network>
2025-04-28 21:57:39 -0400

    fix(nixos): Fix Zen dock icon

--- home-manager/components/gnome/dconf.nix
+++ home-manager/components/gnome/dconf.nix
@@ -35,7 +35,7 @@ in
         "org.gnome.Settings.desktop"
         "org.gnome.Nautilus.desktop"
         "spotify.desktop"
-        "zen.desktop"
+        "zen-beta.desktop"
         "com.mitchellh.ghostty.desktop"
         "standard-notes.desktop"
         "1password.desktop"
