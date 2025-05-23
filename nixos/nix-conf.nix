3e79e2fd02248123791dda67a8a9ab462cd68f9c
Mat Jones <mat@mjones.network>
2025-05-23 11:07:10 -0400

    fix(nix): Fix nix-community cachix config

--- nixos/nix-conf.nix
+++ nixos/nix-conf.nix
@@ -9,10 +9,8 @@
   nix = {
     package = lib.mkDefault pkgs.lix;
     settings = {
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-      ];
+      substituters = [ "https://nix-community.cachix.org" ];
+      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       trusted-users = [
         "root"
         "mat"
