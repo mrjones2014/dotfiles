d17dd200a0872882e3dad33a64a767bab411b189
Mat Jones <mat@mjones.network>
2025-05-27 10:35:39 -0400

    fix(darwin): Explicitly list cache.nixos.org as a substituter

--- nixos/nix-conf.nix
+++ nixos/nix-conf.nix
@@ -9,8 +9,14 @@
   nix = {
     package = lib.mkDefault pkgs.lix;
     settings = {
-      substituters = [ "https://nix-community.cachix.org" ];
-      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       trusted-users = [
         "root"
         "mat"
