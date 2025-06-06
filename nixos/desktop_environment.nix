b0acac8e65491507bea08c6442ee1206e5139868
Mat Jones <mat@mjones.network>
2025-06-05 20:31:17 -0400

    chore(nixos): Remove outdated nix overlay for GNOME

--- nixos/desktop_environment.nix
+++ nixos/desktop_environment.nix
@@ -1,27 +1,5 @@
 { pkgs, ... }:
 {
-  nixpkgs.overlays = [
-    # GNOME 46: triple-buffering-v4-46
-    # See:
-    # - https://nixos.wiki/wiki/GNOME#Dynamic_triple_buffering
-    # - https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441
-    (final: prev: {
-      gnome = prev.gnome.overrideScope (
-        gnomeFinal: gnomePrev: {
-          mutter = gnomePrev.mutter.overrideAttrs (old: {
-            src = pkgs.fetchFromGitLab {
-              domain = "gitlab.gnome.org";
-              owner = "vanvugt";
-              repo = "mutter";
-              rev = "triple-buffering-v4-46";
-              hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
-            };
-          });
-        }
-      );
-    })
-  ];
-
   # Make electron apps detect wayland properly
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   services = {
