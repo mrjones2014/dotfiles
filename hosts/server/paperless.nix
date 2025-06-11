dc258be8111d2f08c53ac11b77f5c880e74f3472
Mat Jones <mat@mjones.network>
2025-06-11 16:31:14 -0400

    fix(nixos): Temporarily disable paperless-ngx until issue is resolved

--- hosts/server/paperless.nix
+++ hosts/server/paperless.nix
@@ -3,7 +3,11 @@
   services.nginx.subdomains.paperless.port = config.services.paperless.port;
   age.secrets.paperless_admin_pw.file = ../../secrets/paperless_admin_pw.age;
   services.paperless = {
-    enable = true;
+    # FIXME: paperless-ngx currently dumps core and spam-restarts itself pinning CPU usage to 100%
+    # https://github.com/NixOS/nixpkgs/issues/414214
+    # https://github.com/NixOS/nixpkgs/pull/414234
+    # https://nixpk.gs/pr-tracker.html?pr=414234
+    enable = false;
     passwordFile = config.age.secrets.paperless_admin_pw.path;
     database.createLocally = true;
     settings.PAPERLESS_URL = "https://paperless.mjones.network";
