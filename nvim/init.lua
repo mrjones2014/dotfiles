593f2d8a8318b2871554fd762d6ffa55ff59ca3c
Mat Jones <mat@mjones.network>
2025-06-26 10:00:05 -0400

    chore(nvim): Simplify _G.dbg function

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,25 +3,7 @@ vim.loader.enable()
 ---Debug Lua stuff and print a nice debug message via `vim.inspect`.
 ---@param ... any
 _G.dbg = function(...)
-  local info = debug.getinfo(2, 'S')
-  local source = info.source:sub(2)
-  source = vim.loop.fs_realpath(source) or source
-  source = vim.fn.fnamemodify(source, ':~:.') .. ':' .. info.linedefined
-  local what = { ... }
-  if vim.islist(what) and vim.tbl_count(what) <= 1 then
-    what = what[1]
-  end
-  local msg = vim.inspect(vim.deepcopy(what))
-  vim.notify(msg, vim.log.levels.INFO, {
-    title = 'Debug: ' .. source,
-    on_open = function(win)
-      vim.wo[win].conceallevel = 3
-      vim.wo[win].concealcursor = ''
-      vim.wo[win].spell = false
-      local buf = vim.api.nvim_win_get_buf(win)
-      vim.treesitter.start(buf, 'lua')
-    end,
-  })
+  require('snacks.debug').inspect(...)
 end
 
 require('my.settings')
