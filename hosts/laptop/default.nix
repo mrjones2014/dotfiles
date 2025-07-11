be9c207c93830e3a08acccd7983aaf8bde6aa4ab
Mat Jones <mat@mjones.network>
2025-07-11 16:00:25 -0400

    fix(nixbook): Fix WireGuard ports

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -8,12 +8,12 @@
       logReversePathDrops = true;
       # wireguard trips rpfilter up
       extraCommands = ''
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 9999 -j RETURN
-        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 9999 -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
       '';
       extraStopCommands = ''
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 9999 -j RETURN || true
-        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 9999 -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
       '';
     };
   };
