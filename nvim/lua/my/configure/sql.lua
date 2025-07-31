8d0527b9ab6a0b4652e6b9a0782e0dd36ea1884f
Mat Jones <mat@mjones.network>
2025-07-31 15:40:38 -0400

    fix(nvim): Disable keymaps from dadbod-ui in sql buffers

--- nvim/lua/my/configure/sql.lua
+++ nvim/lua/my/configure/sql.lua
@@ -1,10 +1,5 @@
 return {
-  {
-    'kristijanhusak/vim-dadbod-ui',
-    init = function()
-      vim.g.db_ui_disable_mappings_sql = 1
-    end,
-  },
+  'kristijanhusak/vim-dadbod-ui',
   dependencies = {
     { 'tpope/vim-dadbod', lazy = true },
     { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
@@ -17,5 +12,6 @@ return {
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_disable_mappings_sql = 1
   end,
 }
