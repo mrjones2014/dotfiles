16f98c4a224f66940c803b071b3aa6cfdd954ce6
Mat Jones <mat@mjones.network>
2025-08-02 14:56:58 -0400

    fix(nix): Remove trusted users config

--- nixos/nix-conf.nix
+++ nixos/nix-conf.nix
@@ -2,7 +2,6 @@
   pkgs,
   lib,
   inputs,
-  isDarwin,
   ...
 }:
 {
@@ -16,10 +15,6 @@
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      trusted-users = [
-        "root"
-        (if isDarwin then "mat.jones" else "mat") # username is set by MDM on work mac :/
       ];
       keep-outputs = true;
       keep-derivations = true;
