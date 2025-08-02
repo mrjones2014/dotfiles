9b40e2bb69376f9bc50cb23835850d25ea88814f
Mat Jones <mat@mjones.network>
2025-08-02 17:37:27 -0400

    fix(nix): Separate nix config into root and user sections

--- nixos/nix-conf.nix
+++ nixos/nix-conf.nix
@@ -2,19 +2,29 @@
   pkgs,
   lib,
   inputs,
+  osConfig ? null,
   ...
 }:
+let
+  isHomeManager = osConfig != null;
+  cachix = {
+    url = "https://mrjones2014-dotfiles.cachix.org";
+    public-key = "mrjones2014-dotfiles.cachix.org-1:c66wfzthG6KZEWnltlzW/EjhlH9FwUVi5jM4rVD1Rw4=";
+  };
+in
 {
   nix = {
     package = lib.mkDefault pkgs.lixPackageSets.latest.lix;
     settings = {
-      trusted-substituters = [ "https://mrjones2014-dotfiles.cachix.org" ];
-      trusted-public-keys = [
-        "mrjones2014-dotfiles.cachix.org-1:c66wfzthG6KZEWnltlzW/EjhlH9FwUVi5jM4rVD1Rw4="
-      ];
+      extra-substituters = [ cachix.url ];
+      extra-trusted-public-keys = [ cachix.public-key ];
       keep-outputs = true;
       keep-derivations = true;
       experimental-features = "nix-command flakes";
+    }
+    // lib.optionalAttrs (!isHomeManager) {
+      trusted-substituters = [ cachix.url ];
+      trusted-public-keys = [ cachix.public-key ];
     };
     # enable `nix-shell -p nixpkgs#something` without using channels
     # also use the exact version of nixpkgs from the flake the system is built from
