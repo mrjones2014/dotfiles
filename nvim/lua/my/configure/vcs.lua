ffb39dbae12098980453b05f5c673a9f7283f03a
Mat Jones <mat@mjones.network>
2025-08-08 13:12:02 -0400

    feat(nvim): Add jj.nvim

--- nvim/lua/my/configure/vcs.lua
+++ nvim/lua/my/configure/vcs.lua
@@ -82,18 +82,5 @@ return {
       },
     },
   },
-  {
-    'pwntester/octo.nvim',
-    cmd = 'Octo',
-    opts = {
-      gh_env = function()
-        local github_token = require('op').get_secret('GitHub', 'token')
-        if not github_token or not vim.startswith(github_token, 'ghp_') then
-          error('Failed to get GitHub token.')
-        end
-
-        return { GITHUB_TOKEN = github_token }
-      end,
-    },
-  },
+  { 'NicolasGB/jj.nvim', cmd = 'J', opts = {} },
 }
