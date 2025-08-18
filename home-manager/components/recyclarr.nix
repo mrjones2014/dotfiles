04892c114022fdbbf247f4f5e6559bf3cb102669
Mat Jones <mat@mjones.network>
2025-08-18 14:24:30 -0400

    fix(nix): Only install recyclarr on Linux

--- home-manager/components/recyclarr.nix
+++ home-manager/components/recyclarr.nix
@@ -1,6 +1,11 @@
-{ pkgs, ... }:
 {
-  home.packages = [
+  pkgs,
+  lib,
+  isLinux,
+  ...
+}:
+{
+  home.packages = lib.lists.optionals isLinux [
     pkgs.recyclarr
     (pkgs.writeShellScriptBin "recyclarr-sync" ''
       op inject -i ~/git/dotfiles/home-manager/components/recyclarr.yaml -o ~/git/dotfiles/recyclarr-tmp.yml \
