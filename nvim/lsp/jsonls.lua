515c97e800093169ba3c3ca0fc34d498c2a774f2
Mat Jones <mat@mjones.network>
2025-10-02 20:35:39 -0400

    fix(nvim): Fix jsonls config

--- nvim/lsp/jsonls.lua
+++ nvim/lsp/jsonls.lua
@@ -1,5 +1,5 @@
 return {
-  cmd = { 'vscode-json-languageserver', '--stdio' },
+  cmd = { 'vscode-json-language-server', '--stdio' },
   filetypes = { 'json', 'jsonc' },
   init_options = { provideFormatter = true },
   root_markers = { '.git' },
