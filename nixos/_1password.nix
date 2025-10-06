065584f29637913bae1c62cdddc289976e3b7dbb
Mat Jones <mat@mjones.network>
2025-10-05 20:13:20 -0400

    fix(darwin): fix linux-specific options

--- nixos/_1password.nix
+++ nixos/_1password.nix
@@ -1,16 +1,14 @@
 { isLinux, lib, ... }:
 {
   programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-  }
-  // lib.optionalAttrs isLinux {
-    polkitPolicyOwners = [ "mat" ];
-    environment.etc."1password/custom_allowed_browsers" = {
-      text = ''
-        zen
-      '';
-      mode = "0755";
-    };
+  programs._1password-gui.enable = true;
+}
+// lib.optionalAttrs isLinux {
+  programs._1password-gui.polkitPolicyOwners = [ "mat" ];
+  environment.etc."1password/custom_allowed_browsers" = {
+    text = ''
+      zen
+    '';
+    mode = "0755";
   };
 }
