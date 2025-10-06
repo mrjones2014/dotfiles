7aa705d3f3da77b3efc47583e46e48c2042bfd1d
Mat Jones <mat@mjones.network>
2025-10-05 20:00:29 -0400

    fix(nix): Fix Firefox settings

--- home-manager/components/zen.nix
+++ home-manager/components/zen.nix
@@ -91,42 +91,42 @@
               installation_mode = "force_installed";
               default_area = "navbar"; # pinned
             };
-            "3rdparty".Extensions = {
-              "uBlock0@raymondhill.net".adminSettings = {
-                userSettings = rec {
-                  uiTheme = "dark";
-                  cloudStorageEnabled = lib.mkForce false;
-
-                  importedLists = [
-                    "https:#filters.adtidy.org/extension/ublock/filters/3.txt"
-                    "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
-                    "https:#mjones.network/ublock-filters.txt"
-                  ];
-
-                  externalLists = lib.concatStringsSep "\n" importedLists;
-                };
-
-                selectedFilterLists = [
-                  "CZE-0"
-                  "adguard-generic"
-                  "adguard-annoyance"
-                  "adguard-social"
-                  "adguard-spyware-url"
-                  "easylist"
-                  "easyprivacy"
-                  "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
-                  "plowe-0"
-                  "ublock-abuse"
-                  "ublock-badware"
-                  "ublock-filters"
-                  "ublock-privacy"
-                  "ublock-quick-fixes"
-                  "ublock-unbreak"
-                  "urlhaus-1"
-                ];
-              };
-            };
           };
+        "3rdparty".Extensions = {
+          "uBlock0@raymondhill.net".adminSettings = {
+            userSettings = rec {
+              uiTheme = "dark";
+              cloudStorageEnabled = lib.mkForce false;
+
+              importedLists = [
+                "https:#filters.adtidy.org/extension/ublock/filters/3.txt"
+                "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+                "https:#mjones.network/ublock-filters.txt"
+              ];
+
+              externalLists = lib.concatStringsSep "\n" importedLists;
+            };
+
+            selectedFilterLists = [
+              "CZE-0"
+              "adguard-generic"
+              "adguard-annoyance"
+              "adguard-social"
+              "adguard-spyware-url"
+              "easylist"
+              "easyprivacy"
+              "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+              "plowe-0"
+              "ublock-abuse"
+              "ublock-badware"
+              "ublock-filters"
+              "ublock-privacy"
+              "ublock-quick-fixes"
+              "ublock-unbreak"
+              "urlhaus-1"
+            ];
+          };
+        };
       };
     };
 }
