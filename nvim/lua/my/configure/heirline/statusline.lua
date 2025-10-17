d62b936b6888af16b1da54f953603bc5c5e48d30
Mat Jones <mat@mjones.network>
2025-10-17 09:26:16 -0400

    feat(nvim): Make copilot statusline component a toggle

--- nvim/lua/my/configure/heirline/statusline.lua
+++ nvim/lua/my/configure/heirline/statusline.lua
@@ -179,16 +179,22 @@ M.UnsavedChanges = {
   },
 }
 
-local function toggle_component(text, check_fn, toggle_fn)
+---Helper to make a toggle switch component
+---@param provider string|fun(self:table):string text provider
+---@param check_fn fun():boolean check function to determine toggle state
+---@param toggle_fn fun() toggle function to be called on click
+---@return table
+local function toggle_component(provider, check_fn, toggle_fn)
+  local id = tostring(vim.uv.hrtime())
   return {
-    provider = text,
+    provider = provider,
     hl = { bg = 'surface0' },
     {
       provider = function()
         return check_fn() and '  ' or '  '
       end,
       hl = { bg = 'surface0' },
-      on_click = { callback = toggle_fn, name = 'heirline_toggle_' .. text:lower():gsub('%s+', '_') },
+      on_click = { callback = toggle_fn, name = 'heirline_toggle_' .. id },
     },
   }
 end
@@ -242,29 +248,48 @@ M.Tabs = {
   utils.make_tablist(Tabpage),
 }
 
+local copilot_enabled = true
 M.CopilotStatus = {
-  condition = function()
+  init = function(self)
     -- don't cause copilot to load with `require` just for statusline
     local loaded = package.loaded.copilot
-    return not not loaded
+    if not loaded then
+      return
+    end
+    local auth = require('copilot.auth')
+    if not auth.is_authenticated() then
+      return
+    end
+    local info = require('copilot.auth').get_creds()
+    if not info then
+      return
+    end
+    for _, config in pairs(info) do
+      if config.githubAppId == 'Iv1.b507a08c87ecfe98' then
+        self.text = string.format('  %s ', config.user)
+        return
+      end
+    end
   end,
   {
-    provider = function()
-      local auth = require('copilot.auth')
-      if not auth.is_authenticated() then
-        return ''
-      end
-      local info = require('copilot.auth').get_creds()
-      if not info then
-        return ''
-      end
-      for _, config in pairs(info) do
-        if config.githubAppId == 'Iv1.b507a08c87ecfe98' then
-          return string.format('  %s ', config.user)
-        end
-      end
+    condition = function(self)
+      return self.text ~= nil
     end,
-    hl = { bg = 'surface0' },
+    toggle_component(function(self)
+      return self.text
+    end, function()
+      return copilot_enabled
+    end, function()
+      if copilot_enabled then
+        vim.notify('Disabling Copilot', vim.log.levels.INFO)
+        require('copilot.command').disable()
+        copilot_enabled = false
+      else
+        vim.notify('Enabling Copilot', vim.log.levels.INFO)
+        require('copilot.command').enable()
+        copilot_enabled = true
+      end
+    end),
   },
 }
 
