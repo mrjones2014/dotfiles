193318adb35d411c4bf8837c7a6c4452d09a0451
Mat Jones <mat@mjones.network>
2025-10-17 08:56:16 -0400

    feat(nvim): Update nvim-navic component highlights and clickable

--- nvim/lua/my/configure/heirline/winbar.lua
+++ nvim/lua/my/configure/heirline/winbar.lua
@@ -157,14 +157,99 @@ M.Diagnostics = {
   },
 }
 
+-- taken from https://github.com/rebelot/heirline.nvim/blob/master/cookbook.md
 M.Navic = {
   condition = function()
     return conditions.lsp_attached() and require('nvim-navic').is_available()
   end,
-  provider = function()
-    return string.format(' %s', require('nvim-navic').get_location())
+  static = {
+    -- create a type highlight map
+    type_hl = {
+      File = 'Directory',
+      Module = '@include',
+      Namespace = '@namespace',
+      Package = '@include',
+      Class = '@structure',
+      Method = '@method',
+      Property = '@property',
+      Field = '@field',
+      Constructor = '@constructor',
+      Enum = '@field',
+      Interface = '@type',
+      Function = '@function',
+      Variable = '@variable',
+      Constant = '@constant',
+      String = '@string',
+      Number = '@number',
+      Boolean = '@boolean',
+      Array = '@field',
+      Object = '@type',
+      Key = '@keyword',
+      Null = '@comment',
+      EnumMember = '@field',
+      Struct = '@structure',
+      Event = '@keyword',
+      Operator = '@operator',
+      TypeParameter = '@type',
+    },
+    -- bit operation dark magic, see below...
+    enc = function(line, col, winnr)
+      return bit.bor(bit.lshift(line, 16), bit.lshift(col, 6), winnr)
+    end,
+    -- line: 16 bit (65535); col: 10 bit (1023); winnr: 6 bit (63)
+    dec = function(c)
+      local line = bit.rshift(c, 16)
+      local col = bit.band(bit.rshift(c, 6), 1023)
+      local winnr = bit.band(c, 63)
+      return line, col, winnr
+    end,
+  },
+  init = function(self)
+    local data = require('nvim-navic').get_data() or {}
+    local children = {}
+    -- create a child for each level
+    for i, d in ipairs(data) do
+      -- encode line and column numbers into a single integer
+      local pos = self.enc(d.scope.start.line, d.scope.start.character, self.winnr)
+      local child = {
+        {
+          provider = d.icon,
+          hl = self.type_hl[d.type],
+        },
+        {
+          -- escape `%`s (elixir) and buggy default separators
+          provider = d.name:gsub('%%', '%%%%'):gsub('%s*->%s*', ''),
+          -- highlight icon only or location name as well
+          hl = self.type_hl[d.type],
+
+          on_click = {
+            -- pass the encoded position through minwid
+            minwid = pos,
+            callback = function(_, minwid)
+              -- decode
+              local line, col, winnr = self.dec(minwid)
+              vim.api.nvim_win_set_cursor(vim.fn.win_getid(winnr), { line, col })
+            end,
+            name = 'heirline_navic',
+          },
+        },
+      }
+      -- add a separator only if needed
+      if #data > 1 and i < #data then
+        table.insert(child, {
+          provider = ' > ',
+        })
+      end
+      table.insert(children, child)
+    end
+    -- instantiate the new child, overwriting the previous one
+    self.child = self:new(children, 1)
   end,
-  hl = { bg = 'surface0' },
+  -- evaluate the children containing navic components
+  provider = function(self)
+    return string.format(' %s', self.child:eval())
+  end,
+  update = 'CursorMoved',
 }
 
 M.FilePosition = {
