10c9008a79aba9d0f1b428912b8866546161c4eb
Mat Jones <mat@mjones.network>
2025-11-20 21:04:53 -0500

    fix(homelab): Set qbittorrent password declaratively

--- nixos/qbittorrent.nix
+++ nixos/qbittorrent.nix
@@ -14,60 +14,75 @@
   ...
 }:
 let
-  data_dir = if isServer then "/mnt/jellyfin" else "/mnt/storage/torrents";
+  data_dir = if isServer then "/mnt/jellyfin" else "/mnt/storage/downloads";
   qbittorrent_port = 8080;
   network_namespace = "qbt-wg";
+  namespace_address = if isServer then "192.168.15.1" else "192.168.15.2";
+  namespace_address_v6 = if isServer then "fd93:9701:1d00::2" else "fd93:9701:1d00::3";
   mullvad_secret_file =
     if isServer then ../secrets/mullvad_wireguard.age else ../secrets/mullvad_wireguard_desktop.age;
 in
 {
+
   imports = [ inputs.vpn-confinement.nixosModules.default ];
-  age.secrets.mullvad_wireguard.file = mullvad_secret_file;
-  vpnNamespaces.${network_namespace} = {
-    enable = true;
-    wireguardConfigFile = config.age.secrets.mullvad_wireguard.path;
-    accessibleFrom = [
-      "192.168.1.0/24"
-    ];
-    portMappings = [
-      {
-        from = qbittorrent_port;
-        to = qbittorrent_port;
-        protocol = "tcp";
-      }
-    ];
-  };
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = false;
-    webuiPort = qbittorrent_port;
-    serverConfig = {
-      Core.AutoDeleteAddedTorrentFile = "Never";
-      Preferences.Connection = {
-        WebUI.LocalHostAuth = false;
-      };
-      BitTorrent.Session = {
-        DefaultSavePath = "${data_dir}/media";
-        TempPath = "${data_dir}/incomplete";
-        TempPathEnabled = true;
-        AnonymousModeEnabled = true;
-        GlobalMaxSeedingMinutes = -1;
+}
+// lib.mkMerge [
+  {
+    age.secrets.mullvad_wireguard.file = mullvad_secret_file;
+    vpnNamespaces.${network_namespace} = {
+      enable = true;
+      wireguardConfigFile = config.age.secrets.mullvad_wireguard.path;
+      namespaceAddress = namespace_address;
+      namespaceAddressIPv6 = namespace_address_v6;
+      accessibleFrom = [
+        "192.168.1.0/24"
+      ];
+      portMappings = [
+        {
+          from = qbittorrent_port;
+          to = qbittorrent_port;
+          protocol = "tcp";
+        }
+      ];
+    };
+    services.qbittorrent = {
+      enable = true;
+      openFirewall = false;
+      webuiPort = qbittorrent_port;
+      serverConfig = {
+        Core.AutoDeleteAddedTorrentFile = "Never";
+        Preferences.WebUI = {
+          LocalHostAuth = false;
+          # generate with the following command (make sure to prefix with a space to avoid going into shell history!)
+          # nix run git+https://codeberg.org/feathecutie/qbittorrent_password -- -p [password here]
+          Password_PBKDF2 = "@ByteArray(ayWHE7QQHrTEJ+yPm1ymsA==:uyzO//xbRfCTABjdHl9L74Dz4rXFAW60iHxp3GnFVUtwIWDVanKj+yRO3QWEfe54A+3SI/SuTmdEVwYfI1vE2A==)";
+        };
+        BitTorrent.Session = {
+          DefaultSavePath = "${data_dir}/media";
+          TempPath = "${data_dir}/incomplete";
+          TempPathEnabled = true;
+          AnonymousModeEnabled = true;
+          GlobalMaxSeedingMinutes = if isServer then -1 else 0;
+        }
+        // lib.optionalAttrs isServer {
+          TorrentExportDirectory = "${data_dir}/torrents";
+        };
       };
     };
-  };
-  systemd.services.qbittorrent.vpnConfinement = {
-    enable = true;
-    vpnNamespace = network_namespace;
-  };
-}
-// lib.optionalAttrs isServer {
-  services.nginx.subdomains.qbittorrent = {
-    address = config.vpnNamespaces.${network_namespace}.namespaceAddress;
-    port = qbittorrent_port;
-  };
-}
-// lib.optionalAttrs (!isServer) {
-  networking.hosts."${config.vpnNamespaces.${network_namespace}.namespaceAddress}" = [
-    "qbittorrent.lan"
-  ];
-}
+    systemd.services.qbittorrent.vpnConfinement = {
+      enable = true;
+      vpnNamespace = network_namespace;
+    };
+  }
+  (lib.mkIf isServer {
+    services.nginx.subdomains.qbittorrent = {
+      address = config.vpnNamespaces.${network_namespace}.namespaceAddress;
+      port = qbittorrent_port;
+    };
+  })
+  (lib.mkIf (!isServer) {
+    networking.hosts."${config.vpnNamespaces.${network_namespace}.namespaceAddress}" = [
+      "qbittorrent.lan"
+    ];
+  })
+]
