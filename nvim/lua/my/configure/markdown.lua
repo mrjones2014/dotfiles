6c2df61c66864726f2f2ce2351ff6fc0522e87a8
Mat Jones <mat@mjones.network>
2026-01-06 20:19:37 -0500

    fix(nvim): Attach render-markdown.nvim to codesettings buffers

--- nvim/lua/my/configure/markdown.lua
+++ nvim/lua/my/configure/markdown.lua
@@ -2,8 +2,8 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', branch = 'main' },
-    opts = { file_types = { 'markdown', 'codecompanion' } },
-    ft = { 'markdown', 'codecompanion' },
+    opts = { file_types = { 'markdown', 'codecompanion', 'codesettings-output' } },
+    ft = { 'markdown', 'codecompanion', 'codesettings-output' },
   },
   {
     'OXY2DEV/helpview.nvim',
