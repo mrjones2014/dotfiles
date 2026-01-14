19aaf75115391eec8ab2285239db26c502379b3b
Mat Jones <mat@mjones.network>
2026-01-13 20:39:36 -0500

    feat(nvim): Use Opencode in codecompanion.nvim

--- home-manager/components/opencode.nix
+++ home-manager/components/opencode.nix
@@ -1,25 +1,10 @@
-{ lib, isDarwin, ... }:
 {
   programs.opencode = {
     enable = true;
-    settings = lib.mkMerge [
-      {
-        theme = "tokyonight";
-      }
-      # ensure only github copilot can be used on work machine
-      # for compliance
-      (lib.optionalAttrs isDarwin {
-        enabled_providers = [ "github-copilot" ];
-        disabled_providers = [
-          "anthropic"
-          "openai"
-          "gemini"
-          "deepseek"
-          "ollama"
-        ];
-        model = "github-copilot/claude-sonnet-4-5";
-        share = "disabled";
-      })
-    ];
+    settings = {
+      theme = "tokyonight";
+      enabled_providers = [ "github-copilot" ];
+      model = "github-copilot/claude-sonnet-4.5";
+    };
   };
 }
