e09337a6dfb31ca883f3b5bc153934d3b545f4b4
Mat Jones <mat@mjones.network>
2026-01-19 13:01:33 -0500

    fix(recyclarr): Install CLI on Aldecaldo

--- home-manager/components/recyclarr.nix
+++ home-manager/components/recyclarr.nix
@@ -1,11 +1,11 @@
 {
   pkgs,
   lib,
-  isLinux,
+  isWorkMac,
   ...
 }:
 {
-  home.packages = lib.lists.optionals isLinux [
+  home.packages = lib.lists.optionals (!isWorkMac) [
     pkgs.recyclarr
     (pkgs.writeShellScriptBin "recyclarr-sync" ''
       op inject -i ~/git/dotfiles/home-manager/components/recyclarr.yaml -o ~/git/dotfiles/recyclarr-tmp.yml \
