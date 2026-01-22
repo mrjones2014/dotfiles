a622500b2c8db7832310427e474dc94c453e504d
Mat Jones <mat@mjones.network>
2026-01-22 13:39:22 -0500

    chore(nvim): Remove redundant config for treesitter

--- nvim/lua/my/configure/markdown.lua
+++ nvim/lua/my/configure/markdown.lua
@@ -1,12 +1,12 @@
 return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', branch = 'main' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
     ft = { 'markdown', 'codecompanion', 'codesettings-output', 'opencode', 'opencode_output' },
   },
   {
     'OXY2DEV/helpview.nvim',
     ft = 'help',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', branch = 'main' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
   },
 }
