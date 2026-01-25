1867ee1e3d7ac8b28f473803c2484db35ec79918
Mat Jones <mat@mjones.network>
2026-01-24 23:52:45 -0500

    fix(homelab): Clean up module

--- hosts/mikoshi/open-webui.nix
+++ hosts/mikoshi/open-webui.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  config.services = {
+  services = {
     nginx.subdomains."ai".port = config.services.open-webui.port;
     ollama.enable = true;
     open-webui = {
@@ -9,7 +9,7 @@
         ANONYMIZED_TELEMETRY = "False";
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
-        OLLAMA_API_BASE_URL = "http://127.0.0.1:${builtins.toString config.services.ollama.port}";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:${toString config.services.ollama.port}";
         DEFAULT_USER_ROLE = "pending";
         ENABLE_SIGNUP = "True";
       };
