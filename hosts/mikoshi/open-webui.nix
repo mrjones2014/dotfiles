863e50bac64ce73cec38703b43fa1e03fafb71cd
Mat Jones <mat@mjones.network>
2026-01-25 12:28:33 -0500

    fix(homelab): Add python deps for open-webui plugin

--- hosts/mikoshi/open-webui.nix
+++ hosts/mikoshi/open-webui.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   age.secrets.open_webui_env.file = ../../secrets/open_webui.age;
   services = {
@@ -7,6 +7,26 @@
     open-webui = {
       enable = true;
       environmentFile = config.age.secrets.open_webui_env.path;
+      package = pkgs.open-webui.overrideAttrs (old: {
+        # for https://github.com/rbb-dev/Open-WebUI-OpenRouter-pipe
+        # see: https://github.com/NixOS/nixpkgs/issues/422030#issuecomment-3146105303
+        propagatedBuildInputs =
+          old.propagatedBuildInputs
+          ++ (with pkgs.python3Packages; [
+            aiohttp
+            cryptography
+            fastapi
+            httpx
+            lz4
+            pydantic
+            pydantic-core
+            sqlalchemy
+            tenacity
+            pyzipper
+            cairosvg
+            pillow
+          ]);
+      });
     };
   };
 }
