ae4ffc69186e51afa6fc3c17cb5a598f0e518000
Mat Jones <mat@mjones.network>
2026-01-31 14:01:30 -0500

    fix(mikoshi): Pin SQLite version for sonarr

--- hosts/mikoshi/media.nix
+++ hosts/mikoshi/media.nix
@@ -1,7 +1,14 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   huntarr_port = 9705;
   huntarr_data = "/var/lib/huntarr";
+  sqlite-3-50 = pkgs.sqlite.overrideAttrs (old: {
+    version = "3.50.0";
+    src = pkgs.fetchurl {
+      url = "https://sqlite.org/2025/sqlite-autoconf-3500000.tar.gz";
+      sha256 = "09w32b04wbh1d5zmriwla7a02r93nd6vf3xqycap92a3yajpdirv";
+    };
+  });
 in
 {
   imports = [ ../../nixos/qbittorrent.nix ];
@@ -35,7 +42,15 @@ in
     jellyfin.enable = true;
     jellyseerr.enable = true;
     prowlarr.enable = true;
-    sonarr.enable = true;
+    sonarr = {
+      enable = true;
+      # Sonarr hangs after a while due to a sqlite issue:
+      # https://github.com/nixos/nixpkgs/issues/481098
+      # https://github.com/h0lylag/nix-config/commit/191876ec684efc0a937ad46acf25be242a7bc4b6
+      package = pkgs.sonarr.override {
+        sqlite = sqlite-3-50;
+      };
+    };
     radarr.enable = true;
     bazarr.enable = true;
   };
