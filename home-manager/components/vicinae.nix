dcbea23b223d5b246f972e857116a4d244884c97
Mat Jones <mat@mjones.network>
2026-01-31 18:48:39 -0500

    fix(nixos): Fix vicinae config

--- home-manager/components/vicinae.nix
+++ home-manager/components/vicinae.nix
@@ -7,11 +7,28 @@ if isLinux then
         enable = true;
         autoStart = true;
       };
+      # easiest way to find these keys is:
+      # ```bash
+      # VICINAE_CFG_PATH=$(readlink ~/.config/vicinae/settings.json) \
+      #   rm ~/.config/vicinae/settings.json && \
+      #   cp $VICINAE_CFG_PATH ~/.config/vicinae/
+      # ```
+      # Then change settings through GUI,
+      # copy the modified settings back into here,
+      # and rebuild.
       settings = {
         close_on_focus_loss = true;
         pop_to_root_on_close = true;
         theme.name = "tokyo-night";
         faviconService = "twenty";
+        fallbacks = [ "shortcuts:kagi" ];
+        providers = {
+          core.entrypoints.sponsor.enabled = false;
+          applications = {
+            enabled = true;
+            preferences.defaultAction = "focus";
+          };
+        };
       };
     };
     dconf.settings = {
