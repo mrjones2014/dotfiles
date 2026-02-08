0c1de23c2f9f8bebc28955f299a2f0fafd359eb7
Mat Jones <mat@mjones.network>
2026-02-08 11:26:45 -0500

    fi(nix): Update flake.lock and remove no longer needed sqlite fix

--- hosts/mikoshi/media.nix
+++ hosts/mikoshi/media.nix
@@ -1,14 +1,7 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 let
   huntarr_port = 9705;
   huntarr_data = "/var/lib/huntarr";
-  sqlite-3-50 = pkgs.sqlite.overrideAttrs (old: {
-    version = "3.50.0";
-    src = pkgs.fetchurl {
-      url = "https://sqlite.org/2025/sqlite-autoconf-3500000.tar.gz";
-      sha256 = "09w32b04wbh1d5zmriwla7a02r93nd6vf3xqycap92a3yajpdirv";
-    };
-  });
 in
 {
   imports = [ ../../nixos/qbittorrent.nix ];
@@ -42,15 +35,7 @@ in
     jellyfin.enable = true;
     jellyseerr.enable = true;
     prowlarr.enable = true;
-    sonarr = {
-      enable = true;
-      # Sonarr hangs after a while due to a sqlite issue:
-      # https://github.com/nixos/nixpkgs/issues/481098
-      # https://github.com/h0lylag/nix-config/commit/191876ec684efc0a937ad46acf25be242a7bc4b6
-      package = pkgs.sonarr.override {
-        sqlite = sqlite-3-50;
-      };
-    };
+    sonarr.enable = true;
     radarr.enable = true;
     bazarr.enable = true;
   };
