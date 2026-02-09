c5fdb6072adb5c85a707b1a83b1d50e57a86bf21
Mat Jones <mat@mjones.network>
2026-02-09 16:52:03 -0500

    feat(nvim): Add keybinds to jump to specifically error severity diagnostics

--- nvim/lua/my/configure/mini_bracketed.lua
+++ nvim/lua/my/configure/mini_bracketed.lua
@@ -7,6 +7,20 @@ return {
     { ']x', desc = 'Jump to next conflict marker' },
     { '[d', desc = 'Jump to previous diagnostic' },
     { ']d', desc = 'Jump to next diagnostic' },
+    {
+      '[e',
+      desc = 'Jump to previous error',
+      function()
+        require('mini.bracketed').diagnostic('backward', { severity = vim.diagnostic.severity.ERROR })
+      end,
+    },
+    {
+      ']e',
+      desc = 'Jump to next error',
+      function()
+        require('mini.bracketed').diagnostic('forward', { severity = vim.diagnostic.severity.ERROR })
+      end,
+    },
     { '[q', desc = 'Jump to previous Quickfix list entry' },
     { ']q', desc = 'Jump to next Quickfix list entry' },
     { '[t', desc = 'Jump to previous Treesitter node' },
