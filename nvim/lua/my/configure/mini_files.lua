108e07fb9144bfc22b4d8b88af76ea75bafe35e4
Mat Jones <mat@mjones.network>
2026-02-11 09:19:52 -0500

    feat(nvim): Add keybind to toggle .gitignore filtering on mini.files

--- nvim/lua/my/configure/mini_files.lua
+++ nvim/lua/my/configure/mini_files.lua
@@ -1,3 +1,46 @@
+local filter_gitignore = true
+
+local function sort_filter_gitignore(entries)
+  if not filter_gitignore then
+    return require('mini.files').default_sort(entries)
+  end
+  -- technically can filter entries here too, and checking gitignore for _every entry individually_
+  -- like I would have to in `content.filter` above is too slow. Here we can give it _all_ the entries
+  -- at once, which is much more performant.
+  local all_paths = table.concat(
+    vim
+      .iter(entries)
+      :map(function(entry)
+        return entry.path
+      end)
+      :totable(),
+    '\n'
+  )
+  local output_lines = {}
+  local job_id = vim.fn.jobstart({ 'git', 'check-ignore', '--stdin' }, {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      output_lines = data
+    end,
+  })
+
+  -- command failed to run
+  if job_id < 1 then
+    return entries
+  end
+
+  -- send paths via STDIN
+  vim.fn.chansend(job_id, all_paths)
+  vim.fn.chanclose(job_id, 'stdin')
+  vim.fn.jobwait({ job_id })
+  return require('mini.files').default_sort(vim
+    .iter(entries)
+    :filter(function(entry)
+      return not vim.tbl_contains(output_lines, entry.path)
+    end)
+    :totable())
+end
+
 return {
   'nvim-mini/mini.files',
   dependencies = {
@@ -26,9 +69,17 @@ return {
           minifiles.close()
         end, { buffer = buf })
 
+        -- toggle .gitignore filtering with <leader>gi
+        vim.keymap.set('n', '<leader>gi', function()
+          filter_gitignore = not filter_gitignore
+          minifiles.refresh({
+            content = { sort = filter_gitignore and sort_filter_gitignore or minifiles.default_sort },
+          })
+        end, { buffer = buf, desc = 'Toggle .gitignore filtering' })
+
         -- set up ability to confirm changes with :w
         vim.api.nvim_set_option_value('buftype', 'acwrite', { buf = buf })
-        vim.api.nvim_buf_set_name(buf, string.format('mini.files-%s', vim.loop.hrtime()))
+        vim.api.nvim_buf_set_name(buf, string.format('mini.files-%s', vim.uv.hrtime()))
         vim.api.nvim_create_autocmd('BufWriteCmd', {
           buffer = buf,
           callback = function()
@@ -69,43 +120,7 @@ return {
       filter = function(entry)
         return entry.name ~= '.DS_Store' and entry.name ~= '.git' and entry.name ~= '.direnv' and entry.name ~= '.jj'
       end,
-      sort = function(entries)
-        -- technically can filter entries here too, and checking gitignore for _every entry individually_
-        -- like I would have to in `content.filter` above is too slow. Here we can give it _all_ the entries
-        -- at once, which is much more performant.
-        local all_paths = table.concat(
-          vim
-            .iter(entries)
-            :map(function(entry)
-              return entry.path
-            end)
-            :totable(),
-          '\n'
-        )
-        local output_lines = {}
-        local job_id = vim.fn.jobstart({ 'git', 'check-ignore', '--stdin' }, {
-          stdout_buffered = true,
-          on_stdout = function(_, data)
-            output_lines = data
-          end,
-        })
-
-        -- command failed to run
-        if job_id < 1 then
-          return entries
-        end
-
-        -- send paths via STDIN
-        vim.fn.chansend(job_id, all_paths)
-        vim.fn.chanclose(job_id, 'stdin')
-        vim.fn.jobwait({ job_id })
-        return require('mini.files').default_sort(vim
-          .iter(entries)
-          :filter(function(entry)
-            return not vim.tbl_contains(output_lines, entry.path)
-          end)
-          :totable())
-      end,
+      sort = sort_filter_gitignore,
     },
     mappings = {
       go_in_plus = '<CR>',
