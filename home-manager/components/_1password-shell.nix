ec2a1a6a942e2365f8f5775b6ad46837f53b824b
Mat Jones <mat@mjones.network>
2026-02-12 13:59:36 -0500

    fix(cli): Custom wrapper for github cli

--- home-manager/components/_1password-shell.nix
+++ home-manager/components/_1password-shell.nix
@@ -15,20 +15,34 @@ let
   '';
 in
 {
-  imports = [ ../modules/_1password-shell-plugins.nix ];
   programs = {
     fish = {
       interactiveShellInit = ''
         export SUDO_ASKPASS="${op_sudo_password_script}/bin/opsudo"
         alias sudo="sudo -A"
       '';
-    };
-    _1password-shell-plugins = {
-      enable = true;
-      plugins = with pkgs; [
-        gh
-        glab
-      ];
+      functions.gh = {
+        description = "Run GitHub CLI with token from 1Password";
+        body = ''
+          # don't prompt for auth if the command doesn't need it
+          if test (count $argv) -eq 0; or contains -- --help $argv; or contains -- -h $argv; or contains -- --version $argv; or test "$argv[1]" = help; or test "$argv[1]" = version; or contains -- __complete $argv
+              ${pkgs.gh}/bin/gh $argv
+              return
+          end
+
+          # default to personal token
+          set -l token "op://Private/GitHub/token"
+          set -l account_uuid ZE3GMX56H5CV5J5IU5PLLFG4KQ
+          set -l url (git config --get remote.origin.url 2>/dev/null)
+          # check if we are in a work repo, and
+          # set the correct token based on that
+          if string match -q -r 'github\.com:agilebits-inc' "$url"
+              set token "op://Employee/1Password GitHub Token/credential"
+              set account_uuid S2EWWY7HCZDGFOQ7WOPBGAC2LY
+          end
+          GITHUB_TOKEN=(op read --account "$account_uuid" "$token") ${pkgs.gh}/bin/gh $argv
+        '';
+      };
     };
   };
 }
