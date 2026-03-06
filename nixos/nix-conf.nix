ecab904810320034bc201076dbd750c9ceb0b4b9
Mat Jones <mat@mjones.network>
2026-03-06 17:32:38 -0500

    feat(nix): Add nix-community cache

--- nixos/nix-conf.nix
+++ nixos/nix-conf.nix
@@ -6,9 +6,13 @@
   ...
 }:
 let
-  cachix = {
+  cachix-personal = {
     url = "https://mrjones2014-dotfiles.cachix.org";
     public-key = "mrjones2014-dotfiles.cachix.org-1:c66wfzthG6KZEWnltlzW/EjhlH9FwUVi5jM4rVD1Rw4=";
+  };
+  cachix-nix-community = {
+    url = "https://nix-community.cachix.org";
+    public-key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
   };
 in
 {
@@ -20,8 +24,14 @@ in
         experimental-features = "nix-command flakes";
         keep-derivations = true;
         keep-outputs = true;
-        trusted-substituters = [ cachix.url ];
-        trusted-public-keys = [ cachix.public-key ];
+        trusted-substituters = [
+          cachix-personal.url
+          cachix-nix-community.url
+        ];
+        trusted-public-keys = [
+          cachix-personal.public-key
+          cachix-nix-community.public-key
+        ];
       };
     # enable `nix-shell -p nixpkgs#something` without using channels
     # also use the exact version of nixpkgs from the flake the system is built from
