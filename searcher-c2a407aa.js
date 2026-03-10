b6dbf9d43981f4ee74569c76bf364f422a79a457
mrjones2014 <mrjones2014@users.noreply.github.com>
2026-03-10 16:59:45 +0000

    deploy: fc48a52df5019b5a6d07afc05afad3d77535ffd2

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-90de14fe.js',
+                path_to_root + 'searchindex-8120f33d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
