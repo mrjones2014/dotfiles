93af62fcd373bc284cbf62e6db4588de8097fa18
Mat Jones <mat@mjones.network>
2026-03-17 11:18:26 -0400

    feat(nvim): Swap back to codecompanion.nvim

--- nvim/lua/my/configure/ai.lua
+++ nvim/lua/my/configure/ai.lua
@@ -1,95 +1,130 @@
+local window = require('my.utils.window')
+
 return {
-  {
-    'zbirenbaum/copilot.lua',
-    event = 'LspAttach',
-    cmd = 'Copilot',
-    config = {
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = '<C-CR>',
-          accept_line = '<C-t>',
-          dismiss = '<C-d>',
+  'olimorris/codecompanion.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'franco-ruggeri/codecompanion-spinner.nvim',
+    'ravitemer/codecompanion-history.nvim',
+    'folke/snacks.nvim',
+    { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'codecompanion' } },
+    {
+      'zbirenbaum/copilot.lua',
+      event = 'LspAttach',
+      cmd = 'Copilot',
+      config = {
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = '<C-CR>',
+            accept_line = '<C-t>',
+            dismiss = '<C-d>',
+          },
         },
-      },
-      server_opts_overrides = {
-        settings = {
-          telemetry = {
-            telemetryLevel = 'off',
+        server_opts_overrides = {
+          settings = {
+            telemetry = {
+              telemetryLevel = 'off',
+            },
           },
         },
       },
     },
   },
-  {
-    'folke/sidekick.nvim',
-    keys = {
-      {
-        '<leader>aa',
-        function()
-          require('sidekick.cli').toggle({ name = 'claude' })
-        end,
-        desc = 'sidekick: toggle',
-        mode = 'n',
-      },
-      {
-        '<leader>aa',
-        function()
-          require('sidekick.cli').send({ name = 'claude', msg = '{selection}' })
-        end,
-        desc = 'sidekick: send selection',
-        mode = 'v',
-      },
-      {
-        '<leader>af',
-        function()
-          require('sidekick.cli').focus()
-        end,
-        desc = 'sidekick: focus',
-      },
+  version = false,
+  cmd = {
+    'CodeCompanionChat',
+    'CodeCompanionInline',
+    'CodeCompanionRefactor',
+    'CodeCompanionNew',
+    'CodeCompanionClear',
+    'CodeCompanionToggle',
+  },
+  keys = {
+    {
+      '<leader>aa',
+      function()
+        require('codecompanion').chat()
+      end,
+      desc = 'codecompanion: chat',
+      mode = { 'n', 'v' },
     },
-    opts = {
-      cli = {
-        mux = {
-          backend = 'zellij',
-          enabled = true,
+    { '<leader>ae', desc = 'codecompanion: edit', mode = { 'v' } },
+    { '<leader>aB', desc = 'codecompanion: add all open buffers' },
+    { '<leader>ac', desc = 'codecompanion: add current buffer' },
+    { '<leader>aC', desc = 'codecompanion: toggle selection' },
+    {
+      '<leader>af',
+      function()
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.bo[buf].filetype == 'codecompanion' and not window.is_floating_window(win) then
+            vim.api.nvim_set_current_win(win)
+          end
+        end
+      end,
+      desc = 'codecompanion: focus',
+    },
+    { '<leader>ah', desc = 'codecompanion: select history' },
+    { '<leader>an', desc = 'codecompanion: new chat' },
+    {
+      '<leader>at',
+      function()
+        require('codecompanion').toggle()
+      end,
+      desc = 'CodeCompanion: toggle',
+    },
+  },
+  opts = {
+    adapters = {
+      copilot = function()
+        return require('codecompanion.adapters').extend('copilot', {
+          schema = {
+            model = {
+              default = 'gpt-4o',
+            },
+          },
+        })
+      end,
+    },
+    extensions = {
+      spinner = {},
+      history = {
+        enabled = true,
+        opts = {
+          picker = 'snacks',
+          expiration_days = 7,
+          continue_last_chat = true,
+          delete_on_clearing_chat = true,
+          title_generation_opts = {
+            adapter = 'copilot',
+            model = 'gpt-4o',
+          },
+          summary = {
+            generation_opts = {
+              adapter = 'copilot',
+              model = 'gpt-4o',
+            },
+          },
         },
       },
     },
+    interactions = {
+      chat = {
+        keymaps = {
+          -- change this mapping because I use gx to open URLs
+          clear = {
+            modes = { n = 'gX' },
+          },
+        },
+        adapter = 'opencode',
+      },
+      inline = {
+        adapter = 'opencode',
+      },
+      cmd = {
+        adapter = 'opencode',
+      },
+    },
   },
-  -- { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'opencode', 'opencode_output' } },
-  -- {
-  --   'sudo-tee/opencode.nvim',
-  --   keys = { '<leader>o' },
-  --   dependencies = {
-  --     'nvim-lua/plenary.nvim',
-  --     'folke/snacks.nvim',
-  --   },
-  --   opts = {
-  --     default_mode = 'plan',
-  --     ui = {
-  --       input = { text = { wrap = true } },
-  --     },
-  --     keymap = {
-  --       input_window = {
-  --         -- only submit with enter in normal mode, not insert mode
-  --         ['<cr>'] = { 'submit_input_prompt', mode = { 'n' } },
-  --         ['<s-cr>'] = false,
-  --         -- don't close the window on `<esc>`,
-  --         -- too easily to close accidentally when trying to exit insert mode
-  --         ['<esc>'] = false,
-  --       },
-  --       output_window = {
-  --         -- don't close the window on `<esc>`,
-  --         -- too easily to close accidentally when trying to exit insert mode
-  --         ['<esc>'] = false,
-  --       },
-  --       session_picker = {
-  --         -- don't override <C-n>, that should be reserved for "next entry",
-  --         -- i.e. <C-n> and <C-p>
-  --         new_session = false,
-  --       },
-  --     },
-  --   },
-  -- },
 }
