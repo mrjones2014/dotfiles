88dd520ad7596769b8d73492b4d701c5750a7def
Mat Jones <mat@mjones.network>
2026-03-24 11:27:43 -0400

    fix(claude): Remove system prompt that didn't do anything

--- home-manager/components/claude-code.nix
+++ home-manager/components/claude-code.nix
@@ -1,22 +1,4 @@
-{ pkgs, lib, ... }:
-let
-  extra_system_prompt = ''
-    # Plan Mode Rules
-
-    When you are in plan mode, this is INTENTIONAL by the user. DO NOT
-    ask the user follow up questions like "Ready to code?",
-    "Shall I implement this?" or any similar question that tries to
-    confirm if you can start implementing the plan. The answer will
-    always be "no, explain the plan to me first". ONLY AFTER explaining
-    the plan to the user, the user will choose to switch to build mode, or
-    not, depending on whether they are satisfied with the plan or not.
-
-    If you create a plan as part of your processing, ALWAYS EXPLAIN
-    the plan to the user, this should be at the VERY END of your response.
-    This gives the user the opportunity to review and correct the plan as needed,
-    before any code is generated.
-  '';
-in
+{ pkgs, ... }:
 {
   home.sessionVariables.DISABLE_TELEMETRY = "1";
   home.packages = with pkgs; [
@@ -30,16 +12,7 @@ in
   ];
   programs.claude-code = {
     enable = true;
-    package = pkgs.symlinkJoin {
-      name = "claude-code-bin";
-      paths = [ pkgs.claude-code-bin ];
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/claude \
-          --append-flag "--append-system-prompt" \
-          --append-flag ${lib.escapeShellArg extra_system_prompt}
-      '';
-    };
+    package = pkgs.claude-code-bin;
     skillsDir = ../../agent/skills;
     # some settings are undocumented, refer to the schema
     # https://www.schemastore.org/claude-code-settings.json
