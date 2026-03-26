c42f1c163e9fa56d3c9c1aed541985026d719941
Mat Jones <mat@mjones.network>
2026-03-25 20:16:34 -0400

    feat(nvim): Enable render-markdown.nvim lsp completions

--- nvim/lua/my/configure/markdown.lua
+++ nvim/lua/my/configure/markdown.lua
@@ -3,6 +3,7 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     ft = 'markdown',
+    opts = { completions = { lsp = { enabled = true } } },
   },
   {
     'OXY2DEV/helpview.nvim',
