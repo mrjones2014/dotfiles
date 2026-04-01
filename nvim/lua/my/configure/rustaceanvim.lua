85b2ef5080b755d9eb97c496831de6723f3adbde
Mat Jones <mat@mjones.network>
2026-04-01 12:24:30 -0400

    fix(nvim): Update to rustaceanvim version ^8

--- nvim/lua/my/configure/rustaceanvim.lua
+++ nvim/lua/my/configure/rustaceanvim.lua
@@ -1,16 +1,11 @@
 return {
   'mrcjkb/rustaceanvim',
   ft = 'rust',
-  version = '^7',
-  dependencies = { 'mrjones2014/codesettings.nvim' },
+  version = '^8',
   init = function()
     vim.g.rustaceanvim = {
       server = {
         cmd = { vim.env.NVIM_RUST_ANALYZER },
-        -- I want VS Code settings to override my settings,
-        -- not the other way around, so rely on codesettings.nvim
-        -- instead of rustaceanvim's built-in vscode settings loader
-        load_vscode_settings = false,
         on_attach = function()
           require('which-key').add({
             {
