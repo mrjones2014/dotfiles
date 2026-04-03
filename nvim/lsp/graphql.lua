b6e59a38a382e2c2b74a76cd53c55b8eb0c2529d
Mat Jones <mat@mjones.network>
2026-04-03 09:23:08 -0400

    fix(nvim): Fix GraphQL LSP config

--- nvim/lsp/graphql.lua
+++ nvim/lsp/graphql.lua
@@ -1,8 +1,5 @@
 return {
   cmd = { 'graphql-lsp', 'server', '-m', 'stream' },
   filetypes = { 'graphql', 'javascriptreact', 'typescriptreact' },
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(vim.fs.root(fname, { '.graphqlrc*', '.graphql.config.*', 'graphql.config.*' }))
-  end,
+  root_markers = { '.graphqlrc*', '.graphql.config.*', 'graphql.config.*' },
 }
