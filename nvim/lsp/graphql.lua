ee93cd24b95dbe6b1ba07211462871cb1bd3af48
Mat <8648891+mrjones2014@users.noreply.github.com>
2026-06-05 13:18:38 -0400

    flake: update dependencies (#295)

--- nvim/lsp/graphql.lua
+++ nvim/lsp/graphql.lua
@@ -120,18 +120,13 @@ return {
   filetypes = { 'graphql', 'javascriptreact', 'typescriptreact' },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(
-      vim.fs.root(
-        fname,
-        {
-          '.graphqlrc',
-          '.graphqlrc.yml',
-          '.graphqlrc.yaml',
-          '.graphql.config.json',
-          'graphql.config.toml',
-          '.graphqlrc.toml',
-        }
-      )
-    )
+    on_dir(vim.fs.root(fname, {
+      '.graphqlrc',
+      '.graphqlrc.yml',
+      '.graphqlrc.yaml',
+      '.graphql.config.json',
+      'graphql.config.toml',
+      '.graphqlrc.toml',
+    }))
   end,
 }
