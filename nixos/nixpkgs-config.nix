cc53deeb7401ab72c294f103e5f0e1759dcf8640
Mat Jones <mat@mjones.network>
2026-06-10 15:41:54 -0400

    chore(nix): Update flake.lock

--- nixos/nixpkgs-config.nix
+++ nixos/nixpkgs-config.nix
@@ -4,27 +4,10 @@
   isServer,
   ...
 }:
-let
-  # TODO remove; see: https://github.com/NixOS/nixpkgs/pull/522452#issuecomment-4519733688
-  # https://nixpk.gs/pr-tracker.html?pr=525089
-  onePasswordHashes = {
-    "aarch64-darwin" = "sha256-WrWbGzBK65tVNl9Dc3OnJURiPpfbNLOYUJcVT0ETaAs=";
-    "x86_64-linux" = "sha256-JwiMi2iozP6jWSIUtgXla86aSAhuUob7snqtUbeXPpI=";
-  };
-in
 {
   nixpkgs = {
     overlays = [
       (_: _: (import ../pkgs { inherit pkgs; }))
-      # TODO remove see comment above
-      (_: prev: {
-        _1password-gui = prev._1password-gui.overrideAttrs (old: {
-          src = prev.fetchurl {
-            inherit (old.src) url;
-            hash = onePasswordHashes.${prev.stdenv.hostPlatform.system};
-          };
-        });
-      })
     ];
     config.allowUnfreePredicate =
       pkg:
