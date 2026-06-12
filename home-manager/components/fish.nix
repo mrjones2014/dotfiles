797a95e6abad043d914555b9752ff8f0f0faaf2a
Mat Jones <mat@mjones.network>
2026-06-12 18:40:45 -0400

    fix(fish): Consistently keep prompt pinned to bottom of terminal

--- home-manager/components/fish.nix
+++ home-manager/components/fish.nix
@@ -70,7 +70,9 @@
         # of the terminal window so that running `clear` doesn't make
         # me move my eyes from the bottom back to the top of the screen;
         # keep the prompt consistently at the bottom
-        _prompt_move_to_bottom # call function manually to load it since event handlers don't get autoloaded
+        # Call the resize handler manually since event handlers don't get autoloaded.
+        # This also loads _prompt_move_to_bottom and registers its postexec handler.
+        _prompt_move_to_bottom_on_resize
       '';
 
       functions = {
@@ -78,6 +80,10 @@
         _prompt_move_to_bottom = {
           onEvent = "fish_postexec";
           body = ''printf '\e[%dH' $LINES'';
+        };
+        _prompt_move_to_bottom_on_resize = {
+          onVariable = "LINES";
+          body = "_prompt_move_to_bottom";
         };
         spc = {
           description = "Print a separator to help visually separate long outputs.";
